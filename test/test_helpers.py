8d92009ae7b13d5bacdb612cc3510acbfd6c1bcc
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2018-03-05 16:16:19 +0100

    helpers CI fix for Python2.7

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -14,7 +14,7 @@ def test_instance_id_returns_id_in_hex_form_without_0x():
     from moler.helpers import instance_id
     from six.moves import builtins
     # 0xf0a1 == 61601 decimal
-    with mock.patch("builtins.id", return_value=61601):
+    with mock.patch.object(builtins, "id", return_value=61601):
         instance = "moler object"
         assert "0x" not in instance_id(instance)
         assert instance_id(instance) == "f0a1"
