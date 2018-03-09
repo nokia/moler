df37f4ca73afdf1b7384e9977f6637fb8460c238
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2018-03-09 11:27:40 +0100

    test_cmd_at_get_imsi fixed after changes in cmd.at.get_imsi.AtCmdGetIMSI

--- test/test_cmd_at_get_imsi.py
+++ test/test_cmd_at_get_imsi.py
@@ -12,10 +12,11 @@ __email__ = 'kamil.kania@nokia.com, grzegorz.latuszek@nokia.com'
 # --------------------------- testing base class ---------------------------
 def test_calling_at_cmd_get_imsi_returns_expected_result(buffer_connection):
     from moler.cmd.at import get_imsi
-    at_cmd_get_imsi = get_imsi.AtCmdGetIMSI(connection=buffer_connection.moler_connection)
-    buffer_connection.remote_inject_response([get_imsi.COMMAND_OUTPUT])
+    at_cmd_get_imsi = get_imsi.AtCmdGetIMSI(connection=buffer_connection.moler_connection,
+                                            **get_imsi.COMMAND_KWARGS_ver_execute)
+    buffer_connection.remote_inject_response([get_imsi.COMMAND_OUTPUT_ver_execute])
     result = at_cmd_get_imsi()
-    assert result == get_imsi.COMMAND_RESULT
+    assert result == get_imsi.COMMAND_RESULT_ver_execute
 
 
 def test_at_cmd_get_imsi_raises_AtCommandModeNotSupported_when_instantiated_in_read_mode():
@@ -25,9 +26,9 @@ def test_at_cmd_get_imsi_raises_AtCommandModeNotSupported_when_instantiated_in_r
 
 
 def test_calling_at_cmd_get_imsi_in_test_mode_returns_empty_result(buffer_connection):
-    from moler.cmd.at.get_imsi import AtCmdGetIMSI
-    buffer_connection.remote_inject_response(["at+cimi=?\nOK\n"])
-    at_cmd_get_imsi = AtCmdGetIMSI(connection=buffer_connection.moler_connection,
-                                   operation="test")
+    from moler.cmd.at import get_imsi
+    buffer_connection.remote_inject_response([get_imsi.COMMAND_OUTPUT_ver_test])
+    at_cmd_get_imsi = get_imsi.AtCmdGetIMSI(connection=buffer_connection.moler_connection,
+                                            **get_imsi.COMMAND_KWARGS_ver_test)
     result = at_cmd_get_imsi()
     assert result == {}
