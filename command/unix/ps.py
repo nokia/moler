6ec338c0d0c3f0b441416cdf125a15b85385bec8
Dariusz Rosinski <dariusz.rosinski@nokia.com>
2018-03-09 10:33:21 +0100

    ps command base files

---
+++ command/unix/ps.py
@@ -0,0 +1,34 @@
+"""
+:copyright: Nokia Networks
+:author: Dariusz Rosinski
+:contact: dariusz.rosinski@nokia.com
+:maintainer:
+"""
+
+
+from moler.command import Command
+
+import re
+
+class Ps(Command):
+    def __init__(self,connection=None):
+
+        pass
+
+    def data_received(self):
+        pass
+
+    def on_prepare_run(self):
+        pass
+
+    def on_new_line(self):
+        pass
+
+    def get_final_timeout(self):
+        pass
+
+    def get_pid_to_name(self):
+        pass
+
+    def get_pids(self):
+        pass
