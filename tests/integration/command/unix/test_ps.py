ec230276d06952e05c7a6dcd16c1460447ec4be3
Dariusz Rosinski <dariusz.rosinski@nokia.com>
2018-03-09 10:33:21 +0100

    ps command base files

---
+++ tests/integration/command/unix/test_ps.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Testing external-IO TCP connection
+
+- open/close
+- send/receive (naming may differ)
+"""
+import time
+
+import pytest
+
+__author__ = 'Dariusz Rosinski'
+__copyright__ = 'Copyright (C) 2018, Nokia'
+__email__ = 'grzegorz.latuszek@nokia.com'
+
+
+def test_ps(ps_class):
+    ps = ps_class()
+    ps.get_pids()
+
+
+
+@pytest.fixture()
+def ps_class():
+    from command.unix.ps import Ps
+    return Ps
