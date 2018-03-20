c135db7f20069b8250dd64d48bcf5a7b406a20db
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2018-03-20 18:27:27 +0100

    connection configuration may define named specification of connection

--- test/test_connection_configuration.py
+++ test/test_connection_configuration.py
@@ -51,6 +51,47 @@ def test_can_select_connection_variant_from_plugin_connections(builtin_connectio
     assert conn.__class__.__name__ == 'DummyTcpConnection'
 
 
+def test_get_connection_may_not_use_both__name_and_io_type():
+    from moler.connection import get_connection
+
+    with pytest.raises(AssertionError) as err:
+        get_connection(name='www_server_1',
+                       io_type='tcp', host='localhost', port=2345)
+    assert "Use either 'name' or 'io_type' parameter (not both)" in str(err)
+
+
+def test_get_connection_must_use_either_name_or_io_type():
+    from moler.connection import get_connection
+
+    with pytest.raises(AssertionError) as err:
+        get_connection(host='localhost', port=2345)
+    assert "Provide either 'name' or 'io_type' parameter (none given)" in str(err)
+
+
+def test_can_select_connection_by_name(connections_config):
+    from moler.connection import get_connection
+
+    connections_config.define_connection(name="www_server_1",
+                                         io_type='tcp',
+                                         host='localhost', port=2345)
+    connections_config.set_default_variant(io_type='tcp', variant='threaded')
+    conn = get_connection(name='www_server_1')
+    assert conn.__module__ == 'moler.io.raw.tcp'
+    assert conn.__class__.__name__ == 'ThreadedTcp'
+    assert conn.host == 'localhost'
+    assert conn.port == 2345
+
+
+def test_cannot_select_connection_by_nonexisting_name(connections_config):
+    """Non-existing means here not defined inside configuration"""
+    from moler.connection import get_connection
+
+    connections_config.set_default_variant(io_type='tcp', variant='threaded')
+    with pytest.raises(KeyError) as err:
+        get_connection(name='www_server_1')
+    assert "Connection named 'www_server_1' was not defined inside configuration" in str(err)
+
+
 # --------------------------- resources ---------------------------
 
 @pytest.yield_fixture
