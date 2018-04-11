8dfe31e61651cb0536fbc23f4edbb9bd11551491
Yeshu Yang <yeshu.yang@nokia-sbell.com>
2018-04-11 17:16:34 +0800

    add whoami command

---
+++ test/unix/test_cmd_whoami.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Testing of whoami command.
+"""
+
+__author__ = 'Yang Yeshu'
+__copyright__ = 'Copyright (C) 2018, Nokia'
+__email__ = 'yeshu.yang@nokia.com'
+
+
+def test_calling_whoami_returns_result(buffer_connection):
+    from moler.cmd.unix.whoami import Whoami
+    command_output, expected_result = command_output_and_expected_result()
+    buffer_connection.remote_inject_response([command_output])
+    whoami_cmd = Whoami(connection=buffer_connection.moler_connection)
+    result = whoami_cmd()
+    assert result == expected_result
+
+
+def command_output_and_expected_result():
+    data="""
+    host:~ # whoami
+    ute
+    host:~ #"""
+
+    result= {
+        "USER": "ute"
+    }
+    return data,result
