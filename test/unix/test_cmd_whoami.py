7a698cdc63a04fa653125e6cf28b3fc88ef9f1fb
Yeshu Yang <yeshu.yang@nokia-sbell.com>
2018-05-09 13:59:29 +0800

    refactor whoami.py file

--- test/unix/test_cmd_whoami.py
+++ test/unix/test_cmd_whoami.py
@@ -5,8 +5,9 @@ Testing of whoami command.
 
 __author__ = 'Yeshu Yang'
 __copyright__ = 'Copyright (C) 2018, Nokia'
-__email__ = 'yeshu.yang@nokia.com'
+__email__ = 'yeshu.yang@nokia-sbell.com'
 
+import pytest
 
 def test_calling_whoami_returns_result(buffer_connection):
     from moler.cmd.unix.whoami import Whoami
@@ -16,7 +17,10 @@ def test_calling_whoami_returns_result(buffer_connection):
     result = whoami_cmd()
     assert result == expected_result
 
+# --------------------------- resources
 
+
+@pytest.fixture
 def command_output_and_expected_result():
     data="""
     host:~ # whoami
