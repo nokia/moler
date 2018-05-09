de625f656b626b25da8c256bc1d1d6c26787497f
Yeshu Yang <yeshu.yang@nokia-sbell.com>
2018-05-09 13:59:29 +0800

    refactor whoami.py file

--- moler/cmd/unix/whoami.py
+++ moler/cmd/unix/whoami.py
@@ -6,15 +6,14 @@ import re
 
 from moler.cmd.unix.genericunix import GenericUnix
 from moler.cmd.converterhelper import ConverterHelper
+from moler.exceptions import ParsingDone
 
 __author__ = 'Yeshu Yang'
 __copyright__ = 'Copyright (C) 2018, Nokia'
-__email__ = 'yeshu.yang@nokia.com'
+__email__ = 'yeshu.yang@nokia-sbell.com'
 
 
 class Whoami(GenericUnix):
-
-    _re_user = re.compile(r"(\S+)\s*$")
 
     def __init__(self, connection, prompt=None, new_line_chars=None):
         super(Whoami, self).__init__(connection, prompt, new_line_chars)
@@ -25,11 +24,20 @@ class Whoami(GenericUnix):
         return cmd
 
     def on_new_line(self, line, is_full_line):
-        if not is_full_line:
-            return super(Whoami, self).on_new_line(line, is_full_line)
-        if self._regex_helper.search_compiled(Whoami._re_user, line):
-            self.current_ret["USER"] = self._regex_helper.group(1)
+        if is_full_line:
+            try:
+                self._parse_user(line)
+            except ParsingDone:
+                pass
         return super(Whoami, self).on_new_line(line, is_full_line)
+
+    _re_user = re.compile(r"(?P<User>\S+)\s*$")
+
+    def _parse_user(self, line):
+        if self._regex_helper.search_compiled(Whoami._re_user, line):
+            self.current_ret["USER"] = self._regex_helper.group("User")
+            raise ParsingDone
+
 
 
 COMMAND_OUTPUT = """
