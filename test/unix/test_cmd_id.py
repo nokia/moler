d47873175224d849f1b2b65b4e2f14f58653bd2f
Michal Ernst <michal.ernst@nokia.com>
2018-05-10 09:31:19 +0200

    Remove duplicate test in pytest which exist in command documentation

--- test/unix/test_cmd_id.py
+++ test/unix/test_cmd_id.py
@@ -13,20 +13,3 @@ def test_id_returns_proper_command_string(buffer_connection):
     from moler.cmd.unix.id import Id
     id_cmd = Id(connection=buffer_connection.moler_connection, user="user")
     assert "id user" == id_cmd.command_string
-
-
-def test_calling_id_returns_result_parsed_from_command_output(buffer_connection):
-    from moler.cmd.unix.id import Id
-    command_output, expected_result = command_output_and_expected_result()
-    buffer_connection.remote_inject_response([command_output])
-    cd_cmd = Id(connection=buffer_connection.moler_connection, user="user")
-    result = cd_cmd()
-    assert result == expected_result
-
-
-@pytest.fixture
-def command_output_and_expected_result():
-    from moler.cmd.unix.id import COMMAND_OUTPUT_ver_execute as data
-    from moler.cmd.unix.id import COMMAND_RESULT_ver_execute as result
-
-    return data, result
