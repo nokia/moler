d6ae3477ee847fdeb050a961bab7fb07e8c15fcc
Snackwell Yang <snackwell.yang@nokia-sbell.com>
2018-05-10 23:11:20 +0800

    commit upon new master version.

--- test/unix/test_cmd_tar.py
+++ test/unix/test_cmd_tar.py
@@ -11,12 +11,3 @@ def test_tar_returns_proper_command_string(buffer_connection):
     from moler.cmd.unix.tar import Tar
     tar_cmd = Tar(buffer_connection, options="xzvf", file="test.tar.gz")
     assert "tar xzvf test.tar.gz" == tar_cmd.command_string
-
-
-def test_tar_raise_exception_wrong_command_string(buffer_connection):
-    from moler.cmd.unix.tar import Tar
-    from moler.exceptions import CommandFailure
-    try:
-        Tar(buffer_connection, options="xzvf")
-    except Exception as e:
-        assert isinstance(e, CommandFailure)
