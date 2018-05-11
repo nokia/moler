83a0a144ac4d246f70bd8a9dfd0bc9c8b819f081
Julia Patacz <julia.patacz@nokia.com>
2018-05-11 12:57:18 +0200

    Adding mpstat command and mpstat_test

---
+++ moler/cmd/unix/mpstat.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Mpstat command module.
+"""
+
+__author__ = 'Julia Patacz'
+__copyright__ = 'Copyright (C) 2018, Nokia'
+__email__ = 'julia.patacz@nokia.com'
+
+import re
+
+from moler.cmd.unix.genericunix import GenericUnix
+from moler.cmd.converterhelper import ConverterHelper
+from moler.exceptions import ParsingDone
+
+
+class Mpstat(GenericUnix):
+
+    def __init__(self, connection, options=None, prompt=None, new_line_chars=None):
+        super(Mpstat, self).__init__(connection, prompt, new_line_chars)
+        self._convert_helper = ConverterHelper()
+        # Parameters defined by calling the command
+        self.options = options
+        self.ret_required = False
+        self.current_ret['cpu'] = []
+
+    def build_command_string(self):
+        cmd = "mpstat"
+        if self.options:
+            cmd = cmd + " " + self.options
+        return cmd
+
+    def on_new_line(self, line, is_full_line):
+        if is_full_line:
+            try:
+                self._parse_line(line)
+            except ParsingDone:
+                pass  # line has been fully parsed by one of above parse-methods
+        return super(Mpstat, self).on_new_line(line, is_full_line)
+
+    # 08:27:55     CPU    %usr   %nice    %sys %iowait    %irq   %soft  %steal  %guest   %idle
+    _re_headers = re.compile(
+        r"(?P<TIME>\S+)\s+(?P<CPU>\S+)\s+(?P<USR>\S+)\s+(?P<NICE>\S+)\s+(?P<SYS>\S+)\s+(?P<IOWAIT>\S+)\s+(?P<IRQ>\S+)\s+(?P<SOFT>\S+)\s+(?P<STEAL>\S+)\s+(?P<GUEST>\S+)\s+(?P<IDLE>\S+)$")
+
+    def _parse_line(self, line):
+        if self._regex_helper.search_compiled(Mpstat._re_headers, line):
+            if self._regex_helper.group('CPU') != 'CPU':
+                temp = {}
+                temp[self._regex_helper.group('CPU')] = {}
+                temp[self._regex_helper.group('CPU')]['TIME'] = self._regex_helper.group('TIME')
+                temp[self._regex_helper.group('CPU')]['USR'] = self._regex_helper.group('USR')
+                temp[self._regex_helper.group('CPU')]['NICE'] = self._regex_helper.group('NICE')
+                temp[self._regex_helper.group('CPU')]['SYS'] = self._regex_helper.group('SYS')
+                temp[self._regex_helper.group('CPU')]['IOWAIT'] = self._regex_helper.group('IOWAIT')
+                temp[self._regex_helper.group('CPU')]['IRQ'] = self._regex_helper.group('IRQ')
+                temp[self._regex_helper.group('CPU')]['SOFT'] = self._regex_helper.group('SOFT')
+                temp[self._regex_helper.group('CPU')]['STEAL'] = self._regex_helper.group('STEAL')
+                temp[self._regex_helper.group('CPU')]['GUEST'] = self._regex_helper.group('GUEST')
+                temp[self._regex_helper.group('CPU')]['IDLE'] = self._regex_helper.group('IDLE')
+                self.current_ret["cpu"].append(temp)
+            raise ParsingDone
+
+
+COMMAND_OUTPUT = """
+toor4nsn@fzm-lsp-k2:~# mpstat
+Linux 4.4.112-rt127 (fzm-lsp-k2)    05/10/18    _armv7l_    (4 CPU)
+11:07:06     CPU    %usr   %nice    %sys %iowait    %irq   %soft  %steal  %guest   %idle
+11:07:06     all    1.50    0.07    2.28    0.50    0.00    0.17    0.00    0.00   95.49
+toor4nsn@fzm-lsp-k2:~# """
+COMMAND_KWARGS = {
+}
+COMMAND_RESULT = {
+    "cpu": [
+        {
+            "all": {
+                'TIME': '11:07:06',
+                'USR': '1.50',
+                'NICE': '0.07',
+                'SYS': '2.28',
+                'IOWAIT': '0.50',
+                'IRQ': '0.00',
+                'SOFT': '0.17',
+                'STEAL': '0.00',
+                'GUEST': '0.00',
+                'IDLE': '95.49',
+            }
+        }
+    ]
+}
