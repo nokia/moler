68c56e51520aaa526325433cf8417210ca1fa9a6
Bartosz Odziomek <bartosz.odziomek@nokia.com>
2018-05-11 11:51:04 +0200

    Added command rm test

---
+++ test/unix/test_cmd_rm.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Rm command module.
+"""
+
+__author__ = 'Bartosz Odziomek'
+__copyright__ = 'Copyright (C) 2018, Nokia'
+__email__ = 'bartosz.odziomek@nokia.com'
+
+def test_rm_returns_proper_command_string(buffer_connection):
+    from moler.cmd.unix.rm import Rm
+    rm_cmd = Rm(connection=buffer_connection.moler_connection, file="test.txt")
+    assert "rm test.txt" == rm_cmd.command_string
