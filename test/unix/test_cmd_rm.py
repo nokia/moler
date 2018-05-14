88b8328219b3b331d2ab2bd6aa34de8483a2fe6d
Bartosz Odziomek <bartosz.odziomek@nokia.com>
2018-05-14 15:20:56 +0200

    Fixed style

--- test/unix/test_cmd_rm.py
+++ test/unix/test_cmd_rm.py
@@ -7,6 +7,7 @@ __author__ = 'Bartosz Odziomek'
 __copyright__ = 'Copyright (C) 2018, Nokia'
 __email__ = 'bartosz.odziomek@nokia.com'
 
+
 def test_rm_returns_proper_command_string(buffer_connection):
     from moler.cmd.unix.rm import Rm
     rm_cmd = Rm(connection=buffer_connection.moler_connection, file="test.txt")
