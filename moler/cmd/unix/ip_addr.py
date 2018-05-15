3803bc66b99963316ff683702d2868937e866310
Sylwester Golonka <sylwester.golonka@nokia.com>
2018-05-15 12:08:56 +0200

    "delete bugs"

--- moler/cmd/unix/ip_addr.py
+++ moler/cmd/unix/ip_addr.py
@@ -14,11 +14,11 @@ from moler.cmd.converterhelper import ConverterHelper
 from moler.exceptions import ParsingDone
 
 
-
 class IpAddr(GenericUnix):
     def __init__(self, connection, prompt=None, new_line_chars=None, options=None):
         super(IpAddr, self).__init__(connection, prompt, new_line_chars)
         self._converter_helper = ConverterHelper()
+        # Parameters defined by calling the command
         self.if_name = None
         self.options = options
 
@@ -40,20 +40,20 @@ class IpAddr(GenericUnix):
                 pass
         return super(IpAddr, self).on_new_line(line, is_full_line)
 
-    def _process_line(self, line, regexp, key_list, type):
+    def _process_line(self, line, regexp, key_list, dict_type):
         if self._regex_helper.search_compiled(regexp, line):
             _ret = dict()
             for key in key_list:
                 _ret[key] = self._regex_helper.group(key)
 
-            if not self.current_ret[self.if_name][type][0]:
-                self.current_ret[self.if_name][type][0]=_ret
+            if not self.current_ret[self.if_name][dict_type][0]:
+                self.current_ret[self.if_name][dict_type][0] = _ret
             else:
-                self.current_ret[self.if_name][type].append(_ret)
+                self.current_ret[self.if_name][dict_type].append(_ret)
             raise ParsingDone
 
+    _re_interface = re.compile(r"^\d+:\s(?P<INTERFACE>[a-z\d.]+):.*$")
 
-    _re_interface = re.compile(r"^\d+:\s(?P<INTERFACE>[a-z\d\.]+):.*$")
     def _parse_interface(self, line):
         if self._regex_helper.search_compiled(IpAddr._re_interface, line):
             self.current_ret[self._regex_helper.group("INTERFACE")] = {"IPV4": [{}], "IPV6": [{}], "LINK": [{}]}
@@ -62,26 +62,26 @@ class IpAddr(GenericUnix):
     _re_ip_v4_brd = re.compile(
             r"\s*inet\s+(?P<IP>\d+\.\d+\.\d+\.\d+)\/(?P<MASK>\d+)\s(brd)\s(?P<BRD>\d+\.\d+\.\d+\.\d+)\sscope\s(\S.*\S)\s(\S.*\S)")
     _key_ip_v4_brd = ["IP", "MASK", "BRD"]
+
     def _parse_v4_brd(self, line):
         return self._process_line(line, IpAddr._re_ip_v4_brd, IpAddr._key_ip_v4_brd, "IPV4")
 
-
-
     _re_ip_v4 = re.compile(r"^\s*inet\s+(?P<IP>\d+\.\d+\.\d+\.\d+)\/(?P<MASK>\d+)\sscope\s(\S.*\S)\s(\S.*\S)$")
     _key_ip_v4 = ["IP", "MASK"]
+
     def _parse_v4(self, line):
         return self._process_line(line, IpAddr._re_ip_v4, IpAddr._key_ip_v4, "IPV4")
 
-
-
     _re_ip_v6 = re.compile(r"^.*inet6\s(?P<IP>.*)\/(?P<MASK>\d+)\sscope\s(\S.*\S)\s?$")
+
     _key_ip_v6 = ["IP", "MASK"]
+
     def _parse_v6(self, line):
         return self._process_line(line, IpAddr._re_ip_v6, IpAddr._key_ip_v6, "IPV6")
 
-
     _re_link = re.compile(r"^.*(link/[a-z]*)\s(?P<MAC>.*)\sbrd\s(?P<BRD>.*)$")
     _key_link = ["MAC", "BRD"]
+
     def _parse_link(self, line):
         return self._process_line(line, IpAddr._re_link, IpAddr._key_link, "LINK")
 
@@ -117,23 +117,22 @@ COMMAND_OUTPUT = """
       link/ether 00:16:3e:04:35:15 brd ff:ff:ff:ff:ff:ff
   root@fzm-lsp-k2:~# """
 COMMAND_KWARGS = {}
-COMMAND_RESULT ={
+COMMAND_RESULT = {
     'eth0': {
-            'IPV4': [   {
+            'IPV4': [{
                                  'BRD': '10.83.207.255',
                                  'IP': '10.83.206.42',
                                  'MASK': '21'}],
             'IPV6': [{
                                  'IP': 'fe80::216:3eff:fe71:7b5d',
                                  'MASK': '64'}],
-            'LINK': [   {
+            'LINK': [{
                                  'BRD': 'ff:ff:ff:ff:ff:ff',
                                  'MAC': '00:16:3e:71:7b:5d'}]},
     'eth1': {
-            'IPV4': [   {
+            'IPV4': [{
                                  'IP': '192.168.255.126',
-                                 'MASK': '24'},
-                        {
+                                 'MASK': '24'}, {
                                  'IP': '10.0.0.3',
                                  'MASK': '24'}],
             'IPV6': [{
@@ -144,20 +143,20 @@ COMMAND_RESULT ={
                                  'BRD': 'ff:ff:ff:ff:ff:ff',
                                  'MAC': '00:16:3e:86:4a:3a'}]},
     'eth2': {
-            'IPV4': [   {
+            'IPV4': [{
                                  'BRD': '192.168.255.255',
                                  'IP': '192.168.255.25',
                                  'MASK': '24'}],
             'IPV6': [{
                                  'IP': 'fe80::216:3eff:fe04:3515',
                                  'MASK': '64'}],
-            'LINK': [   {
+            'LINK': [{
                                  'BRD': 'ff:ff:ff:ff:ff:ff',
                                  'MAC': '00:16:3e:04:35:15'}]},
     'eth3': {
             'IPV4': [{}],
             'IPV6': [{}],
-            'LINK': [   {
+            'LINK': [{
                                  'BRD': 'ff:ff:ff:ff:ff:ff',
                                  'MAC': '00:16:3e:04:35:15'}]},
     'lo': {
@@ -167,6 +166,6 @@ COMMAND_RESULT ={
             'IPV6': [{
                                  'IP': '::1',
                                  'MASK': '128'}],
-            'LINK': [   {
+            'LINK': [{
                                  'BRD': '00:00:00:00:00:00',
                                  'MAC': '00:00:00:00:00:00'}]}}
