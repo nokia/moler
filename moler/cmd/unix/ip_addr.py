ded71e92f204496bfa452d8c8ce9bcbd96760c89
Sylwester Golonka <sylwester.golonka@nokia.com>
2018-05-15 15:22:41 +0200

    "test validation pep8"

--- moler/cmd/unix/ip_addr.py
+++ moler/cmd/unix/ip_addr.py
@@ -92,7 +92,7 @@ COMMAND_OUTPUT = """
       link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
       inet 127.0.0.1/8 scope host lo
          valid_lft forever preferred_lft forever
-      inet6 ::1/128 scope host 
+      inet6 ::1/128 scope host
          valid_lft forever preferred_lft forever
 2: eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UNKNOWN group default qlen 1000
       link/ether 00:16:3e:71:7b:5d brd ff:ff:ff:ff:ff:ff
