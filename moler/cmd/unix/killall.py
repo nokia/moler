45f091cc3c5b5b12d2777301d506af56aefbe70d
Yeshu Yang <yeshu.yang@nokia-sbell.com>
2018-05-16 17:19:41 +0800

    update killall and test

--- moler/cmd/unix/killall.py
+++ moler/cmd/unix/killall.py
@@ -10,35 +10,33 @@ __email__ = 'yeshu.yang@nokia.com'
 import re
 
 from moler.cmd.unix.genericunix import GenericUnix
-from moler.cmd.converterhelper import ConverterHelper
 from moler.exceptions import CommandFailure
 from moler.exceptions import ParsingDone
 
 
 class Killall(GenericUnix):
 
-    def __init__(self, connection, name, prompt=None, new_line_chars=None, is_verbose=None):
+    def __init__(self, connection, name, is_verbose=False, prompt=None, new_line_chars=None):
         super(Killall, self).__init__(connection, prompt=prompt, new_line_chars=new_line_chars)
-        self._converter_helper = ConverterHelper()
         self.is_verbose = is_verbose
         self.name = name
 
     def build_command_string(self):
         if self.is_verbose:
-            cmd = "{} {} {}".format("killall", self.is_verbose, self.name)
+            cmd = "{} {} {}".format("killall", "-v", self.name)
         else:
             cmd = "{} {}".format("killall", self.name)
         return cmd
 
     def on_new_line(self, line, is_full_line):
-        if not is_full_line:
-            return super(Killall, self).on_new_line(line, is_full_line)
-        try:
-            self._parse_no_permit(line)
-            self._parse_no_process(line)
-            self._parse_killall(line)
-        except ParsingDone:
-            pass
+        if is_full_line:
+            try:
+                self._parse_no_permit(line)
+                self._parse_no_process(line)
+                self._parse_killall_verbose(line)
+            except ParsingDone:
+                pass
+        self.current_ret["Status"] = "True"
         return super(Killall, self).on_new_line(line, is_full_line)
 
     def _parse_no_permit(self, line):
@@ -53,35 +51,19 @@ class Killall(GenericUnix):
 
     _re_killall = re.compile(r"Killed (?P<Name>[^\(]+)\((?P<Pid>\d+)\) with signal")
 
-    def _parse_killall(self, line):
+    def _parse_killall_verbose(self, line):
         if self.is_verbose:
             if self._regex_helper.search_compiled(Killall._re_killall, line):
                 if "Detail" not in self.current_ret:
                     self.current_ret["Detail"] = dict()
                 pid = self._regex_helper.group("Pid")
                 self.current_ret["Detail"][pid] = self._regex_helper.group("Name")
-        self.current_ret["Status"] = "True"
-        raise ParsingDone
+                raise ParsingDone
 
-
-COMMAND_OUTPUT_verbose = """
-Pclinux90:~ #  killall -v iperf
-Killed iperf(15054) with signal 15
-Pclinux90:~ #
-"""
-
-COMMAND_KWARGS_verbose = {"name": "iperf",
-                          "is_verbose": "-v"}
-
-COMMAND_RESULT_verbose = {
-    "Status": "True",
-    "Detail": {"15054": "iperf"}
-}
 
 COMMAND_OUTPUT_no_verbose = """
 Pclinux90:~ #  killall iperf
-Pclinux90:~ #
-"""
+Pclinux90:~ # """
 
 COMMAND_KWARGS_no_verbose = {"name": "iperf"}
 
@@ -92,8 +74,7 @@ COMMAND_RESULT_no_verbose = {
 COMMAND_OUTPUT_no_process = """
 PClinux110:/home/runner # killall tshark
 tshark: no process found
-PClinux110:/home/runner #
-"""
+PClinux110:/home/runner #"""
 
 COMMAND_KWARGS_no_process = {"name": "tshark"}
 
