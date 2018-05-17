2ce0c0d72286440ca5f26fcbe990bae899293b14
Sylwester Golonka <sylwester.golonka@nokia.com>
2018-05-17 10:49:37 +0200

    "fix getting options"

--- test/unix/test_cmd_ip_addr.py
+++ test/unix/test_cmd_ip_addr.py
@@ -9,5 +9,5 @@ __email__ = 'sylwester.golonka@nokia.com'
 
 def test_id_returns_proper_command_string(buffer_connection):
     from moler.cmd.unix.ip_addr import IpAddr
-    ip_addr_cmd = IpAddr(connection=buffer_connection.moler_connection)
+    ip_addr_cmd = IpAddr(connection=buffer_connection.moler_connection, options="a")
     assert "ip addr a" == ip_addr_cmd.command_string
