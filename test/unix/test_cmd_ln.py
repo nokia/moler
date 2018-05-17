3b7d5d22ada3da5401ba7b94f53758acf4f8b780
Haili Guo <haili.guo@nokia-sbell.com>
2018-05-17 15:57:23 +0800

    commit for the  command ln

--- test/unix/test_cmd_ln.py
+++ test/unix/test_cmd_ln.py
@@ -7,21 +7,30 @@ __copyright__ = 'Copyright (C) 2018, Nokia'
 __email__ = 'haili.guo@nokia-sbell.com'
 
 import pytest
+from moler.exceptions import CommandFailure
 
-def test_calling_ln_returns_result_parsed_from_command_output(buffer_connection):
+
+def test_ln_returns_proper_command_string(buffer_connection):
     from moler.cmd.unix.ln import Ln
-    command_output, expected_result = command_output_and_expected_result()
-    buffer_connection.remote_inject_response([command_output])
     ln_cmd = Ln(connection=buffer_connection.moler_connection, options="-s file1 file2")
-    result = ln_cmd()
-    assert result == expected_result
+    assert "ln -s file1 file2" == ln_cmd.command_string
+
+
+def test_calling_ln_raise_exception_wrong_command_string(buffer_connection):
+        from moler.cmd.unix.ln import Ln
+        command_output, expected_result = command_output_and_expected_result_file_exist()
+        buffer_connection.remote_inject_response([command_output])
+        ln_cmd = Ln(connection=buffer_connection.moler_connection, options="-s file1 file2")
+        with pytest.raises(CommandFailure, match=r'Command failed \'ln -s file1 file2\' with ERROR: '
+                                                 r'ln: failed to create symbolic link, File exists'):
+            ln_cmd()
 
 
 @pytest.fixture
-def command_output_and_expected_result():
+def command_output_and_expected_result_file_exist():
     data = """
-user@server:~> ln -s file1 file2
-user@server:~>"""
-    result = {
-    }
+    host:~ # ln -s file1 file2
+    ln: failed to create symbolic link, File exists
+    host:~ # """
+    result = {}
     return data, result
