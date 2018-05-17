75d93a1052f053183c9de94f3eaa350fb7dfd09d
Sylwester Golonka <sylwester.golonka@nokia.com>
2018-05-17 10:39:08 +0200

    "create test_cmd_ip_addr"

---
+++ test/unix/test_cmd_ip_addr.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Testing of id command.
+"""
+__author__ = 'Sylwester Golonka'
+__copyright__ = 'Copyright (C) 2018, Nokia'
+__email__ = 'sylwester.golonka@nokia.com'
+
+
+def test_id_returns_proper_command_string(buffer_connection):
+    from moler.cmd.unix.ip_addr import IpAddr
+    ip_addr_cmd = IpAddr(connection=buffer_connection.moler_connection)
+    assert "ip addr a" == ip_addr_cmd.command_string
