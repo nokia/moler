bcc0925a58ca08c71f151c9dd5c7842d3b7a51d5
Haili Guo <haili.guo@nokia-sbell.com>
2018-05-30 13:50:46 +0800

    commit for command zip

---
+++ test/unix/test_cmd_zip.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Testing of zip command.
+"""
+__author__ = 'Haili Guo'
+__copyright__ = 'Copyright (C) 2018, Nokia'
+__email__ = 'haili.guo@nokia-sbell.com'
+
+import pytest
+from moler.exceptions import CommandFailure
+
+
+def test_zip_returns_proper_command_string(buffer_connection):
+    from moler.cmd.unix.zip import Zip
+    zip_cmd = Zip(buffer_connection, options="", zip_file="test.zip", file="test.txt")
+    assert "zip test.zip test.txt" == zip_cmd.command_string
+
+
+def test_calling_zip_raise_exception_wrong_command_string(buffer_connection):
+    from moler.cmd.unix.zip import Zip
+    command_output, expected_result = command_output_and_expected_result_file_not_exist()
+    buffer_connection.remote_inject_response([command_output])
+    zip_cmd = Zip(connection=buffer_connection.moler_connection, options="", zip_file="test.zip", file="test.txt")
+    with pytest.raises(CommandFailure, match=r"Command failed 'zip test.zip test.txt' with ERROR: "r'zip error: Nothing to do! \(test.zip\)'):
+        zip_cmd()
+
+
+def test_zip_raise_exception_wrong_command_string(buffer_connection):
+    from moler.cmd.unix.zip import Zip
+    with pytest.raises(TypeError, match=r'.*missing \d+ required positional argument.*|__init__\(\) takes at least \d+ arguments \(\d+ given\)'):
+        Zip(buffer_connection, options="").command_string
+
+
+def test_calling_zip_timeout(buffer_connection):
+    from moler.cmd.unix.zip import Zip
+    command_output, expected_result = command_output_and_expected_result_timeout()
+    buffer_connection.remote_inject_response([command_output])
+    zip_cmd = Zip(connection=buffer_connection.moler_connection, options="", zip_file="test.zip", file="test.txt")
+    from moler.exceptions import CommandTimeout
+    with pytest.raises(CommandTimeout) as exception:
+        zip_cmd(timeout=0.5)
+    assert exception is not None
+
+
+@pytest.fixture
+def command_output_and_expected_result_file_not_exist():
+    data = """
+    user@server:~> zip test.zip test.txt
+    zip error: Nothing to do! (test.zip)
+    user@server:~> """
+    result = {
+
+    }
+    return data, result
+
+
+def command_output_and_expected_result_timeout():
+    data = """
+    user@server:~> zip test.zip test.txt
+    """
+    result={}
+    return data, result
