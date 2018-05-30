c517191e07e03013318f1623669f08d2ec439197
Haili Guo <haili.guo@nokia-sbell.com>
2018-05-30 13:50:46 +0800

    commit for command zip

---
+++ moler/cmd/unix/zip.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Zip command module.
+"""
+
+__author__ = 'Haili Guo'
+__copyright__ = 'Copyright (C) 2018, Nokia'
+__email__ = 'haili.guo@nokia-sbell.com'
+
+
+import re
+
+from moler.cmd.unix.genericunix import GenericUnix
+from moler.exceptions import CommandFailure, ParsingDone
+
+
+class Zip(GenericUnix):
+
+    def __init__(self, connection, options, file, zip_file, timeout=60, prompt=None, new_line_chars=None):
+        super(Zip, self).__init__(connection, prompt, new_line_chars)
+        # Parameters defined by calling the command
+        self.options = options
+        self.file = file
+        self.zip_file = zip_file
+        self.ret_required = False
+        self.timeout = timeout
+
+    def build_command_string(self):
+        if self.options:
+          cmd = "{} {} {} {}".format("zip", self.options, self.zip_file, self.file)
+        else:
+          cmd = "{} {} {}".format("zip", self.zip_file, self.file)
+        return cmd
+
+    def on_new_line(self, line, is_full_line):
+        if is_full_line:
+            try:
+                self._parse_error_via_output_line(line)
+            except ParsingDone:
+                pass  # line has been fully parsed by one of above parse-methods
+        return super(Zip, self).on_new_line(line, is_full_line)
+
+    _re_zip_line = re.compile(r'(?P<error>zip error:.*)')
+
+    def _parse_error_via_output_line(self, line):
+        if self._cmd_output_started and self._regex_helper.search_compiled(Zip._re_zip_line, line):
+            self.set_exception(CommandFailure(self, "ERROR: {}".format(self._regex_helper.group("error"))))
+            raise ParsingDone
+
+
+COMMAND_OUTPUT = """
+host:~ # zip test.zip test.txt
+  adding: test.txt (deflated 76%)
+host:~ # """
+
+COMMAND_RESULT = {
+}
+
+COMMAND_KWARGS = {
+    "options": "",
+    "zip_file": "test.zip",
+    "file": "test.txt",
+}
