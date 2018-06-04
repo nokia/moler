bd2c85400949fbac01db87599c9696dcf1073850
Sylwester Golonka <sylwester.golonka@nokia.com>
2018-06-04 13:36:51 +0200

    "create test_cmd_mkdir file"

---
+++ test/unix/test_cmd_mkdir.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Testing of mkdir command.
+"""
+__author__ = 'Sylwester Golonka'
+__copyright__ = 'Copyright (C) 2018, Nokia'
+__email__ = 'sylwester.golonka@nokia.com'
+
+
+def test_mkdir_returns_proper_command_string(buffer_connection):
+    from moler.cmd.unix.mkdir import Mkdir
+    mkdir_cmd = Mkdir(connection=buffer_connection.moler_connection, path="/home/user/")
+    assert "mkdir /home/user/" == mkdir_cmd.command_string
