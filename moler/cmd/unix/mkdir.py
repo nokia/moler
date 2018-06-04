ca7f37cde0c76ef387cac62633c33b9291cbe640
Sylwester Golonka <sylwester.golonka@nokia.com>
2018-06-04 14:57:43 +0200

    "edit on_new_line, add _parse_error"

--- moler/cmd/unix/mkdir.py
+++ moler/cmd/unix/mkdir.py
@@ -8,6 +8,8 @@ __email__ = 'sylwester.golonka@nokia.com'
 
 from moler.cmd.unix.genericunix import GenericUnix
 from moler.exceptions import CommandFailure
+from moler.exceptions import ParsingDone
+import re
 
 
 class Mkdir(GenericUnix):
@@ -26,20 +28,41 @@ class Mkdir(GenericUnix):
         return cmd
 
     def on_new_line(self, line, is_full_line):
-        if self._cmd_output_started and self._regex_helper.search(
-                r'mkdir:\scannot\screate\sdirectory\s(?P<PATH>.*):\s(?P<ERROR>.*)', line):
-            self.set_exception(CommandFailure(self, "ERROR: {}".format(self._regex_helper.group("ERROR"))))
+        if is_full_line:
+            try:
+                self._parse_error(line)
+            except ParsingDone:
+                pass
         return super(Mkdir, self).on_new_line(line, is_full_line)
 
+    _re_error = re.compile(r'mkdir:\scannot\screate\sdirectory\s(?P<PATH>.*):\s(?P<ERROR>.*)')
 
-COMMAND_OUTPUT = """
+    def _parse_error(self, line):
+        if self._regex_helper.search_compiled(Mkdir._re_parse_error, line):
+            self.set_exception(CommandFailure(self, "ERROR: {}".format(self._regex_helper.group("ERROR"))))
+            raise ParsingDone
+
+
+COMMAND_OUTPUT_no_parms = """
 user@server:~> mkdir /home/ute/test
 user@server:~>"""
 
-COMMAND_RESULT = {
+COMMAND_RESULT_no_parms = {
 
 }
-
-COMMAND_KWARGS = {
+COMMAND_KWARGS_no_parms = {
     "path": "/home/ute/test",
+}
+
+#
+COMMAND_OUTPUT_parms = """
+user@server:~> mkdir /home/ute/test -m 700
+user@server:~>"""
+
+COMMAND_RESULT_parms = {
+
+}
+COMMAND_KWARGS_parms = {
+    "path": "/home/ute/test",
+    "options": "-m 700",
 }
--- test/unix/test_cmd_mkdir.py
+++ test/unix/test_cmd_mkdir.py
@@ -6,29 +6,8 @@ __author__ = 'Sylwester Golonka'
 __copyright__ = 'Copyright (C) 2018, Nokia'
 __email__ = 'sylwester.golonka@nokia.com'
 
-from moler.cmd.unix.mkdir import Mkdir
-
-
-def test_calling_mkdir_returns_result_parsed_from_command_output(buffer_connection):
-    command_output, expected_result = command_output_and_expected_result()
-    buffer_connection.remote_inject_response([command_output])
-
-    mkdir_cmd = Mkdir(connection=buffer_connection.moler_connection, path="/home/ute/test")
-    result = mkdir_cmd()
-    assert result == expected_result
-
 
 def test_mkdir_returns_proper_command_string(buffer_connection):
     from moler.cmd.unix.mkdir import Mkdir
-    mkdir_cmd = Mkdir(connection=buffer_connection.moler_connection, path="/home/user/")
-    assert "mkdir /home/user/" == mkdir_cmd.command_string
-
-
-def command_output_and_expected_result():
-    data = """
-user@server:~> mkdir /home/ute/test
-user@server:~>"""
-
-    result = {
-    }
-    return data, result
+    mkdir_cmd = Mkdir(connection=buffer_connection.moler_connection, path="/home/ute/test")
+    assert "mkdir /home/ute/test" == mkdir_cmd.command_string
