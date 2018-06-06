42deade7765e2006d81adae1f83a95bd2ca6f029
Sylwester Golonka <sylwester.golonka@nokia.com>
2018-06-06 12:39:07 +0200

    "check mkdir raise wrong path"

--- test/unix/test_cmd_mkdir.py
+++ test/unix/test_cmd_mkdir.py
@@ -6,8 +6,29 @@ __author__ = 'Sylwester Golonka'
 __copyright__ = 'Copyright (C) 2018, Nokia'
 __email__ = 'sylwester.golonka@nokia.com'
 
+from moler.cmd.unix.mkdir import Mkdir
+from pytest import raises
+
 
 def test_mkdir_returns_proper_command_string(buffer_connection):
-    from moler.cmd.unix.mkdir import Mkdir
     mkdir_cmd = Mkdir(connection=buffer_connection.moler_connection, path="/home/ute/test")
     assert "mkdir /home/ute/test" == mkdir_cmd.command_string
+
+
+def test_mkdir_raise_exception_wrong_path(buffer_connection):
+    command_output, expected_result = command_output_and_expected_result()
+    buffer_connection.remote_inject_response([command_output])
+    mkdir_cmd = Mkdir(connection=buffer_connection.moler_connection, path="/home/test/test")
+    result = mkdir_cmd()
+    assert result == expected_result
+
+
+def command_output_and_expected_result():
+    data = """
+ute@debdev:~$ mkdir /home/test/test
+mkdir: cannot create directory ‘/home/test/test’: No such file or directory
+ute@debdev:~$
+    """
+    result = "ERROR: No such file or directory"
+
+    return data, result
