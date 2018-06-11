5ad3a2f78b9456d83ea16dc1774710bbf856b299
Michal Ernst <michal.ernst@nokia.com>
2018-06-11 15:27:07 +0200

    Fix PEP8

--- test/integration/test_io_raw_terminal.py
+++ test/integration/test_io_raw_terminal.py
@@ -7,13 +7,15 @@ __author__ = 'Marcin Usielski, Michal Ernst'
 __copyright__ = 'Copyright (C) 2018, Nokia'
 __email__ = 'marcin.usielski@nokia.com, michal.ernst@nokia.com'
 
-from moler.io.raw.terminal import Terminal
-from moler.cmd.unix.whoami import Whoami
-from moler.cmd.unix.ls import Ls
-from moler.cmd.unix.env import Env
-from moler.exceptions import CommandTimeout
 import getpass
+
 import pytest
+
+from moler.cmd.unix.env import Env
+from moler.cmd.unix.ls import Ls
+from moler.cmd.unix.whoami import Whoami
+from moler.exceptions import CommandTimeout
+from moler.io.raw.terminal import Terminal
 
 
 def test_terminal_cmd_whoami():
@@ -30,7 +32,7 @@ def test_terminal_timeout_next_command():
     max_nr = 5
     for i in range(1, max_nr):
         cmd = Env(connection=terminal)
-        #exchange to command ping when it is ready
+        # exchange to command ping when it is ready
         cmd.command_string = "ping 127.0.0.1"
         with pytest.raises(CommandTimeout):
             cmd(timeout=0.3)
