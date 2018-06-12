3e7b13985c9d559d21186810891d879ecb1a914f
Marcin Usielski <marcin.usielski@nokia.com>
2018-06-12 09:55:00 +0200

    moved removing colors from command output to Genericunix

--- moler/cmd/unix/genericunix.py
+++ moler/cmd/unix/genericunix.py
@@ -15,6 +15,11 @@ from moler.textualgeneric import TextualGeneric
 
 class GenericUnix(TextualGeneric):
     _re_fail = re.compile(r'command not found|No such file or directory|running it may require superuser privileges')
+    _re_color_codes = re.compile(r"\x1B\[[0-?]*[ -/]*[@-~]")  # Regex to remove color codes from command output
+
+    def __init__(self, connection, prompt=None, new_line_chars=None):
+        super(GenericUnix, self).__init__(connection, prompr, new_line_chars)
+        self.remove_colors_from_terminal_output = True
 
     def on_new_line(self, line, is_full_line):
         """
@@ -30,3 +35,17 @@ class GenericUnix(TextualGeneric):
 
     def is_failure_indication(self, line):
         return self._regex_helper.search_compiled(GenericUnix._re_fail, line)
+
+    def _strip_new_lines_chars(self, line):
+        line = super(GenericUnix, self)._strip_new_lines_chars(line)
+        if self.remove_colors_from_terminal_output:
+            line = self._remove_color_terminal_codes(line)
+        return line
+
+    def _remove_color_terminal_codes(self, line):
+        """
+        :param line: line from terminal
+        :return: line without terminal color codes
+        """
+        line = re.sub(GenericUnix._re_color_codes, "", line)
+        return line
