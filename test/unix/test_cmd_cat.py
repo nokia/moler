aa40c368bcae88b591bc58cd832f330b757b5c7c
Sylwester Golonka <sylwester.golonka@nokia.com>
2018-06-13 10:21:21 +0200

    "create test_cmd_cat file"

---
+++ test/unix/test_cmd_cat.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Testing of cat command.
+"""
+__author__ = 'Sylwester Golonka'
+__copyright__ = 'Copyright (C) 2018, Nokia'
+__email__ = 'sylwester.golonka@nokia.com'
+
+from moler.cmd.unix.cat import Cat
+from moler.exceptions import CommandFailure
+import pytest
+
+
+def test_cat_returns_proper_command_string(buffer_connection):
+    cat_cmd = Cat(connection=buffer_connection.moler_connection, path="/home/ute/test")
+    assert "cat /home/ute/test" == cat_cmd.command_string
+
+
+def test_cat_raise_exception_wrong_path(buffer_connection):
+    command_output, expected_result = command_output_and_expected_result()
+    buffer_connection.remote_inject_response([command_output])
+    cat_cmd = Cat(connection=buffer_connection.moler_connection, path="/home/test/test")
+    with pytest.raises(CommandFailure, match=r'Command failed \'cat /home/test/test\' with command failed in line \'cat: /home/ute/test: Is a directory\''):
+        cat_cmd()
+
+
+@pytest.fixture
+def command_output_and_expected_result():
+    data = """
+ute@debdev:~$ cat /home/test/test
+cat: /home/ute/test: Is a directory
+ute@debdev:~$
+    """
+    result = {
+
+    }
+
+    return data, result
