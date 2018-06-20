d2785ed156cd69a1ffb40106a2ee675f893d996b
Marcin Usielski <marcin.usielski@nokia.com>
2018-06-20 14:12:53 +0200

    Moved checkig states of device from Command to Device-decorator of Command

--- moler/device/__init__.py
+++ moler/device/__init__.py
@@ -9,20 +9,4 @@ __author__ = 'Grzegorz Latuszek'
 __copyright__ = 'Copyright (C) 2018, Nokia'
 __email__ = 'grzegorz.latuszek@nokia.com'
 
-from moler.connection import get_connection
-
-
-# TODO: name, logger/logger_name as param
-class Device(object):
-    def __init__(self, io_connection):
-        """
-        Create Device communicating over io_connection
-
-        :param io_connection: External-IO connection having embedded moler-connection
-        """
-        self.io_connection = io_connection
-
-    @classmethod
-    def from_named_connection(cls, connection_name):
-        io_conn = get_connection(name=connection_name)
-        return cls(io_connection=io_conn)
+from moler.device.device import Device
--- moler/device/unix.py
+++ moler/device/unix.py
@@ -10,13 +10,7 @@ __copyright__ = 'Copyright (C) 2018, Nokia'
 __email__ = 'grzegorz.latuszek@nokia.com'
 
 from moler.device import Device
-from moler.cmd.unix.cd import Cd
 
-# hardcode factory-registry for now.
-# TODO Future: make it dynamic import or alike to not touch this class when new cmd is created
-commands_of_device = {
-    'cd': Cd,
-}
 
 
 # TODO: name, logger/logger_name as param
@@ -29,20 +23,6 @@ class Unix(Device):
         """
         super(Unix, self).__init__(io_connection=io_connection)
 
-    def get_cmd(self, cmd_name, **kwargs):
-        """Return Command object assigned to cmd_name of given device"""
-        # TODO: return command object wrapped in decorator mocking it's start()
-        # TODO:  to check it it is starting in correct state (do it on flag)
-        if cmd_name in commands_of_device:
-            cmd_class = commands_of_device[cmd_name]  # load from factory/registry
-            cmd = cmd_class(connection=self.io_connection.moler_connection, **kwargs)
-            return cmd
-        for_whom = "for '{}' command of {} device".format(cmd_name, self.__class__.__name__)
-        raise KeyError("Unknown Command-derived class to instantiate " + for_whom)
-
-    def get_observer(self, observer_name, **kwargs):
-        """Return ConnectionObserver object assigned to observer_name of given device"""
-        raise NotImplemented
 
     def run(self, cmd_name='cd', **kwargs):
         """
