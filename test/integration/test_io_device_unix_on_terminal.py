128085fa1e373afab95af626efcc09a05b89fe92
Michal Ernst <michal.ernst@nokia.com>
2018-07-06 13:32:38 +0200

    Remove some wait_workaround -> still not all can be removed

--- test/integration/test_io_device_unix_on_terminal.py
+++ test/integration/test_io_device_unix_on_terminal.py
@@ -15,8 +15,6 @@ from moler.device.unix import Unix
 def test_unix_device_can_execute_cmds():
     unix = Unix(io_type='terminal', variant='threaded')
 
-    _wait_workaround(unix, Device.connected)
-
     cmd = unix.get_cmd('ls', options="-l")
     r = cmd()
     assert (r is not None)
@@ -29,7 +27,6 @@ def test_unix_device_can_execute_cmds():
 def test_device_unix_can_not_execute_cmds_in_incorect_state():
     unix = Unix(io_type='terminal', variant='threaded')
 
-    _wait_workaround(unix, Unix.connected)
     unix.goto_state(Unix.not_connected)
     _wait_workaround(unix, Unix.not_connected)
 
