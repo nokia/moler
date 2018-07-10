b217f94514e97b85f3a6719e4a694e39d14c7635
Michal Ernst <michal.ernst@nokia.com>
2018-07-10 07:30:19 +0200

    Make transition, state_hops, state_prompts object variable

--- moler/events/unix/wait4prompt.py
+++ moler/events/unix/wait4prompt.py
@@ -11,23 +11,17 @@ import time
 from moler.event import Event
 
 
-class Wait4(Event):
-    def __init__(self, connection, detect_pattern=None, detect_patterns=None, callback=None, callback_params=None,
-                 end_on_caught=True):
-        super(Wait4, self).__init__(connection=connection)
+class Wait4prompt(Event):
+    def __init__(self, connection, prompt=None, end_on_caught=True):
+        super(Wait4prompt, self).__init__(connection=connection)
         self.connection = connection
-        self.detect_pattern = detect_pattern
-        self.detect_patterns = detect_patterns
+        self.detect_pattern = prompt
         self.logger = logging.getLogger('moler.{}'.format(self))
         self.end_on_caught = end_on_caught
 
     def data_received(self, data):
         if not self.done():
-            if not (self.detect_pattern is None):
-                self._process_data(data=data, detect_pattern=self.detect_pattern)
-            elif self.detect_patterns:
-                for detect_pattern in self.detect_patterns:
-                    self._process_data(data=data, detect_pattern=detect_pattern)
+            self._process_data(data=data, detect_pattern=self.detect_pattern)
 
     def _process_data(self, data, detect_pattern):
         if re.search(detect_pattern, data):
