7f3cf9c461912888a165c003c55a0dfe2bdeaa44
Michal Plichta <michal.plichta@nokia.com>
2018-07-30 15:35:53 +0200

    make import local

--- test/test_cmds_doc.py
+++ test/test_cmds_doc.py
@@ -1,6 +1,4 @@
-from pytest import mark
-
-from moler.util.cmds_doc import check_if_documentation_exists
+from pytest import mark, raises
 
 __author__ = 'Michal Plichta'
 __copyright__ = 'Copyright (C) 2018, Nokia'
@@ -9,4 +7,5 @@ __email__ = 'michal.plichta@nokia.com'
 
 @mark.parametrize("path2cmds", ["moler/cmd"])
 def test_documentation_exists(path2cmds):
+    from moler.util.cmds_doc import check_if_documentation_exists
     assert check_if_documentation_exists(path2cmds) is True
