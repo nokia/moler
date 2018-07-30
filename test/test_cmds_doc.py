c48cf2d7c52cf1df0644bad95597348bafae03e0
Michal Plichta <michal.plichta@nokia.com>
2018-07-30 15:36:23 +0200

    add pytest for buffer_connection

--- test/test_cmds_doc.py
+++ test/test_cmds_doc.py
@@ -9,3 +9,13 @@ __email__ = 'michal.plichta@nokia.com'
 def test_documentation_exists(path2cmds):
     from moler.util.cmds_doc import check_if_documentation_exists
     assert check_if_documentation_exists(path2cmds) is True
+
+
+def test_buffer_connection_returns_threadconnection_with_moler_conn():
+    from moler.io.raw.memory import ThreadedFifoBuffer
+    from moler.connection import ObservableConnection
+    from moler.util.cmds_doc import _buffer_connection
+
+    buff_conn = _buffer_connection()
+    assert isinstance(buff_conn, ThreadedFifoBuffer) is True
+    assert isinstance(buff_conn.moler_connection, ObservableConnection) is True
