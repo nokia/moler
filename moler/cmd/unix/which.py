6dd8dc5194174dfb66263fc46f0d0b1756529d55
bylica <agnieszka.bylica@nokia.com>
2018-07-31 15:24:33 +0200

    Add in Which class init parameters, implement _parse and _set_result_keys methods, add tests. Add tests in test module.

--- moler/cmd/unix/which.py
+++ moler/cmd/unix/which.py
@@ -11,21 +11,88 @@ __email__ = 'agnieszka.bylica@nokia.com'
 import re
 
 from moler.cmd.unix.genericunix import GenericUnixCommand
-from moler.exceptions import CommandFailure
 from moler.exceptions import ParsingDone
 
 
 class Which(GenericUnixCommand):
-    def __init__(self, connection, prompt=None, new_line_chars=None):
+    def __init__(self, connection, names, show_all=None, prompt=None, new_line_chars=None):
         super(Which, self).__init__(connection=connection, prompt=prompt, new_line_chars=new_line_chars)
 
+        # Parameters defined by calling the command
+        self.names = names
+        self.show_all = show_all
+
+        self._set_result_keys()
+
+        # Internal variables
+        self._result_set = False
+
     def build_command_string(self):
-        pass
+        cmd = "which"
+        if self.show_all:
+            cmd = cmd + " -a"
+        for name in self.names:
+            cmd = cmd + " {}".format(name)
+        return cmd
 
     def on_new_line(self, line, is_full_line):
         if is_full_line:
             try:
-                pass
+                self._parse(line)
             except ParsingDone:
                 pass
+        elif not self.done() and not self._result_set:
+            self.set_result({})
         return super(Which, self).on_new_line(line, is_full_line)
+
+    def _parse(self, line):
+        for name in self.names:
+            _re_name = re.compile(r"(?P<NAME>.*%s.*)" % name, re.IGNORECASE)
+
+            if self._regex_helper.search_compiled(_re_name, line):
+                self.current_ret[name].append(self._regex_helper.group("NAME"))
+                self._result_set = True
+                raise ParsingDone
+
+    def _set_result_keys(self):
+        for name in self.names:
+            if name and name.split(" \t\n\r\f\v"):
+                self.current_ret[name] = list()
+
+
+COMMAND_OUTPUT = """
+xyz@debian:~$ which uname git
+/bin/uname
+/usr/bin/git
+xyz@debian:~$"""
+
+COMMAND_KWARGS = {
+    'names': ['uname', 'git']
+}
+
+COMMAND_RESULT = {'uname': ['/bin/uname'], 'git': ['/usr/bin/git']}
+
+
+COMMAND_OUTPUT_all = """
+xyz@debian:~$ which -a git less
+/usr/bin/git
+/usr/bin/less
+/bin/less
+xyz@debian:~$"""
+
+COMMAND_KWARGS_all = {
+    'names': ['git', 'less'], 'show_all': True
+}
+
+COMMAND_RESULT_all = {'git': ['/usr/bin/git'], 'less': ['/usr/bin/less', '/bin/less']}
+
+
+COMMAND_OUTPUT_no_result = """
+xyz@debian:~$ which -a abc
+xyz@debian:~$"""
+
+COMMAND_KWARGS_no_result = {
+    'names': ['abc'], 'show_all': True
+}
+
+COMMAND_RESULT_no_result = {}
--- test/unix/test_cmd_which.py
+++ test/unix/test_cmd_which.py
@@ -8,12 +8,16 @@ __copyright__ = 'Copyright (C) 2018, Nokia'
 __email__ = 'agnieszka.bylica@nokia.com'
 
 
-import pytest
-
-from moler.exceptions import CommandFailure
 from moler.cmd.unix.which import Which
 
 
 def test_which_returns_proper_command_string(buffer_connection):
-    which_cmd = Which(connection=buffer_connection.moler_connection)
-    assert "which" == which_cmd.command_string
+    which_cmd = Which(connection=buffer_connection.moler_connection, names=["uname", "git", "firefox"], prompt=None,
+                      new_line_chars=None)
+    assert "which uname git firefox" == which_cmd.command_string
+
+
+def test_which_returns_proper_command_string_show_all(buffer_connection):
+    which_cmd = Which(connection=buffer_connection.moler_connection, show_all=True, names=["man"],
+                      prompt=None, new_line_chars=None)
+    assert "which -a man" == which_cmd.command_string
