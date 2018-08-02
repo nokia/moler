52381014ed19be5f49d0a278a381a3374c0a58e8
Adrianna Pienkowska <adrianna.pienkowska@nokia.com>
2018-08-02 10:21:26 +0200

    Add next test to documentation

--- moler/cmd/unix/hexdump.py
+++ moler/cmd/unix/hexdump.py
@@ -3,9 +3,9 @@
 Hexdump command module.
 """
 
-__author__ = 'Agnieszka Bylica, Adrianna Pienkowska'
+__author__ = 'Agnieszka Bylica', 'Adrianna Pienkowska'
 __copyright__ = 'Copyright (C) 2018, Nokia'
-__email__ = 'agnieszka.bylica@nokia.com, adrianna.pienkowska@nokia,com'
+__email__ = 'agnieszka.bylica@nokia.com', 'adrianna.pienkowska@nokia.com'
 
 from moler.cmd.unix.genericunix import GenericUnixCommand
 from moler.exceptions import CommandFailure
@@ -60,19 +60,48 @@ class Hexdump(GenericUnixCommand):
             self.set_exception(CommandFailure(self, "No files given to hexdump: {}".format(self.files)))
 
 
-COMMAND_OUTPUT = """
+COMMAND_OUTPUT_proper_use = """
 xyz@debian:~$ hexdump old
 0000000 6741 0a61 6e41 6169 410a 646e 7a72 6a65
 0000010 410a 746e 6e6f 0a69
 0000018
 xyz@debian:~$"""
 
-COMMAND_KWARGS = {
+COMMAND_KWARGS_proper_use = {
     'files': ["old"]
 }
 
-COMMAND_RESULT = {
-    'RESULT': ['0000000 6741 0a61 6e41 6169 410a 646e 7a72 6a65',
-               '0000010 410a 746e 6e6f 0a69',
-               '0000018']
+COMMAND_RESULT_proper_use = {
+    'RESULT': ['0000000 6741 0a61 6e41 6169 410a 646e 7a72 6a65', '0000010 410a 746e 6e6f 0a69', '0000018']
+}
+
+
+COMMAND_OUTPUT_empty_file = """
+xyz@debian:~$ hexdump new
+xyz@debian:~$"""
+
+COMMAND_KWARGS_empty_file = {
+    'files': ["new"]
+}
+
+COMMAND_RESULT_empty_file = {
+    'RESULT': []
+}
+
+
+COMMAND_OUTPUT_options = """
+xyz@debian:~$ hexdump -b old
+0000000 101 147 141 012 101 156 151 141 012 101 156 144 162 172 145 152
+0000010 012 101 156 164 157 156 151 012
+0000018
+xyz@debian:~$"""
+
+COMMAND_KWARGS_options = {
+    'files': ["old"],
+    'options': '-b'
+}
+
+COMMAND_RESULT_options = {
+    'RESULT': ['0000000 101 147 141 012 101 156 151 141 012 101 156 144 162 172 145 152',
+               '0000010 012 101 156 164 157 156 151 012', '0000018']
 }
--- test/unix/test_cmd_hexdump.py
+++ test/unix/test_cmd_hexdump.py
@@ -5,11 +5,12 @@ Hexdump command test module.
 
 __author__ = 'Agnieszka Bylica, Adrianna Pienkowska'
 __copyright__ = 'Copyright (C) 2018, Nokia'
-__email__ = 'agnieszka.bylica@nokia.com, adrianna.pienkowska@nokia,com'
+__email__ = 'agnieszka.bylica@nokia.com, adrianna.pienkowska@nokia.com'
 
 import pytest
 from moler.cmd.unix.hexdump import Hexdump
 from moler.exceptions import CommandFailure
+
 
 def test_hexdump_returns_proper_command_string(buffer_connection):
     hexdump_cmd = Hexdump(buffer_connection, files=["old"])
@@ -30,16 +31,31 @@ def test_hexdump_raise_error_on_wrong_option(buffer_connection):
     with pytest.raises(CommandFailure):
         hexdump_cmd()
 
+
 def test_hexdump_raise_error_on_no_such_file(buffer_connection):
     hexdump_cmd = Hexdump(connection=buffer_connection.moler_connection, files=["new5"])
-    command_output, expected_result = command_output_and_expected_result_on_wrong_option()
+    command_output, expected_result = command_output_and_expected_result_on_no_such_file()
     buffer_connection.remote_inject_response([command_output])
-    assert 'hexdump -abc old' == hexdump_cmd.command_string
+    assert 'hexdump new5' == hexdump_cmd.command_string
     with pytest.raises(CommandFailure):
         hexdump_cmd()
 
+
 @pytest.fixture
 def command_output_and_expected_result_on_wrong_option():
+    output = """xyz@debian:~/Dokumenty/sed$ hexdump -abc old
+hexdump: invalid option -- 'a'
+usage: hexdump [-bcCdovx] [-e fmt] [-f fmt_file] [-n length]
+               [-s skip] [file ...]
+       hd      [-bcdovx]  [-e fmt] [-f fmt_file] [-n length]
+               [-s skip] [file ...]
+xyz@debian:~/Dokumenty/sed$ """
+    result = dict()
+    return output, result
+
+
+@pytest.fixture
+def command_output_and_expected_result_on_no_such_file():
     output = """xyz@debian:~/Dokumenty/sed$ hexdump new5
 hexdump: new5: No such file or directory
 xyz@debian:~/Dokumenty/sed$ """
