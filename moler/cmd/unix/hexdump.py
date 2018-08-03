a23baa6dd4e442c18956b1a3f93d0a552a7f294d
Adrianna Pienkowska <adrianna.pienkowska@nokia.com>
2018-08-03 13:04:42 +0200

    Deleting function _is_file, putting every hex into separate strings

--- moler/cmd/unix/hexdump.py
+++ moler/cmd/unix/hexdump.py
@@ -19,28 +19,28 @@ class Hexdump(GenericUnixCommand):
         self.options = options
         self.files = files
         self.current_ret['RESULT'] = list()
-        self._is_file()
 
     def build_command_string(self):
         cmd = "hexdump"
         if self.options:
             cmd = cmd + " " + self.options
         if self.files:
-            for file in self.files:
-                cmd = cmd + ' {}'.format(file)
+            for afile in self.files:
+                cmd = cmd + ' ' + afile
         return cmd
 
     def on_new_line(self, line, is_full_line):
         if is_full_line:
             try:
                 self._command_failure(line)
-                self._parse(line)
+                self._parse_hex_line(line)
             except ParsingDone:
                 pass
         return super(Hexdump, self).on_new_line(line, is_full_line)
 
-    def _parse(self, line):
-        self.current_ret["RESULT"].append(line)
+    def _parse_hex_line(self, line):
+        separate_hex = line.split()
+        self.current_ret["RESULT"].extend(separate_hex)
         raise ParsingDone
 
     _re_error = re.compile(r"hexdump:\s(?P<ERROR_MSG>.*)", re.IGNORECASE)
@@ -49,15 +49,6 @@ class Hexdump(GenericUnixCommand):
         if self._regex_helper.search_compiled(Hexdump._re_error, line):
             self.set_exception(CommandFailure(self, "ERROR: {}".format(self._regex_helper.group("ERROR_MSG"))))
             raise ParsingDone
-
-    def _is_file(self):
-        is_empty = True
-        if isinstance(self.files, list):
-            for file in self.files:
-                if file and file.strip(" \t\n\r\f\v"):
-                    is_empty = False
-        if is_empty:
-            self.set_exception(CommandFailure(self, "No files given to hexdump: {}".format(self.files)))
 
 
 COMMAND_OUTPUT_proper_use = """
@@ -72,7 +63,8 @@ COMMAND_KWARGS_proper_use = {
 }
 
 COMMAND_RESULT_proper_use = {
-    'RESULT': ['0000000 6741 0a61 6e41 6169 410a 646e 7a72 6a65', '0000010 410a 746e 6e6f 0a69', '0000018']
+    'RESULT': ['0000000', '6741', '0a61', '6e41', '6169', '410a', '646e', '7a72', '6a65', '0000010', '410a', '746e',
+               '6e6f', '0a69', '0000018']
 }
 
 
@@ -102,6 +94,6 @@ COMMAND_KWARGS_options = {
 }
 
 COMMAND_RESULT_options = {
-    'RESULT': ['0000000 101 147 141 012 101 156 151 141 012 101 156 144 162 172 145 152',
-               '0000010 012 101 156 164 157 156 151 012', '0000018']
+    'RESULT': ['0000000', '101', '147', '141', '012', '101', '156', '151', '141', '012', '101', '156', '144', '162',
+               '172', '145', '152', '0000010', '012', '101', '156', '164', '157', '156', '151', '012', '0000018']
 }
