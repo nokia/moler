5ab5e1106b1afbf78205003c1388ca8065f1206e
bylica <agnieszka.bylica@nokia.com>
2018-08-08 10:06:24 +0200

    Add testing cammands with -e option and text with white signs.

--- test/unix/test_cmd_echo.py
+++ test/unix/test_cmd_echo.py
@@ -13,3 +13,18 @@ from moler.cmd.unix.echo import Echo
 def test_echo_returns_proper_command_string(buffer_connection):
     echo_cmd = Echo(connection=buffer_connection.moler_connection)
     assert "echo" == echo_cmd.command_string
+
+
+def test_echo_returns_proper_command_string_with_e_option(buffer_connection):
+    echo_cmd = Echo(connection=buffer_connection.moler_connection, options='-e')
+    assert "echo -e" == echo_cmd.command_string
+
+
+def test_echo_returns_proper_command_string_with_text(buffer_connection):
+    echo_cmd = Echo(connection=buffer_connection.moler_connection, text='Hello my  beautiful   code!')
+    assert "echo 'Hello my  beautiful   code!'" == echo_cmd.command_string
+
+
+def test_echo_returns_proper_command_string_with_new_line_chars(buffer_connection):
+    echo_cmd = Echo(connection=buffer_connection.moler_connection, text='Hello \nmy \nbeautiful \ncode!')
+    assert "echo 'Hello \\nmy \\nbeautiful \\ncode!'" == echo_cmd.command_string
