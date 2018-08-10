d0a099d3b9a01e8bfb495261b00626713e511c7d
bylica <agnieszka.bylica@nokia.com>
2018-08-10 11:21:07 +0200

    Add implementation of build_command_string, _command_error and regex, command tests and tests in test file.

--- test/unix/test_cmd_userdel.py
+++ test/unix/test_cmd_userdel.py
@@ -15,5 +15,23 @@ from moler.cmd.unix.userdel import Userdel
 
 
 def test_userdel_returns_proper_command_string(buffer_connection):
-    useradd_cmd = Userdel(buffer_connection, user='xyz', prompt=None, new_line_chars=None)
-    assert "useradd xyz" == useradd_cmd.command_string
+    userdel_cmd = Userdel(connection=buffer_connection.moler_connection, user='xyz', prompt=None, new_line_chars=None)
+    assert "userdel xyz" == userdel_cmd.command_string
+
+
+def test_userdel_raises_command_error(buffer_connection):
+    command_output, expected_result = command_output_and_expected_result()
+    buffer_connection.remote_inject_response([command_output])
+    userdel_cmd = Userdel(connection=buffer_connection.moler_connection, user='xyz', prompt=None, new_line_chars=None)
+    with pytest.raises(CommandFailure):
+        userdel_cmd()
+
+
+@pytest.fixture
+def command_output_and_expected_result():
+    data = """xyz@debian:~$ userdel xyz
+userdel: user bylica is currently used by process 788
+xyz@debian:~$"""
+    result = dict()
+    return data, result
+
