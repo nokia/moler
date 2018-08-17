12c36f091db3bf92c7b4d5360a736484e8258f52
bylica <agnieszka.bylica@nokia.com>
2018-08-17 07:33:11 +0200

    Add init attributes and build_command_string implementation.

--- moler/cmd/unix/sftp.py
+++ moler/cmd/unix/sftp.py
@@ -13,11 +13,29 @@ from moler.exceptions import ParsingDone
 
 
 class Sftp(GenericUnixCommand):
-    def __init__(self, connection, prompt=None, new_line_chars=None):
+    def __init__(self, connection, host, user="", password=None, pathname=None, new_pathname=None, batch_file=None, prompt=None, new_line_chars=None):
         super(Sftp, self).__init__(connection=connection, prompt=prompt, new_line_chars=new_line_chars)
+
+        self.host = host
+        self.user = user
+        self.password = password
+        # For command without interactive session pathname or batch_file should be obligatory
+        self.pathname = pathname
+        self.new_pathname = new_pathname
+        self.batch_file = batch_file
 
     def build_command_string(self):
         cmd = "sftp"
+        if self.batch_file:
+            cmd = "{} -b {}".format(cmd, self.batch_file)
+        if self.user:
+            cmd = "{} {}@{}".format(cmd, self.user, self.host)
+        else:
+            cmd = "{} {}".format(cmd, self.host)
+        if self.pathname:
+            cmd = "{}:{}".format(cmd, self.pathname)
+        if self.new_pathname:
+            cmd = "{} {}".format(cmd, self.new_pathname)
         return cmd
 
     def on_new_line(self, line, is_full_line):
@@ -28,3 +46,8 @@ class Sftp(GenericUnixCommand):
                 pass
 
         super(Sftp, self).on_new_line(line, is_full_line)
+
+
+COMMAND_OUTPUT = ""
+COMMAND_KWARGS = {}
+COMMAND_RESULT = {}
--- test/unix/test_cmd_sftp.py
+++ test/unix/test_cmd_sftp.py
@@ -12,5 +12,25 @@ from moler.cmd.unix.sftp import Sftp
 
 
 def test_sftp_returns_proper_command_string(buffer_connection):
-    sftp_cmd = Sftp(connection=buffer_connection.moler_connection, prompt=None, new_line_chars=None)
-    assert "sftp" == sftp_cmd.command_string
+    sftp_cmd = Sftp(connection=buffer_connection.moler_connection, host="myhost.com", prompt=None, new_line_chars=None)
+    assert "sftp myhost.com" == sftp_cmd.command_string
+
+
+def test_sftp_returns_proper_command_string_user(buffer_connection):
+    sftp_cmd = Sftp(connection=buffer_connection.moler_connection, host="myhost.com", user="fred",
+                    prompt=None, new_line_chars=None)
+    assert "sftp fred@myhost.com" == sftp_cmd.command_string
+
+
+def test_sftp_returns_proper_command_string_pathname(buffer_connection):
+    sftp_cmd = Sftp(connection=buffer_connection.moler_connection, host="myhost.com", user="fred",
+                    pathname="/home/fred/homework.txt", prompt=None, new_line_chars=None)
+    assert "sftp fred@myhost.com:/home/fred/homework.txt" == sftp_cmd.command_string
+
+
+def test_sftp_returns_proper_command_string_new_pathname(buffer_connection):
+    sftp_cmd = Sftp(connection=buffer_connection.moler_connection, host="myhost.com", user="fred",
+                    pathname="/home/fred/homework.txt", new_pathname="/home/vivi/new_homework.txt", prompt=None,
+                    new_line_chars=None)
+    assert "sftp fred@myhost.com:/home/fred/homework.txt /home/vivi/new_homework.txt" == sftp_cmd.command_string
+
