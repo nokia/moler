1763f8ce9c14e89a200b18d5e567af74e7c90c8e
bylica <agnieszka.bylica@nokia.com>
2018-08-22 15:49:33 +0200

    Add _parse_line_from_prompt() and regex, change _parse_line, delete command_list.

--- moler/cmd/unix/sftp.py
+++ moler/cmd/unix/sftp.py
@@ -30,6 +30,9 @@ class Sftp(GenericUnixCommand):
         self.options = options
         self.command = command
 
+        self.command_sent = False
+        self.sending_started = False
+
         self.current_ret['RESULT'] = list()
 
     def build_command_string(self):
@@ -50,17 +53,20 @@ class Sftp(GenericUnixCommand):
         if is_full_line:
             try:
                 self._command_error(line)
+
                 self._confirm_connection(line)
                 self._send_password(line)
+
                 self._authentication_failure(line)
                 self._file_error(line)
                 self._connection_error(line)
+
                 self._check_if_connected(line)
                 self._send_command_if_prompt(line)
                 self._parse_line(line)
+                self._parse_line_from_prompt(line)
             except ParsingDone:
                 pass
-
         super(Sftp, self).on_new_line(line, is_full_line)
 
     _re_confirm_connection = re.compile(r"Are\syou\ssure\syou\swant\sto\scontinue\sconnecting\s\(yes/no\)\?", re.I)
@@ -90,18 +96,31 @@ class Sftp(GenericUnixCommand):
     _re_prompt = re.compile(r"sftp>", re.I)
 
     def _send_command_if_prompt(self, line):
-        if self.command and self._regex_helper.search_compiled(Sftp._re_prompt, line):
+        if not self.command_sent and self._regex_helper.search_compiled(Sftp._re_prompt, line):
             self.connection.sendline(self.command)
-            self.command = None
+            self.command_sent = True
             raise ParsingDone
-        elif not self.command and self._regex_helper.search_compiled(Sftp._re_prompt, line):
+        elif self.command_sent and self._regex_helper.search_compiled(Sftp._re_prompt, line):
             self.connection.sendline("exit")
             raise ParsingDone
 
+    def _parse_line_from_prompt(self, line):
+        if self.ready_to_parse_line:
+            if self.command_sent:
+                self.current_ret['RESULT'].append(line)
+
+    _re_fetching = re.compile(r"(Fetching\s.*)", re.I)
+    _re_progress_bar = re.compile(r"(.+\s+\d+%\s+\d+\s+.+\s+\d+:\d+)", re.I)
+
     def _parse_line(self, line):
         if self.ready_to_parse_line:
-            self.current_ret['RESULT'].append(line)
-        raise ParsingDone
+            if self._regex_helper.search_compiled(Sftp._re_fetching, line):
+                self.sending_started = True
+                raise ParsingDone
+            elif self.sending_started and self._regex_helper.search_compiled(Sftp._re_progress_bar, line):
+                raise ParsingDone
+            elif self.sending_started:
+                self.set_exception(CommandFailure(self, "ERROR: {}".format(line)))
 
     _re_resend_password = re.compile(r"(?P<RESEND>Permission\sdenied,\splease\stry\sagain)", re.I)
     _re_authentication = re.compile(r"(?P<AUTH>Authentication\sfailed.*)|(?P<PERM>Permission\sdenied\s.*)", re.I)
@@ -162,8 +181,27 @@ COMMAND_KWARGS = {
     'password': '1234'
 }
 COMMAND_RESULT = {
-    'RESULT': ["Fetching /upload/cat to /home/xyz/Docs/cat",
-               "/upload/cat                                   100%   23    34.4KB/s   00:00"]
+    'RESULT': []
+}
+
+
+COMMAND_OUTPUT_no_confirm_connection = """xyz@debian:/home$ sftp fred@192.168.0.102:cat /home/xyz/Docs/cat
+The authenticity of host '192.168.0.102 (192.168.0.102)' can't be established.
+ECDSA key fingerprint is SHA256:ghQ3iy/gH4YTqZOggql1eJCe3EETOOpn5yANJwFeRt0.
+Are you sure you want to continue connecting (yes/no)?
+Host key verification failed.
+xyz@debian:/home$"""
+
+COMMAND_KWARGS_no_confirm_connection = {
+    'host': '192.168.0.102',
+    'user': 'fred',
+    'pathname': 'cat',
+    'new_pathname': '/home/xyz/Docs/cat',
+    'confirm_connection': False,
+    'password': '1234'
+}
+COMMAND_RESULT_no_confirm_connection = {
+    'RESULT': []
 }
 
 
@@ -192,9 +230,11 @@ COMMAND_OUTPUT_upload = """xyz@debian:/home$ sftp fred@192.168.0.102
 fred@192.168.0.102's password:
 Connected to 10.0.2.15.
 sftp>
+sftp> put /home/xyz/Docs/echo/special_chars.py
 Uploading /home/xyz/Docs/echo/special_chars.py to /upload/special_chars.py
 /home/xyz/Docs/echo/special_chars.py         100%   95   377.2KB/s   00:00
 sftp>
+sftp> exit
 xyz@debian:/home$"""
 
 COMMAND_KWARGS_upload = {
@@ -209,23 +249,23 @@ COMMAND_RESULT_upload = {
 }
 
 
-COMMAND_OUTPUT_no_result = """xyz@debian:/home$ sftp fred@192.168.0.102
+COMMAND_OUTPUT_multiple_commands = """xyz@debian:/home$ sftp fred@192.168.0.102
 fred@192.168.0.102's password:
 Permission denied, please try again.
 fred@192.168.0.102's password:
 Connected to 192.168.0.102.
 sftp>
-sftp> mkdir animals
+sftp> mkdir pets
 sftp>
 sftp> exit
 xyz@debian:/home$"""
 
-COMMAND_KWARGS_no_result = {
+COMMAND_KWARGS_multiple_commands = {
     'host': '192.168.0.102',
     'user': 'fred',
     'password': '1234',
-    'command': 'mkdir animals'
+    'command': 'mkdir pets'
 }
-COMMAND_RESULT_no_result = {
+COMMAND_RESULT_multiple_commands = {
     'RESULT': []
 }
--- test/unix/test_cmd_sftp.py
+++ test/unix/test_cmd_sftp.py
@@ -98,8 +98,7 @@ Connected to 192.168.0.102.
 Fetching /upload/bear to /home/xyz/Docs/bear
 /upload/bear                                   100%   23    34.4KB/s   00:00
 xyz@debian:/home$"""
-    result = {'RESULT': ["Fetching /upload/bear to /home/xyz/Docs/bear",
-                         "/upload/bear                                   100%   23    34.4KB/s   00:00"]}
+    result = {'RESULT': []}
     return data, result
 
 
