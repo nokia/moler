3b8aa91d814f106318e8052ee7b384fa0501fba9
bylica <agnieszka.bylica@nokia.com>
2018-09-21 12:02:21 +0200

    Add _re_progress_bar_no_filename regex.

--- moler/cmd/unix/wget.py
+++ moler/cmd/unix/wget.py
@@ -53,16 +53,21 @@ class Wget(GenericUnixCommand):
 
     _re_progress_bar = re.compile(r"(?P<BAR>\S+\s+(?P<PERCENT>\d{1,2})%\s\[\s*[=+>]+\s*\]\s+\S+\s+\S+\s+in\s+[^s]+s)",
                                   re.I)
+    _re_progress_bar_no_filename = re.compile(r"(?P<BAR>(?P<PERCENT>\d{1,2})%\s\[\s*[=+>]+\s*\]\s+\S+\s+\S+\s+in\s+"
+                                              r"[^s]+s)", re.I)
 
     def _parse_line_progress_bar(self, line):
-        progress_bars = re.findall(Wget._re_progress_bar, line)
-        print(progress_bars)
+        if self._regex_helper.match_compiled(Wget._re_progress_bar_no_filename, line):
+            progress_bars = re.findall(Wget._re_progress_bar_no_filename, line)
+        else:
+            progress_bars = re.findall(Wget._re_progress_bar, line)
         if progress_bars:
             for bar in progress_bars:
                 self.current_percent = int(bar[1])
                 if (self.next_percent == 0) or (self.current_percent > self.next_percent):
                     self.next_percent = self.current_percent + 9
                     self.current_ret['PROGRESS_LOG'].append(bar[0])
+            raise ParsingDone
 
     _re_file_saved = re.compile(r"(?P<SAVED>\d\d\d\d-\d\d-\d\d\s\d\d:\d\d:\d\d\s\(\d+.\d+\s\w\w/s\)\s-\s.*)", re.I)
 
@@ -79,24 +84,7 @@ Connecting to ftp.gnu.org|208.118.235.20|:80... connected.
 HTTP request sent, awaiting response... 200 OK
 Length: 446966 (436K) [application/x-gzip]
 Saving to: wget-1.5.3.tar.gz
-wget-1.5.3.tar.gz 10% [==>                                          ]   3.56K  --.-KB/s    in 0s
-wget-1.5.3.tar.gz 14% [===>                                         ]   3.56K  --.-KB/s    in 2s
-wget-1.5.3.tar.gz 18% [====>                                        ]   3.56K  --.-KB/s    in 2s
-wget-1.5.3.tar.gz 25% [====+=====>                                  ]   3.56K  --.-KB/s    in 3s
-wget-1.5.3.tar.gz 30% [====+======++>                               ]   3.56K  --.-KB/s    in 3.4s
-wget-1.5.3.tar.gz 37% [===========++=>                              ]   3.56K  --.-KB/s    in 4s
-wget-1.5.3.tar.gz 38% [====+======++=>                              ]   3.56K  --.-KB/s    in 4s
-wget-1.5.3.tar.gz 42% [====+======++=>                              ]   3.56K  --.-KB/s    in 4.62s
-wget-1.5.3.tar.gz 50% [====+======++========>                       ]   3.56K  --.-KB/s    in 5s
-wget-1.5.3.tar.gz 51% [====+======++========>                       ]   3.56K  --.-KB/s    in 5s
-wget-1.5.3.tar.gz 56% [====+======++=========>                      ]   3.56K  --.-KB/s    in 5s
-wget-1.5.3.tar.gz 59% [====+======++===========>                    ]   3.56K  --.-KB/s    in 6s
-wget-1.5.3.tar.gz 61% [====+======++============>                   ]   3.56K  --.-KB/s    in 6s
-wget-1.5.3.tar.gz 64% [====+======++============>                   ]   3.56K  --.-KB/s    in 6s
-wget-1.5.3.tar.gz 78% [====+======++=================>              ]   3.56K  --.-KB/s    in 7s
-wget-1.5.3.tar.gz 88% [====+======++========================>       ]   3.56K  --.-KB/s    in 8s
-wget-1.5.3.tar.gz 99% [====+======++==============================> ]   3.56K  --.-KB/s    in 9.2s
-wget-1.5.3.tar.gz 100%[====+======++===============================>]   3.56K  --.-KB/s    in 9.3s
+wget-1.5.3.tar.gz 10% [==>                                          ]   3.56K  --.-KB/s    in 0s wget-1.5.3.tar.gz 14% [===>                                         ]   3.56K  --.-KB/s    in 2s wget-1.5.3.tar.gz 18% [====>                                        ]   3.56K  --.-KB/s    in 2s wget-1.5.3.tar.gz 25% [====+=====>                                  ]   3.56K  --.-KB/s    in 3s wget-1.5.3.tar.gz 30% [====+======++>                               ]   3.56K  --.-KB/s    in 3.4s wget-1.5.3.tar.gz 37% [===========++=>                              ]   3.56K  --.-KB/s    in 4s wget-1.5.3.tar.gz 38% [====+======++=>                              ]   3.56K  --.-KB/s    in 4s wget-1.5.3.tar.gz 42% [====+======++=>                              ]   3.56K  --.-KB/s    in 4.62s wget-1.5.3.tar.gz 50% [====+======++========>                       ]   3.56K  --.-KB/s    in 5s wget-1.5.3.tar.gz 51% [====+======++========>                       ]   3.56K  --.-KB/s    in 5s wget-1.5.3.tar.gz 56% [====+======++=========>                      ]   3.56K  --.-KB/s    in 5s wget-1.5.3.tar.gz 59% [====+======++===========>                    ]   3.56K  --.-KB/s    in 6s wget-1.5.3.tar.gz 61% [====+======++============>                   ]   3.56K  --.-KB/s    in 6s wget-1.5.3.tar.gz 64% [====+======++============>                   ]   3.56K  --.-KB/s    in 6s wget-1.5.3.tar.gz 78% [====+======++=================>              ]   3.56K  --.-KB/s    in 7s wget-1.5.3.tar.gz 88% [====+======++========================>       ]   3.56K  --.-KB/s    in 8s wget-1.5.3.tar.gz 99% [====+======++==============================> ]   3.56K  --.-KB/s    in 9.2s wget-1.5.3.tar.gz 100%[====+======++===============================>]   3.56K  --.-KB/s    in 9.3s
 2012-10-02 11:28:38 (58.9 KB/s) - wget-1.5.3.tar.gz
 moler@debian:~$"""
 
@@ -126,7 +114,7 @@ Connecting to ftp.gnu.org|208.118.235.20|:80... connected.
 HTTP request sent, awaiting response... 200 OK
 Length: 446966 (436K) [application/x-gzip]
 Saving to: wget-1.5.3.tar.gz
- 0% [==>                                          ]   3.56K  --.-KB/s    in 0s 10% [==>                                          ]   3.56K  --.-KB/s    in 1s 14% [===>                                         ]   3.56K  --.-KB/s    in 2s 18% [====>                                        ]   3.56K  --.-KB/s    in 2s 25% [====+=====>                                  ]   3.56K  --.-KB/s    in 3s 30% [====+======++>                               ]   3.56K  --.-KB/s    in 3.4s 37% [===========++=>                              ]   3.56K  --.-KB/s    in 4s 38% [====+======++=>                              ]   3.56K  --.-KB/s    in 4s 42% [====+======++=>                              ]   3.56K  --.-KB/s    in 4.62s 50% [====+======++========>                       ]   3.56K  --.-KB/s    in 5s 51% [====+======++========>                       ]   3.56K  --.-KB/s    in 5s 56% [====+======++=========>                      ]   3.56K  --.-KB/s    in 5s 59% [====+======++===========>                    ]   3.56K  --.-KB/s    in 6s 61% [====+======++============>                   ]   3.56K  --.-KB/s    in 6s 64% [====+======++============>                   ]   3.56K  --.-KB/s    in 6s 78% [====+======++=================>              ]   3.56K  --.-KB/s    in 7s 88% [====+======++========================>       ]   3.56K  --.-KB/s    in 8s 99% [====+======++==============================> ]   3.56K  --.-KB/s    in 9.2s 100%[====+======++===============================>]   3.56K  --.-KB/s    in 9.3s
+0% [==>                                          ]   3.56K  --.-KB/s    in 0s 10% [==>                                          ]   3.56K  --.-KB/s    in 1s 14% [===>                                         ]   3.56K  --.-KB/s    in 2s 18% [====>                                        ]   3.56K  --.-KB/s    in 2s 25% [====+=====>                                  ]   3.56K  --.-KB/s    in 3s 30% [====+======++>                               ]   3.56K  --.-KB/s    in 3.4s 37% [===========++=>                              ]   3.56K  --.-KB/s    in 4s 38% [====+======++=>                              ]   3.56K  --.-KB/s    in 4s 42% [====+======++=>                              ]   3.56K  --.-KB/s    in 4.62s 50% [====+======++========>                       ]   3.56K  --.-KB/s    in 5s 51% [====+======++========>                       ]   3.56K  --.-KB/s    in 5s 56% [====+======++=========>                      ]   3.56K  --.-KB/s    in 5s 59% [====+======++===========>                    ]   3.56K  --.-KB/s    in 6s 61% [====+======++============>                   ]   3.56K  --.-KB/s    in 6s 64% [====+======++============>                   ]   3.56K  --.-KB/s    in 6s 78% [====+======++=================>              ]   3.56K  --.-KB/s    in 7s 88% [====+======++========================>       ]   3.56K  --.-KB/s    in 8s 99% [====+======++==============================> ]   3.56K  --.-KB/s    in 9.2s 100%[====+======++===============================>]   3.56K  --.-KB/s    in 9.3s
 2012-10-02 11:28:38 (58.9 KB/s) - wget-1.5.3.tar.gz
 moler@debian:~$"""
 
@@ -138,7 +126,8 @@ COMMAND_KWARGS_2 = {
 
 COMMAND_RESULT_2 = {
     'RESULT': ['2012-10-02 11:28:38 (58.9 KB/s) - wget-1.5.3.tar.gz'],
-    'PROGRESS_LOG': ['10% [==>                                          ]   3.56K  --.-KB/s    in 0s',
+    'PROGRESS_LOG': ['0% [==>                                          ]   3.56K  --.-KB/s    in 0s',
+                     '10% [==>                                          ]   3.56K  --.-KB/s    in 1s',
                      '25% [====+=====>                                  ]   3.56K  --.-KB/s    in 3s',
                      '37% [===========++=>                              ]   3.56K  --.-KB/s    in 4s',
                      '50% [====+======++========>                       ]   3.56K  --.-KB/s    in 5s',
