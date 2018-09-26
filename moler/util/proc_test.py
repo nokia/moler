cebdf07c22fded1629a060422d755a0b956be09f
Marcin Usielski <marcin.usielski@nokia.com>
2018-09-26 13:30:08 +0200

    proc test

--- moler/util/proc_test.py
+++ moler/util/proc_test.py
@@ -8,8 +8,59 @@ __copyright__ = 'Copyright (C) 2018, Nokia'
 __email__ = 'grzegorz.latuszek@nokia.com, marcin.usielski@nokia.com, michal.ernst@nokia.com'
 
 import datetime
+import time
 from functools import wraps
 from types import FunctionType
+from moler.connection_observer import ConnectionObserver
+import logging
+
+
+class ProcTestWrapper(object):
+    start_time = 0
+    was_error = False
+    was_steps_end = False
+    logger = logging.getLogger("moler")
+
+    @staticmethod
+    def steps_start():
+        ProcTestWrapper.was_steps_end = False
+
+    @staticmethod
+    def steps_end():
+        ProcTestWrapper.was_steps_end = True
+
+    @staticmethod
+    def final_check():
+        # Checks exceptions since last call final_check
+        exceptions = ConnectionObserver.get_active_exceptions_in_time(ProcTestWrapper.start_time)
+        for exception in exceptions:
+            ProcTestWrapper.log_error("Unhandled exception: '{}'".format(exception))
+        ProcTestWrapper.start_time = time.time()
+        was_error_in_last_execution = ProcTestWrapper.was_error
+        ProcTestWrapper.was_error = False
+        assert ProcTestWrapper.was_steps_end is True
+        assert was_error_in_last_execution is False
+
+    @staticmethod
+    def log_error(msg):
+        ProcTestWrapper.status = False
+        ProcTestWrapper.logger.error(msg)
+
+    @staticmethod
+    def log(msg):
+        ProcTestWrapper.logger.info(msg)
+
+
+def log_error(msg):
+    ProcTestWrapper.log_error(msg)
+
+
+def log(msg):
+    ProcTestWrapper.log(msg)
+
+
+def steps_end():
+    ProcTestWrapper.steps_end()
 
 
 def wrapper(method):
@@ -18,6 +69,8 @@ def wrapper(method):
         class_name = args[0].__class__.__name__
         method_name = method.__name__
 
+
+        ProcTestWrapper.steps_start()
         start_time = datetime.datetime.now()
         print("START Method: {}.{} -> {}".format(class_name, method_name, start_time))
 
@@ -25,8 +78,10 @@ def wrapper(method):
 
         stop_time = datetime.datetime.now()
         print("END Method: {}.{} -> {}".format(class_name, method_name, stop_time))
+        ProcTestWrapper.final_check()
 
         return result
+
 
     return wrapped
 
@@ -35,9 +90,10 @@ class MetaProcTest(type):
     def __new__(meta, class_name, bases, classDict):
         newClassDict = {}
         for attributeName, attribute in classDict.items():
-            if isinstance(attribute, FunctionType):
-                # replace it with a wrapped version
-                attribute = wrapper(attribute)
+            if attributeName.startswith("test"):
+                if isinstance(attribute, FunctionType):
+                    # replace it with a wrapped version
+                    attribute = wrapper(attribute)
             newClassDict[attributeName] = attribute
         return type.__new__(meta, class_name, bases, newClassDict)
 
