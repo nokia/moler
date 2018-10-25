1c8b19f66039856f66cb64881a10e6bc562e93e3
Adrianna Pienkowska <adrianna.pienkowska@nokia.com>
2018-10-25 08:59:05 -0400

    Update pytests

--- test/unix/test_cmd_gunzip.py
+++ test/unix/test_cmd_gunzip.py
@@ -17,50 +17,52 @@ def test_gunzip_returns_proper_command_string(buffer_connection):
      assert "gunzip old.gz" == gunzip_cmd.command_string
 
 
-def test_gunzip_raise_error_on_wrong_option(buffer_connection):
+def test_gunzip_raise_error_on_wrong_option(buffer_connection, command_output_and_expected_result_on_wrong_option):
     gunzip_cmd = Gunzip(connection=buffer_connection.moler_connection, archive_name=["old.gz"], options='-b')
-    command_output, expected_result = command_output_and_expected_result_on_wrong_option()
+    command_output, expected_result = command_output_and_expected_result_on_wrong_option
     buffer_connection.remote_inject_response([command_output])
     assert 'gunzip -b old.gz' == gunzip_cmd.command_string
     with pytest.raises(CommandFailure):
         gunzip_cmd()
 
 
-def test_gunzip_raise_error_on_no_such_file(buffer_connection):
+def test_gunzip_raise_error_on_no_such_file(buffer_connection, command_output_and_expected_result_on_no_such_file):
     gunzip_cmd = Gunzip(connection=buffer_connection.moler_connection, archive_name=["new5.gz"])
-    command_output, expected_result = command_output_and_expected_result_on_no_such_file()
+    command_output, expected_result = command_output_and_expected_result_on_no_such_file
     buffer_connection.remote_inject_response([command_output])
     assert 'gunzip new5.gz' == gunzip_cmd.command_string
     with pytest.raises(CommandFailure):
         gunzip_cmd()
 
 
-def test_gunzip_raise_error_on_cannot_overwrite(buffer_connection):
+def test_gunzip_raise_error_on_cannot_overwrite(buffer_connection, command_output_and_expected_result_on_cannot_overwrite):
     gunzip_cmd = Gunzip(connection=buffer_connection.moler_connection, archive_name=["new5.gz"])
-    command_output, expected_result = command_output_and_expected_result_on_cannot_overwrite()
+    command_output, expected_result = command_output_and_expected_result_on_cannot_overwrite
     buffer_connection.remote_inject_response([command_output])
     assert 'gunzip new5.gz' == gunzip_cmd.command_string
     with pytest.raises(CommandFailure):
         gunzip_cmd()
 
 
-def test_gunzip_raise_error_on_cannot_overwrite_multiple_files(buffer_connection):
+def test_gunzip_raise_error_on_cannot_overwrite_multiple_files(
+        buffer_connection, command_output_and_expected_result_on_cannot_overwrite_multiple_files):
     gunzip_cmd = Gunzip(connection=buffer_connection.moler_connection, archive_name=["new5.gz", "new.gz"])
     assert 'gunzip new5.gz new.gz' == gunzip_cmd.command_string
     gunzip_cmd.start()
-    command_output, expected_result = command_output_and_expected_result_on_cannot_overwrite_multiple_files()
+    command_output, expected_result = command_output_and_expected_result_on_cannot_overwrite_multiple_files
     for output in command_output:
         buffer_connection.moler_connection.data_received(output.encode("utf-8"))
     with pytest.raises(CommandFailure):
         gunzip_cmd()
 
 
-def test_gunzip_raise_error_on_cannot_multiple_files(buffer_connection):
+def test_gunzip_raise_error_on_can_multiple_files(
+        buffer_connection, command_output_and_expected_result_on_overwrite_multiple_files):
     gunzip_cmd = Gunzip(connection=buffer_connection.moler_connection,
                         archive_name=["new5.gz", "new.gz"], overwrite=True)
     assert 'gunzip new5.gz new.gz' == gunzip_cmd.command_string
     gunzip_cmd.start()
-    command_output, expected_result = command_output_and_expected_result_on_overwrite_multiple_files()
+    command_output, expected_result = command_output_and_expected_result_on_overwrite_multiple_files
     for output in command_output:
         buffer_connection.moler_connection.data_received(output.encode("utf-8"))
     assert gunzip_cmd.done() is True
