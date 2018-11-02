03e68125a27229cef45ad1f7533e137af01e553c
Marcin Usielski <marcin.usielski@nokia.com>
2018-11-02 14:01:22 +0100

    test_cmd_sudo

--- test/unix/test_cmd_sudo.py
+++ test/unix/test_cmd_sudo.py
@@ -9,6 +9,7 @@ __email__ = 'marcin.usielski@nokia.com'
 
 from moler.cmd.unix.sudo import Sudo
 from moler.cmd.unix.pwd import Pwd
+from moler.exceptions import CommandTimeout
 import pytest
 
 
@@ -23,12 +24,36 @@ def test_calling_by_command_object(buffer_connection, command_output_and_expecte
     assert result == expected_result
 
 
+def test_failing_with_timeout(buffer_connection, command_output_and_expected_result_timeout):
+    command_output, expected_result = command_output_and_expected_result_timeout
+    buffer_connection.remote_inject_response([command_output])
+    cmd_pwd = Pwd(connection=buffer_connection.moler_connection)
+    cmd_sudo = Sudo(connection=buffer_connection.moler_connection, sudo_password="pass", cmd_object=cmd_pwd)
+    with pytest.raises(CommandTimeout):
+        cmd_sudo(timeout=0.1)
+
+
+
 @pytest.fixture()
 def command_output_and_expected_result():
     output = """user@client:~/moler$ sudo pwd
 [sudo] password for user: 
 /home/user/moler
 ute@debdev:~/moler$ """
+    result = {"cmd_ret": {
+        'current_path': 'moler',
+        'full_path': '/home/user/moler',
+        'path_to_current': '/home/user'
+    }}
+    return output, result
+
+
+@pytest.fixture()
+def command_output_and_expected_result_timeout():
+    output = """user@client:~/moler$ sudo pwd
+[sudo] password for user: 
+/home/user/moler
+"""
     result = {"cmd_ret": {
         'current_path': 'moler',
         'full_path': '/home/user/moler',
