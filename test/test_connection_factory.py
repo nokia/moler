f02091b26379f91b0a6a1e696264423cb46dd0b6
Michal Ernst <michal.ernst@nokia.com>
2018-11-13 10:27:21 +0100

    Refactoring due to Greg comments

--- test/test_connection_factory.py
+++ test/test_connection_factory.py
@@ -85,6 +85,8 @@ def builtin_io_type_example(request):
 @pytest.yield_fixture
 def builtin_connection_factories():
     import moler.connection  # installs builtin ones
+    import moler.config.connections as connection_cfg
     yield
     # restore since tests may overwrite builtins
-    moler.connection._register_builtin_connections()
+    connection_cfg.register_builtin_connections(moler.connection.ConnectionFactory,
+                                                moler.connection.ObservableConnection)
