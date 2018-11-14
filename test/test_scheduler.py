a2927cf0c3ec8c21c68a8c7e2f677293ee7a3901
Marcin Usielski <marcin.usielski@nokia.com>
2018-11-14 13:51:29 +0100

    new test

--- test/test_scheduler.py
+++ test/test_scheduler.py
@@ -10,13 +10,27 @@ from moler.scheduler import Scheduler
 from time import sleep
 
 
-def test_interval():
+def test_job():
     values = {'number': 0}
     job = Scheduler.get_job(callback, 0.1, {'param_dict': values})
     job.start()
-    sleep(0.25)
+    sleep(0.22)
     job.stop()
     assert(2 == values['number'])
+
+
+def test_2_jobs_concurrently():
+    values_1 = {'number': 0}
+    values_2 = {'number': 0}
+    job1 = Scheduler.get_job(callback, 0.05, {'param_dict': values_1})
+    job2 = Scheduler.get_job(callback, 0.10, {'param_dict': values_2})
+    job1.start()
+    job2.start()
+    sleep(0.23)
+    job1.stop()
+    job2.stop()
+    assert (2 == values_2['number'])
+    assert (4 == values_1['number'])
 
 
 def callback(param_dict):
