337e4f4968235621d04256ed0822ecc8a0f5f8ba
Marcin Usielski <marcin.usielski@nokia.com>
2018-11-14 16:10:13 +0100

    scheduler

--- moler/scheduler.py
+++ moler/scheduler.py
@@ -6,17 +6,19 @@ __email__ = 'marcin.usielski@nokia.com'
 
 
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.schedulers import SchedulerNotRunningError
+from moler.exceptions import WrongUsage
 
 
 class Scheduler(object):
     _object = None
 
-    def __init__(self, kind='interval'):
+    def __init__(self, scheduler_type='thread'):
         super(Scheduler, self).__init__()
-        self.kind = kind
-        self.scheduler = BackgroundScheduler()
-        self.scheduler.start()
+        self._scheduler_type = None
+        self._scheduler = None
+        self._swap_scheduler(scheduler_type)
 
     @staticmethod
     def _get_instance():
@@ -27,15 +29,31 @@ class Scheduler(object):
     @staticmethod
     def get_job(fun, interval, fun_params=None):
         instance = Scheduler._get_instance()
-        job_internal = instance.scheduler.add_job(fun, instance.kind, seconds=interval, kwargs=fun_params)
+        job_internal = instance._scheduler.add_job(fun, 'interval', seconds=interval, kwargs=fun_params)
         job_internal.pause()
         job = Job(job_internal)
         return job
 
     @staticmethod
-    def change_kind(kind):
+    def change_kind(scheduler_type):
         instance = Scheduler._get_instance()
-        instance.kind = kind
+        instance._swap_scheduler(scheduler_type)
+
+    def _swap_scheduler(self, new_scheduler_type):
+        self._scheduler = self._create_scheduler(new_scheduler_type)
+        self._scheduler_type = new_scheduler_type
+
+    def _create_scheduler(self, scheduler_type):
+        scheduler = self._scheduler
+        if self._scheduler_type != scheduler_type:
+            if scheduler_type == 'thread':
+                scheduler = BackgroundScheduler()
+            elif scheduler_type == 'asyncio':
+                scheduler = AsyncIOScheduler()
+            else:
+                raise WrongUsage("Wrong value of 'scheduler_type': '{}'. Allowed are 'thread' or 'asyncio'")
+        scheduler.start()
+        return scheduler
 
 
 class Job(object):
