e3ac482df996205ff181cf4f3632778d27aa6b63
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2018-12-21 14:25:41 +0100

    added clarification comments for easier maintainability of runner's integration test

--- test/integration/py3test_runners.py
+++ test/integration/py3test_runners.py
@@ -4,13 +4,17 @@ Testing connection observer runner API that should be fullfilled by any runner
 
 - submit
 - wait_for
+
+This integration tests check cooperation of the 3 players:
+connection_observer - runner - connection
+Main focus is on runner and it's correctness.
 """
 
 __author__ = 'Grzegorz Latuszek'
 __copyright__ = 'Copyright (C) 2018, Nokia'
 __email__ = 'grzegorz.latuszek@nokia.com'
 
-import sys
+import re
 import threading
 import time
 import platform
@@ -22,8 +26,15 @@ import pytest
 from moler.connection_observer import ConnectionObserver
 
 
+# --------------------------------------------------------------------
+# Testing data path from connection to connection observer
+# Runner is involved in data path establishing/dropping/securing
+# --------------------------------------------------------------------
+
 @pytest.mark.asyncio
 async def test_observer_gets_all_data_of_connection_after_it_is_submitted_to_background(observer_runner):
+    # another words: after returning from runner.submit() no data can be lost, no races
+
     # Raw 'def' usage note:
     # This functionality works as well when runner is used inside raw def function
     # since it only uses runner.submit() + awaiting time
@@ -116,6 +127,8 @@ async def test_runner_unsubscribes_from_connection_after_runner_shutdown(observe
     assert len(moler_conn._observers) == 0
 
 
+# TODO: test_runner_unsubscribes_from_connection_after_observer_is_done
+
 @pytest.mark.asyncio
 async def test_runner_doesnt_break_on_exception_raised_inside_observer(observer_runner):
     """Runner should be secured against 'wrongly written' connection-observer"""
@@ -132,9 +145,19 @@ async def test_runner_doesnt_break_on_exception_raised_inside_observer(observer_
     assert conn_observer.all_data_received == ["61 bytes"]
 
 
+# --------------------------------------------------------------------
+# Testing exceptions handling
+# Runner is involved in data path securing
+# --------------------------------------------------------------------
+
+# TODO: correct handling/storage of stack-trace of caught exceptions
+
 @pytest.mark.asyncio
 async def test_runner_sets_observer_exception_result_for_exception_raised_inside_observer(observer_runner):
     """Runner should correct behaviour of 'wrongly written' connection-observer"""
+    # Correctly written observer should not allow exceptions escaping from data_received().
+    # Such exceptions should be caught and stored inside observer via set_exception()
+
     # see - Raw 'def' usage note
     unknown_format_exception = Exception("unknown format")
     conn_observer = failing_net_down_detector(fail_on_data="zero bytes",
@@ -151,6 +174,10 @@ async def test_runner_sets_observer_exception_result_for_exception_raised_inside
 
 @pytest.mark.asyncio
 async def test_future_is_not_exception_broken_when_observer_is_exception_broken(observer_runner):
+    # Runner created future is involved in data path handling.
+    # That handling includes catching/storing exceptions. But such exception is exception of connection_observer
+    # and not future itself - future behaviour is OK when it can correctly handle exception of observer.
+
     # see - Raw 'def' usage note
     conn_observer = failing_net_down_detector(fail_on_data="zero bytes",
                                               fail_by_raising=Exception("unknown format"))
@@ -161,7 +188,7 @@ async def test_future_is_not_exception_broken_when_observer_is_exception_broken(
     connection.data_received("zero bytes")
     await asyncio.sleep(0.2)
 
-    assert future.exception() is None
+    assert future.exception() is None  # assumption here: used future has .exceptions() API
 
 
 @pytest.mark.asyncio
@@ -180,6 +207,16 @@ async def test_future_doesnt_return_result_of_observer(observer_runner, net_down
     assert future.result() is None
 
 
+# --------------------------------------------------------------------
+# Testing timeouts handling
+#
+# Part I - future's reaction on timeout
+# Future is a result produced by runner.submit(). Future expresses
+# "background life" of connection observer. In part I we test
+# pure-background-life without impact of wait_for() API - means
+# just send it to background and wait till timeout
+# --------------------------------------------------------------------
+
 @pytest.mark.asyncio
 async def test_future_timeouts_after_timeout_of_observer(observer_runner, connection_observer):
     """Observer has .timeout member"""
@@ -187,16 +224,17 @@ async def test_future_timeouts_after_timeout_of_observer(observer_runner, connec
     from moler.exceptions import ResultNotAvailableYet, MolerTimeout
 
     connection_observer.timeout = 0.1
-    observer_runner.submit(connection_observer)
+    future = observer_runner.submit(connection_observer)
     with pytest.raises(ResultNotAvailableYet):
         connection_observer.result()
     await asyncio.sleep(0.2)
     with pytest.raises(MolerTimeout):
-        connection_observer.result()
+        connection_observer.result()   # we should have exception in connection_observer
+    assert future.exception() is None  # but not inside future itself
 
 
 @pytest.mark.asyncio
-async def test_future_accomodates_to_extending_timeout_of_observer(observer_runner, connection_observer):
+async def test_future_accommodates_to_extending_timeout_of_observer(observer_runner, connection_observer):
     # see - Raw 'def' usage note
     from moler.exceptions import ResultNotAvailableYet, MolerTimeout
 
@@ -217,7 +255,7 @@ async def test_future_accomodates_to_extending_timeout_of_observer(observer_runn
 
 
 @pytest.mark.asyncio
-async def test_future_accomodates_to_shortening_timeout_of_observer(observer_runner, connection_observer):
+async def test_future_accommodates_to_shortening_timeout_of_observer(observer_runner, connection_observer):
     # see - Raw 'def' usage note
     from moler.exceptions import ResultNotAvailableYet, MolerTimeout
 
@@ -234,6 +272,15 @@ async def test_future_accomodates_to_shortening_timeout_of_observer(observer_run
         connection_observer.result()
 
 
+# --------------------------------------------------------------------
+# Testing timeouts handling
+#
+# Part II - timeouts while inside wait_for()
+# wait_for() API takes observer from background-life into foreground-life
+# testing here:
+# being inside blocking wait_for() - escape it on timeout
+# --------------------------------------------------------------------
+
 def test_wait_for__times_out_on_constructor_timeout(observer_runner, connection_observer):
     from moler.exceptions import MolerTimeout
 
@@ -247,6 +294,7 @@ def test_wait_for__times_out_on_constructor_timeout(observer_runner, connection_
     duration = time.time() - start_time
     assert duration >= 0.2
     assert duration < 0.25
+    assert future.exception() is None  # but no exception inside future itself
 
 
 def test_wait_for__times_out_on_specified_timeout(observer_runner, connection_observer):
@@ -323,6 +371,7 @@ def test_wait_for__tracks_changes_of_observer_timeout__shortening(observer_runne
 
 def test_wait_for__direct_timeout_takes_precedence_over_extended_observer_timeout(observer_runner,
                                                                                   connection_observer):
+    # this is another variant of test_wait_for__times_out_on_earlier_timeout
     from moler.exceptions import MolerTimeout
 
     connection_observer.timeout = 0.2
@@ -343,6 +392,33 @@ def test_wait_for__direct_timeout_takes_precedence_over_extended_observer_timeou
     assert duration < 0.3
 
 
+# --------------------------------------------------------------------
+# Testing timeouts handling
+#
+# Part III - on_timeout() callback
+# --------------------------------------------------------------------
+
+def test_observer__on_timeout__is_called_once_at_timeout(observer_runner, connection_observer):
+    from moler.exceptions import MolerTimeout
+
+    connection_observer.timeout = 0.33
+    future = observer_runner.submit(connection_observer)
+    with mock.patch.object(connection_observer, "on_timeout") as timeout_callback:
+        with pytest.raises(MolerTimeout):
+            observer_runner.wait_for(connection_observer, future,
+                                     timeout=0.33)
+            connection_observer.result()  # should raise Timeout
+        timeout_callback.assert_called_once()
+
+
+# --------------------------------------------------------------------
+# Testing correct usage
+#
+# We want to be helpful for users. Even if some usage is 'user fault'
+# (like calling long lasting functions inside async code) we want
+# to inform about such cases as much as we can. Not always it is possible.
+# --------------------------------------------------------------------
+
 @pytest.mark.asyncio
 async def test_wait_for__is_prohibited_inside_async_def(async_runner, connection_observer):
     # can't raise in generic runner since why non-async-runner should bother about being used inside 'async def'
@@ -354,47 +430,22 @@ async def test_wait_for__is_prohibited_inside_async_def(async_runner, connection
     # "magically_determine_if_being_yielded_from() is actually event_loop.is_running()"
     # but that works for asyncio and not for curio/trio
     #
-    # maybe we should just WARN - wait_for() is blocking, you should not use it inside 'async def'
-    # as right now any user may use time.sleep(12) inside 'async def'
     # Any way to treat wait_for() as awaitable?
     #
-    # If asyncio detects "slow/blocking" callback it logs it (asyncio/base_events.py def _run_once()):
-    #     if dt >= self.slow_callback_duration:
-    #         logger.warning('Executing %s took %.3f seconds',_format_handle(handle), dt)
-    #
-    # self.slow_callback_duration = 0.1 sec
-    #
-    # self._clock_resolution = 1e-09 = 0.000000001 s = 0.000001 ms = 0.001 us = 1ns (used by event loop scheduling)
-    #
-    # _run_once() realnie wola schedulowane funkcje. Wola je przez handle._run()
-    # gdzie handle to TaskStepMethWrapper  ktory oddaje sterowanie do coroutyn
-    # TaskStepMethWrapper to obiekt w C tworzony wewnatrz base_events.py def create_task()
-    #     task = tasks.Task(coro, loop=self)  wola call_soon(TaskStepMethWrapper)
-    #     opakowuje to jeszcze w events.Handle i opakowanego scheduluje: self._ready.append(handle)
-    # wiec przypuszczenie ze ten wrapper wola .send() na generatorze/coroutynie i odbiera wynik z yielda
     future = async_runner.submit(connection_observer)
     with pytest.raises(WrongUsage) as err:
-        # TODO: we should not allow 'wait_for()' inside 'async def' since it blocks asyncio-loop
         async_runner.wait_for(connection_observer, future)
         connection_observer.result()  # should raise WrongUsage
-    print("\n")
-    print(err.value)
+    # TODO: check if called from observer.await_done() and let exception speak more
+    # TODO:   in observer API not runner API since user uses observers-API (runner is hidden)
+    # assert "Can't call wait_for() from 'async def' - it is blocking call" in str(err.value)
+    assert "Can't call await_done() from 'async def' - it is blocking call" in str(err.value)
+    # check "fix-hint" inside exception
+    assert re.findall(r'consider using:\s+await observer\s+instead of:\s+observer.await_done()', str(err.value))
 
 
-def test_observer__on_timeout__is_called_once_at_timeout(observer_runner, connection_observer):
-    from moler.exceptions import MolerTimeout
+# TODO: test usage of iterable/awaitable
 
-    connection_observer.timeout = 0.33
-    future = observer_runner.submit(connection_observer)
-    with mock.patch.object(connection_observer, "on_timeout") as timeout_callback:
-        with pytest.raises(MolerTimeout):
-            observer_runner.wait_for(connection_observer, future,
-                                     timeout=0.33)  # means: timeout of wait_for longer then initial one
-            connection_observer.result()  # should raise Timeout
-        timeout_callback.assert_called_once()
-
-
-# TODO: tests for error cases
 # TODO: handling not awaited futures (infinite background observer, timeouting observer but "failing path stopped"
 
 # --------------------------- resources ---------------------------
