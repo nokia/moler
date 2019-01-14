8e9cd1bc1d9c0f3d69a1ad19131a1f0df0ac7c7b
Marcin Usielski <marcin.usielski@nokia.com>
2019-01-14 15:53:24 +0100

    refactroing

--- moler/command_scheduler.py
+++ moler/command_scheduler.py
@@ -4,10 +4,17 @@ __author__ = 'Marcin Usielski'
 __copyright__ = 'Copyright (C) 2019, Nokia'
 __email__ = 'marcin.usielski@nokia.com'
 
-
 import threading
 import time
+import logging
 from moler.util.moler_test import MolerTest
+
+
+class ConnectionQueue(object):
+    def __init__(self):
+        self.queue = list()
+        self.lock = threading.Lock()
+        self.current_cmd = None
 
 
 class CommandScheduler(object):
@@ -16,62 +23,59 @@ class CommandScheduler(object):
     def add_command_to_connection(cmd, do_not_wait=False):
         connection = cmd.connection
         lock = CommandScheduler._lock_for_connection(connection)
-        conn_atr = CommandScheduler._locks[connection]
+        conn_atr = CommandScheduler._connections[connection]
         with lock:
-            if conn_atr['current_cmd'] is None:
-                conn_atr['current_cmd'] = cmd
+            if conn_atr.current_cmd is None:
+                conn_atr.current_cmd = cmd
                 return True
             else:
                 if do_not_wait:
                     return False
-                conn_atr['queue'].append(cmd)
+                conn_atr.queue.append(cmd)
         start_time = cmd.start_time
         while cmd.timeout > (time.time() - start_time):
             MolerTest.sleep(seconds=0.001, quiet=True)
             with lock:
-                if conn_atr['current_cmd'] is None and cmd == conn_atr['queue'][0]:
-                    conn_atr['queue'].pop(0)
-                    conn_atr['current_cmd'] = cmd
-                    self._log(logging.DEBUG,
-                              ">'{}' Connection.add_command_to_connection '{}' added cmd from  queue.".format(cmd,
-                                                                                                              cmd.command_string))
+                if cmd.done():
+                    # command was marked done by something else (for example it was cancelled in other thread)
+                    CommandScheduler.remove_command_from_connection(cmd)
+                    return False
+                if conn_atr.current_cmd is None and cmd == conn_atr.queue[0]:
+                    conn_atr.queue.pop(0)
+                    conn_atr.current_cmd = cmd
+                    CommandScheduler._logger.log(logging.DEBUG,
+                                                 ">'{}' Connection.add_command_to_connection '{}' added cmd from  queue.".format(
+                                                     cmd,
+                                                     cmd.command_string))
                     return True
         # If we are here it means command timeout before it really starts
         with lock:
-            index = conn_atr['queue'].index(cmd)
-            conn_atr['queue'].pop(index)
+            CommandScheduler.remove_command_from_connection(cmd)
             return False
 
     @staticmethod
-    def remove_command_form_connection(cmd):
+    def remove_command_from_connection(cmd):
         connection = cmd.connection
         lock = CommandScheduler._lock_for_connection(connection)
-        conn_atr = CommandScheduler._locks[connection]
+        conn_atr = CommandScheduler._connections[connection]
         with lock:
-            if conn_atr['current_cmd'] == cmd:
-                conn_atr['current_cmd'] = None
+            if conn_atr.current_cmd == cmd:
+                conn_atr.current_cmd = None
             try:
-                index = conn_atr['queue'].index(cmd)
-                conn_atr['queue'].pop(index)
+                index = conn_atr.queue.index(cmd)
+                conn_atr.queue.pop(index)
             except ValueError:
                 pass  # Command does not wait in the queue so nothing to remove
 
     # internal methods and variables
 
     _conn_lock = threading.Lock()
-    _locks = dict()
+    _connections = dict()
+    _logger = logging.getLogger('moler.command_scheduler')
 
     @staticmethod
     def _lock_for_connection(connection):
         with CommandScheduler._conn_lock:
-            if connection not in CommandScheduler._locks:
-                CommandScheduler._locks[connection] = CommandScheduler._create_empty_connection_dict()
-            return CommandScheduler._locks[connection]['lock']
-
-    @staticmethod
-    def _create_empty_connection_dict():
-        ret = dict()
-        ret['lock'] = threading.Lock()
-        ret['queue'] = list()
-        ret['current_cmd'] = None
-        return ret
+            if connection not in CommandScheduler._connections:
+                CommandScheduler._connections[connection] = ConnectionQueue()
+            return CommandScheduler._connections[connection].lock
