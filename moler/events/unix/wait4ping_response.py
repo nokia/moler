d443630a9f3661ce4e29e3c87d83292977eda5ba
bylica <agnieszka.bylica@nokia.com>
2019-01-16 16:05:21 +0100

    Divide wait4ping into two classes.

--- moler/events/unix/wait4ping_response.py
+++ moler/events/unix/wait4ping_response.py
@@ -7,27 +7,18 @@ __email__ = 'agnieszka.bylica@nokia.com, tomasz.krol@nokia.com'
 
 import re
 from moler.events.lineevent import LineEvent
-from moler.exceptions import CommandFailure
 
 
-class Wait4Ping(LineEvent):
-    def __init__(self, connection, options, till_occurs_times=-1):
+class Wait4PingResponse(LineEvent):
+    def __init__(self, connection, till_occurs_times=-1):
         """
-        Event for 'Wait for ping.'.
+        Event for 'Wait for response from ping.'.
         :param connection: moler connection to device, terminal when command is executed
-        :param options: up - waits for correct answer; down - waits for 'no answer'
         :param till_occurs_times: number of event occurrence
         """
-        super(Wait4Ping, self).__init__(connection=connection, till_occurs_times=till_occurs_times)
+        super(Wait4PingResponse, self).__init__(connection=connection, till_occurs_times=till_occurs_times)
         self.connection = connection
-        self.options = options
-        if self.options == "up":
-            self.detect_pattern = r'\d+\s+bytes\s+from.+'
-        elif self.options == "down":
-            self.detect_pattern = r'no\s+answer\s+yet\s+for.*'
-        else:
-            self.set_exception(CommandFailure(self, "Wrong option used: {}, available options: up down.".format(
-                self.options)))
+        self.detect_pattern = r'\d+\s+bytes\s+from.+'
 
     def on_new_line(self, line, is_full_line):
         """
