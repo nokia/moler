db6c5310c7f1da65b152e25b5ba21df977b7ee20
bylica <agnieszka.bylica@nokia.com>
2019-01-21 08:24:02 +0100

    Delete on_new_line, add ret_required=False and change docstring.

--- moler/cmd/unix/reboot.py
+++ moler/cmd/unix/reboot.py
@@ -14,19 +14,17 @@ class Reboot(GenericUnixCommand):
 
     def __init__(self, connection, prompt=None, newline_chars=None, runner=None):
         """
-        :param connection: connection
+        :param connection: moler connection to device, terminal when command is executed
         :param prompt: Prompt of the starting shell
-        :param newline_chars: end of line characters
+        :param newline_chars: Characters to split lines
+        :param runner: Runner to run command
         """
-
         super(Reboot, self).__init__(connection=connection, prompt=prompt, newline_chars=newline_chars, runner=runner)
+
+        self.ret_required = False
 
     def build_command_string(self):
         return "reboot"
-
-    def on_new_line(self, line, is_full_line):
-        self.set_result({})
-        return super(Reboot, self).on_new_line(line, is_full_line)
 
 
 COMMAND_OUTPUT = """
