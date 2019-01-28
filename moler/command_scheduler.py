4a38ae2f059375d6904d721dd45d8572a4a9b613
Marcin Usielski <marcin.usielski@nokia.com>
2019-01-28 15:33:59 +0100

    added test_command_scheduler

--- moler/command_scheduler.py
+++ moler/command_scheduler.py
@@ -4,57 +4,42 @@ __author__ = 'Marcin Usielski'
 __copyright__ = 'Copyright (C) 2019, Nokia'
 __email__ = 'marcin.usielski@nokia.com'
 
-
 import threading
 import time
+import logging
 from moler.util.moler_test import MolerTest
 
 
 class CommandScheduler(object):
 
     @staticmethod
-    def add_command_to_connection(cmd, do_not_wait=False):
-        connection = cmd.connection
-        lock = CommandScheduler._lock_for_connection(connection)
-        conn_atr = CommandScheduler._locks[connection]
-        with lock:
-            if conn_atr['current_cmd'] is None:
-                conn_atr['current_cmd'] = cmd
-                return True
-            else:
-                if do_not_wait:
-                    return False
-                conn_atr['queue'].append(cmd)
-        start_time = cmd.start_time
-        while cmd.timeout > (time.time() - start_time):
-            MolerTest.sleep(seconds=0.001, quiet=True)
-            with lock:
-                if conn_atr['current_cmd'] is None and cmd == conn_atr['queue'][0]:
-                    conn_atr['queue'].pop(0)
-                    conn_atr['current_cmd'] = cmd
-                    self._log(logging.DEBUG,
-                              ">'{}' Connection.add_command_to_connection '{}' added cmd from  queue.".format(cmd,
-                                                                                                              cmd.command_string))
+    def add_command_to_connection(cmd, wait_for_slot=True):
+        """
+        Adds command to execute on connection.
+        :param cmd: Command object to add to connection
+        :param wait_for_slot: If True then waits till command timeout or there is free slot to execute command. If False
+        then returns immediately regardless there is free slot or not.
+        :return: True if command was marked as current executed, False if command cannot be set as current executed.
+        """
+        if CommandScheduler._add_command_to_execute(cmd=cmd):
+            return True
+        else:
+            if wait_for_slot:
+                CommandScheduler._add_command_to_queue(cmd=cmd)
+                if CommandScheduler._wait_for_slot_for_command(cmd=cmd):
                     return True
-        # If we are here it means command timeout before it really starts
-        with lock:
-            index = conn_atr['queue'].index(cmd)
-            conn_atr['queue'].pop(index)
-            return False
+                # If we are here it means command timeout before it really starts
+                CommandScheduler._remove_command(cmd=cmd)
+        return False
 
     @staticmethod
-    def remove_command_form_connection(cmd):
-        connection = cmd.connection
-        lock = CommandScheduler._lock_for_connection(connection)
-        conn_atr = CommandScheduler._locks[connection]
-        with lock:
-            if conn_atr['current_cmd'] == cmd:
-                conn_atr['current_cmd'] = None
-            try:
-                index = conn_atr['queue'].index(cmd)
-                conn_atr['queue'].pop(index)
-            except ValueError:
-                pass  # Command does not wait in the queue so nothing to remove
+    def remove_command_from_connection(cmd):
+        """
+        Removes command from queue and/or current executed on connection.
+        :param cmd: Command object to remove from connection
+        :return: None
+        """
+        CommandScheduler._remove_command(cmd=cmd)
 
     # internal methods and variables
 
@@ -75,3 +60,54 @@ class CommandScheduler(object):
         ret['queue'] = list()
         ret['current_cmd'] = None
         return ret
+
+    @staticmethod
+    def _wait_for_slot_for_command(cmd):
+        connection = cmd.connection
+        lock = CommandScheduler._lock_for_connection(connection)
+        start_time = cmd.start_time
+        conn_atr = CommandScheduler._locks[connection]
+        while cmd.timeout > (time.time() - start_time):
+            MolerTest.sleep(seconds=0.001, quiet=True)
+            with lock:
+                if conn_atr['current_cmd'] is None and cmd == conn_atr['queue'][0]:
+                    conn_atr['queue'].pop(0)
+                    conn_atr['current_cmd'] = cmd
+                    cmd._log(logging.DEBUG,
+                             ">'{}' Connection.add_command_to_connection '{}' added cmd from  queue.".format(
+                                 cmd, cmd.command_string))
+                    return True
+        return False
+
+    @staticmethod
+    def _remove_command(cmd):
+        connection = cmd.connection
+        lock = CommandScheduler._lock_for_connection(connection)
+        conn_atr = CommandScheduler._locks[connection]
+        with lock:
+            if cmd == conn_atr['current_cmd']:
+                conn_atr['current_cmd'] = None
+            try:
+                index = conn_atr['queue'].index(cmd)
+                conn_atr['queue'].pop(index)
+            except ValueError:  # command object does not exist in the list
+                pass
+
+    @staticmethod
+    def _add_command_to_execute(cmd):
+        connection = cmd.connection
+        lock = CommandScheduler._lock_for_connection(connection)
+        conn_atr = CommandScheduler._locks[connection]
+        with lock:
+            if conn_atr['current_cmd'] is None:
+                conn_atr['current_cmd'] = cmd
+                return True
+        return False
+
+    @staticmethod
+    def _add_command_to_queue(cmd):
+        connection = cmd.connection
+        lock = CommandScheduler._lock_for_connection(connection)
+        conn_atr = CommandScheduler._locks[connection]
+        with lock:
+            conn_atr['queue'].append(cmd)
---
+++ test/test_command_scheduler.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Tests for CommandScheduler
+"""
+
+__author__ = 'Marcin Usielski'
+__copyright__ = 'Copyright (C) 2019, Nokia'
+__email__ = 'marcin.usielski@nokia.com'
+
+
+from moler.cmd.unix.ls import Ls
+from moler.cmd.unix.whoami import Whoami
+from moler.command_scheduler import CommandScheduler
+
+
+def test_add_when_empty(buffer_connection):
+    cmd = Ls(connection=buffer_connection)
+    assert CommandScheduler.add_command_to_connection(cmd) is True
+    CommandScheduler.remove_command_from_connection(cmd)
+
+
+def test_add_when_queue_occupied(buffer_connection):
+    cmd1 = Ls(connection=buffer_connection)
+    cmd2 = Whoami(connection=buffer_connection)
+    assert CommandScheduler.add_command_to_connection(cmd1) is True
+    assert CommandScheduler.add_command_to_connection(cmd2, wait_for_slot=False) is False
+    CommandScheduler.remove_command_from_connection(cmd1)
+    assert CommandScheduler.add_command_to_connection(cmd2, wait_for_slot=False) is True
+    CommandScheduler.remove_command_from_connection(cmd2)
