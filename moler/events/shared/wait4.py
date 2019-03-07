cd1a206d5bafa4bf65fa477c3d9c5ff19cca3034
Tomasz Krol <tomasz.krol@nokia.com>
2019-03-07 09:10:21 +0100

    add doc tests

--- moler/events/shared/wait4.py
+++ moler/events/shared/wait4.py
@@ -22,29 +22,118 @@ class Wait4(LineEvent):
                                     detect_patterns=detect_patterns)
 
 
-EVENT_OUTPUT = """
-8d FCT-1011 <2019-02-27T00:30:46.372Z> INF/NODEOAM/MZ Memory usage: { rss: 149495808,
-8e FCT-1011 <2019-02-27T00:30:46.372Z> INF/NODEOAM/MZ   heapTotal: 91846144,
-8f FCT-1011 <2019-02-27T00:30:46.372Z> INF/NODEOAM/MZ   heapUsed: 76202904,
-90 FCT-1011 <2019-02-27T00:30:46.372Z> INF/NODEOAM/MZ   external: 378214 }
-91 FCT-1011 <2019-02-27T00:30:56.372Z> INF/NODEOAM/MZ Memory usage: { rss: 149762048,
-92 FCT-1011 <2019-02-27T00:30:56.372Z> INF/NODEOAM/MZ   heapTotal: 91846144,
-93 FCT-1011 <2019-02-27T00:30:56.372Z> INF/NODEOAM/MZ   heapUsed: 76353672,
-94 FCT-1011 <2019-02-27T00:30:56.372Z> INF/NODEOAM/MZ   external: 379414 }
+EVENT_OUTPUT_any = """
+Line1 contains message number 20
+Line2 contains message number 15
+Line3 contains message number 21
+Line4 contains message number 20
+Line5 contains message number 15
+Line6 contains message number 20
 """
 
-EVENT_KWARGS = {
-    "detect_patterns": ['external', 'Memory'],
-    "match": "sequence"
+EVENT_KWARGS_any = {
+    "detect_patterns": ['number 15', 'number 20'],
+    "match": "any",
+    "till_occurs_times": 3
 }
 
-EVENT_RESULT = [
+EVENT_RESULT_any = [
     {
-        'line': "90 FCT-1011 <2019-02-27T00:30:46.372Z> INF/NODEOAM/MZ   external: 378214 }",
+        'line': "Line1 contains message number 20",
         'time': datetime.datetime(2019, 1, 14, 13, 12, 48, 224929),
     },
     {
-        'line': "91 FCT-1011 <2019-02-27T00:30:56.372Z> INF/NODEOAM/MZ Memory usage: { rss: 149762048,",
+        'line': "Line2 contains message number 15",
         'time': datetime.datetime(2019, 1, 14, 13, 12, 48, 224935),
+    },
+    {
+        'line': "Line4 contains message number 20",
+        'time': datetime.datetime(2019, 1, 14, 13, 12, 48, 224940),
+    }
+]
+
+EVENT_OUTPUT_all = """
+Line1 contains message number 20
+Line2 contains message number 15
+Line3 contains message number 21
+Line4 contains message number 20
+Line5 contains message number 15
+Line6 contains message number 20
+"""
+
+EVENT_KWARGS_all = {
+    "detect_patterns": ['number 15', 'number 20'],
+    "match": "all",
+    "till_occurs_times": 1
+}
+
+EVENT_RESULT_all = [
+    {
+        'line': "Line1 contains message number 20",
+        'time': datetime.datetime(2019, 1, 14, 13, 12, 48, 224929),
+    },
+    {
+        'line': "Line2 contains message number 15",
+        'time': datetime.datetime(2019, 1, 14, 13, 12, 48, 224935),
+    }
+]
+
+EVENT_OUTPUT_sequence = """
+Line1 contains message number 20
+Line2 contains message number 15
+Line3 contains message number 21
+Line4 contains message number 20
+Line5 contains message number 15
+Line6 contains message number 20
+"""
+
+EVENT_KWARGS_sequence = {
+    "detect_patterns": ['number 15', 'number 20'],
+    "match": "sequence",
+    "till_occurs_times": 1
+}
+
+EVENT_RESULT_sequence = [
+    {
+        'line': "Line2 contains message number 15",
+        'time': datetime.datetime(2019, 1, 14, 13, 12, 48, 224929),
+    },
+    {
+        'line': "Line4 contains message number 20",
+        'time': datetime.datetime(2019, 1, 14, 13, 12, 48, 224935),
+    }
+]
+
+EVENT_OUTPUT_sequence2 = """
+Line1 contains message number 20
+Line2 contains message number 15
+Line3 contains message number 21
+Line4 contains message number 20
+Line5 contains message number 15
+Line6 contains message number 20
+"""
+
+EVENT_KWARGS_sequence2 = {
+    "detect_patterns": ['number 15', 'number 20'],
+    "match": "sequence",
+    "till_occurs_times": 2
+}
+
+EVENT_RESULT_sequence2 = [
+    {
+        'line': "Line2 contains message number 15",
+        'time': datetime.datetime(2019, 1, 14, 13, 12, 48, 224929),
+    },
+    {
+        'line': "Line4 contains message number 20",
+        'time': datetime.datetime(2019, 1, 14, 13, 12, 48, 224935),
+    },
+    {
+        'line': "Line5 contains message number 15",
+        'time': datetime.datetime(2019, 1, 14, 13, 12, 48, 224940),
+    },
+    {
+        'line': "Line6 contains message number 20",
+        'time': datetime.datetime(2019, 1, 14, 13, 12, 48, 224945),
     }
 ]
