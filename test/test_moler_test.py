1511a31d79f329b95378d34c682cf452aea695de
Marcin Usielski <marcin.usielski@nokia.com>
2019-03-07 13:24:58 +0100

    new test

--- test/test_moler_test.py
+++ test/test_moler_test.py
@@ -35,6 +35,15 @@ def test_moler_test_wrapper():
     assert decorated == ret
 
 
+def test_moler_test_exception_no_exception():
+    ConnectionObserver.get_unraised_exceptions()
+    from moler.cmd.unix.ls import Ls
+    cmd = Ls(connection=None)
+    cmd.set_exception("wrong exception")
+    with pytest.raises(MolerStatusException):
+        moler_test_not_raise_exception_when_no_steps_end_for_global_method()
+
+
 def test_moler_test_not_raise_exception_when_steps_end(moler_test_se):
     ConnectionObserver.get_unraised_exceptions()
     moler_test_se.test_not_raise_exception_when_steps_end()
