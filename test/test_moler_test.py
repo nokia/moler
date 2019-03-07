dd0af377746d17beedb1e8f415bbbd540b177e72
Marcin Usielski <marcin.usielski@nokia.com>
2019-03-07 13:06:34 +0100

    another new test

--- test/test_moler_test.py
+++ test/test_moler_test.py
@@ -17,13 +17,22 @@ def test_moler_test_warn():
 
 
 def test_moler_test_not_raise_exception_when_no_steps_end_for_global_method_twice():
+    ConnectionObserver.get_unraised_exceptions()
     moler_test_not_raise_exception_when_no_steps_end_for_global_method_twice()
 
 
 def test_moler_test_raise_exception_when_not_callable_passed():
-    var="blabla"
+    ConnectionObserver.get_unraised_exceptions()
+    var = "blabla"
     with pytest.raises(MolerStatusException):
         MolerTest._decorate(var)
+
+
+def test_moler_test_wrapper():
+    ConnectionObserver.get_unraised_exceptions()
+    decorated = moler_test_raise_exception_when_no_steps_end_for_global_method
+    ret = MolerTest._wrapper(decorated, False)
+    assert decorated == ret
 
 
 def test_moler_test_not_raise_exception_when_steps_end(moler_test_se):
