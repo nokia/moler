71940996194fc89c2ae4bbe94545f762d368df81
Sylwester Golonka <sylwester.golonka@nokia.com>
2019-03-29 11:02:54 +0100

    add unix_local_root hops, add abstract metaclass

--- moler/device/junipergeneric.py
+++ moler/device/junipergeneric.py
@@ -9,8 +9,11 @@ __email__ = 'sylwester.golonka@nokia.com'
 
 import logging
 from moler.device.unixlocal import UnixLocal
+from abc import ABCMeta
+from six import add_metaclass
 
 
+@add_metaclass(ABCMeta)
 class JuniperGeneric(UnixLocal):
     """Junipergeneric device class."""
 
@@ -36,11 +39,12 @@ class JuniperGeneric(UnixLocal):
         self.logger = logging.getLogger('moler.juniper')
 
     def _get_default_sm_configuration(self):
+        config = super(JuniperGeneric, self)._get_default_sm_configuration()
         if self.use_proxy_pc:
-            config = self._get_default_sm_configuration_with_proxy_pc()
+            default_config = self._get_default_sm_configuration_with_proxy_pc()
         else:
-            config = self._get_default_sm_configuration_without_proxy_pc()
-
+            default_config = self._get_default_sm_configuration_without_proxy_pc()
+        self._update_dict(config, default_config)
         return config
 
     def _get_default_sm_configuration_with_proxy_pc(self):
@@ -156,7 +160,6 @@ class JuniperGeneric(UnixLocal):
 
     def _prepare_transitions(self):
         super(JuniperGeneric, self)._prepare_transitions()
-
         if self.use_proxy_pc:
             transitions = self._prepare_transitions_with_proxy_pc()
         else:
@@ -259,7 +262,7 @@ class JuniperGeneric(UnixLocal):
                     JuniperGeneric.proxy_pc][
                     "command_params"]["expected_prompt"],
             JuniperGeneric.cli:
-                self._configurations[JuniperGeneric.connection_hops][JuniperGeneric.configure][JuniperGeneric.cli][
+                self._configurations[JuniperGeneric.connection_hops][JuniperGeneric.proxy_pc][JuniperGeneric.cli][
                     "command_params"]["expected_prompt"],
             JuniperGeneric.configure:
                 self._configurations[JuniperGeneric.connection_hops][JuniperGeneric.cli][JuniperGeneric.configure][
@@ -274,7 +277,7 @@ class JuniperGeneric(UnixLocal):
                 self._configurations[JuniperGeneric.connection_hops][JuniperGeneric.cli][JuniperGeneric.unix_local][
                     "command_params"]["expected_prompt"],
             JuniperGeneric.cli:
-                self._configurations[JuniperGeneric.connection_hops][JuniperGeneric.configure][JuniperGeneric.cli][
+                self._configurations[JuniperGeneric.connection_hops][JuniperGeneric.unix_local][JuniperGeneric.cli][
                     "command_params"]["expected_prompt"],
             JuniperGeneric.configure:
                 self._configurations[JuniperGeneric.connection_hops][JuniperGeneric.cli][JuniperGeneric.configure][
@@ -295,27 +298,30 @@ class JuniperGeneric(UnixLocal):
 
     def _prepare_state_hops_with_proxy_pc(self):
         state_hops = {
-            UnixLocal.not_connected: {
-                JuniperGeneric.cli: UnixLocal.unix_local,
-                JuniperGeneric.configure: UnixLocal.unix_local,
-                JuniperGeneric.proxy_pc: UnixLocal.unix_local
+            JuniperGeneric.not_connected: {
+                JuniperGeneric.cli: JuniperGeneric.unix_local,
+                JuniperGeneric.configure: JuniperGeneric.unix_local,
+                JuniperGeneric.proxy_pc: JuniperGeneric.unix_local,
+                JuniperGeneric.unix_local_root: JuniperGeneric.unix_local,
             },
             JuniperGeneric.cli: {
-                UnixLocal.not_connected: JuniperGeneric.proxy_pc,
-                UnixLocal.unix_local: JuniperGeneric.proxy_pc
+                JuniperGeneric.not_connected: JuniperGeneric.proxy_pc,
+                JuniperGeneric.unix_local: JuniperGeneric.proxy_pc,
+                JuniperGeneric.unix_local_root: JuniperGeneric.unix_local,
             },
             JuniperGeneric.configure: {
-                UnixLocal.unix_local: JuniperGeneric.cli,
+                JuniperGeneric.unix_local: JuniperGeneric.cli,
                 JuniperGeneric.proxy_pc: JuniperGeneric.cli,
-                UnixLocal.not_connected: JuniperGeneric.cli,
-
+                JuniperGeneric.not_connected: JuniperGeneric.cli,
+                JuniperGeneric.unix_local_root: JuniperGeneric.unix_local
             },
             JuniperGeneric.unix_local: {
                 JuniperGeneric.cli: JuniperGeneric.proxy_pc,
                 JuniperGeneric.configure: JuniperGeneric.proxy_pc,
             },
             JuniperGeneric.proxy_pc: {
-                JuniperGeneric.configure: JuniperGeneric.cli
+                JuniperGeneric.configure: JuniperGeneric.cli,
+                JuniperGeneric.unix_local_root: JuniperGeneric.unix_local
             }
         }
         return state_hops
@@ -325,16 +331,20 @@ class JuniperGeneric(UnixLocal):
             UnixLocal.not_connected: {
                 JuniperGeneric.cli: UnixLocal.unix_local,
                 JuniperGeneric.configure: UnixLocal.unix_local,
+                JuniperGeneric.unix_local_root: JuniperGeneric.unix_local,
+
             },
             UnixLocal.unix_local: {
                 JuniperGeneric.configure: JuniperGeneric.cli
             },
             JuniperGeneric.cli: {
                 UnixLocal.not_connected: UnixLocal.unix_local,
+                JuniperGeneric.unix_local_root: JuniperGeneric.unix_local,
             },
             JuniperGeneric.configure: {
                 UnixLocal.unix_local: JuniperGeneric.cli,
                 UnixLocal.not_connected: JuniperGeneric.cli,
+                JuniperGeneric.unix_local_root: JuniperGeneric.unix_local,
 
             },
         }
