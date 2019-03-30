353054b2466cbe94fbc2c707aebc253d5abc3e8f
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2019-03-30 14:20:35 +0100

    ping started into background

--- trainings/workshop1/network_outage.py
+++ trainings/workshop1/network_outage.py
@@ -7,8 +7,8 @@ def test_network_outage():
     load_config(config=os.path.abspath('config/my_devices.yml'))
     unix1 = DeviceFactory.get_device(name='MyMachine1')
     unix2 = DeviceFactory.get_device(name='MyMachine2')
-    ping = unix1.get_cmd(cmd_name="ping", cmd_params={"destination": "localhost", "options": "-O -c 10"})
-    ping(timeout=10)
+    ping = unix1.get_cmd(cmd_name="ping", cmd_params={"destination": "localhost", "options": "-O"})
+    ping.start(timeout=120)
 
 
 if __name__ == '__main__':
