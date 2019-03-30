017b7474e29d7f9b6d5d0cac107443e228e4f403
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2019-03-30 14:17:41 +0100

    run ping command with timeout

--- trainings/workshop1/network_outage.py
+++ trainings/workshop1/network_outage.py
@@ -7,6 +7,8 @@ def test_network_outage():
     load_config(config=os.path.abspath('config/my_devices.yml'))
     unix1 = DeviceFactory.get_device(name='MyMachine1')
     unix2 = DeviceFactory.get_device(name='MyMachine2')
+    ping = unix1.get_cmd(cmd_name="ping", cmd_params={"destination": "localhost", "options": "-O"})
+    ping(timeout=10)
 
 
 if __name__ == '__main__':
