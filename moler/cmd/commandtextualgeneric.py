722629a66e9c2f5a3d200d50bc5a29bab388b35d
Marcin Usielski <marcin.usielski@nokia.com>
2019-04-01 15:21:40 +0200

    on_new_line adopted

--- moler/cmd/commandtextualgeneric.py
+++ moler/cmd/commandtextualgeneric.py
@@ -83,19 +83,6 @@ class CommandTextualGeneric(Command):
         else:
             self._cmd_escaped = re.compile(re.escape(command_string))
 
-    @property
-    def _is_done(self):
-        return super(CommandTextualGeneric, self)._is_done()
-
-    @_is_done.setter
-    def _is_done(self, value):
-        if value:
-            if self._stored_exception:
-                exception = self._stored_exception
-                self._stored_exception = None
-                super(CommandTextualGeneric, self).set_exception(exception=exception)
-        super(CommandTextualGeneric, self)._is_done(value)
-
     @staticmethod
     def _calculate_prompt(prompt):
         if not prompt:
@@ -160,7 +147,12 @@ class CommandTextualGeneric(Command):
         if self.is_end_of_cmd_output(line):
             if (self.ret_required and self.has_any_result()) or not self.ret_required:
                 if not self.done():
-                    self.set_result(self.current_ret)
+                    if self._stored_exception:
+                        exception = self._stored_exception
+                        self._stored_exception = None
+                        super(CommandTextualGeneric, self).set_exception(exception=exception)
+                    else:
+                        self.set_result(self.current_ret)
             else:
                 self._log(lvl=logging.DEBUG,
                           msg="Found candidate for final prompt but current ret is None or empty, required not None nor empty.")
