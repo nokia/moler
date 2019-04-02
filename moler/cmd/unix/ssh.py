0d540d07ec9aa9b31234e2ccc3aeba0b6a5c556f
Marcin Usielski <marcin.usielski@nokia.com>
2019-04-02 16:07:34 +0200

    Fixed ssh command and tests

--- moler/cmd/unix/ssh.py
+++ moler/cmd/unix/ssh.py
@@ -113,6 +113,7 @@ class Ssh(GenericUnixCommand):
             self._id_dsa(line)
             self._host_key_verification(line)
             self._commands_after_established(line, is_full_line)
+            self._detect_prompt(line)
         except ParsingDone:
             pass
         if is_full_line:
@@ -144,6 +145,18 @@ class Ssh(GenericUnixCommand):
                     if not self.done():
                         self.set_result({})
                     raise ParsingDone()
+
+    def _detect_prompt(self, line):
+        """
+        Detects start prompt.
+
+        :param line: Line from device.
+        :return: Nothing but raises ParsingDone if detects start prompt and any exception was set.
+        """
+        if self._regex_helper.search_compiled(self._re_prompt, line):
+            if self._stored_exception:
+                self._is_done = True
+                raise ParsingDone()
 
     def _host_key_verification(self, line):
         """
@@ -226,22 +239,25 @@ class Ssh(GenericUnixCommand):
 
         :return: Nothing.
         """
+        exception = None
         if "rm" == self.known_hosts_on_failure:
             self.connection.sendline("\nrm -f {}".format(self._hosts_file))
         elif "keygen" == self.known_hosts_on_failure:
             self.connection.sendline("\nssh-keygen -R {}".format(self.host))
         else:
-            self.set_exception(
-                CommandFailure(self,
+            exception = CommandFailure(self,
                                "Bad value of parameter known_hosts_on_failure '{}'. "
                                "Supported values: rm or keygen.".format(
-                                   self.known_hosts_on_failure)))
-        self._cmd_output_started = False
-        self._sent_continue_connecting = False
-        self._sent_prompt = False
-        self._sent_timeout = False
-        self._sent_password = False
-        self.connection.sendline(self.command_string)
+                                   self.known_hosts_on_failure))
+        if exception:
+            self.set_exception(exception=exception)
+        else:
+            self._cmd_output_started = False
+            self._sent_continue_connecting = False
+            self._sent_prompt = False
+            self._sent_timeout = False
+            self._sent_password = False
+            self.connection.sendline(self.command_string)
 
     def _send_after_login_settings(self, line):
         """
