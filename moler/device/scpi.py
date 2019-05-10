ee76a76d0b1498804c867ba3b1fafa5347990516
Marcin Usielski <marcin.usielski@nokia.com>
2019-05-10 10:27:39 +0200

    peppa pig

--- moler/device/scpi.py
+++ moler/device/scpi.py
@@ -31,7 +31,7 @@ class Scpi(UnixLocal):
         self.use_proxy_pc = self._is_proxy_pc_in_sm_params(sm_params, Scpi.proxy_pc)
         initial_state = initial_state if initial_state is not None else Scpi.scpi
         super(Scpi, self).__init__(sm_params=sm_params, name=name, io_connection=io_connection, io_type=io_type,
-                                  variant=variant, initial_state=initial_state)
+                                   variant=variant, initial_state=initial_state)
         self.logger = logging.getLogger('moler.scpi')
 
     def _get_default_sm_configuration(self):
