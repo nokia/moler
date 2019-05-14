4379714a5c84e56c5c1de290ce575a55cf61b236
Marcin Usielski <marcin.usielski@nokia.com>
2019-05-14 11:05:16 +0200

    scpi commands

--- moler/cmd/scpi/genricscpi.py
+++ moler/cmd/scpi/genricscpi.py
@@ -14,22 +14,4 @@ __email__ = 'marcin.usielski@nokia.com'
 
 @six.add_metaclass(abc.ABCMeta)
 class GenericScpi(CommandTextualGeneric):
-
-    def __init__(self, connection, prompt=None, newline_chars=None, runner=None):
-        """
-        Base class for Scpi commands.
-
-        :param connection: connection to device.
-        :param prompt: expected prompt sending by device after command execution. Maybe String or compiled re.
-        :param newline_chars:  new line chars on device (a list).
-        :param runner: runner to run command.
-        """
-
-        super(GenericScpi, self).__init__(connection=connection, prompt=prompt, newline_chars=newline_chars,
-                                          runner=runner)
-        self.current_ret['RAW_OUTPUT'] = list()
-
-    def on_new_line(self, line, is_full_line):
-        if is_full_line:
-            self.current_ret['RAW_OUTPUT'].append(line)
-        return super(GenericScpi, self).on_new_line(line=line, is_full_line=is_full_line)
+    pass
--- moler/cmd/scpi/scpi/genericscpi.py
+++ moler/cmd/scpi/scpi/genericscpi.py
@@ -14,4 +14,22 @@ __email__ = 'marcin.usielski@nokia.com'
 
 @six.add_metaclass(abc.ABCMeta)
 class GenericScpi(DefaultGeneric):
-    pass
+    def __init__(self, connection, prompt=None, newline_chars=None, runner=None):
+        """
+        Base class for Scpi commands in state SCPI.
+
+        :param connection: connection to device.
+        :param prompt: expected prompt sending by device after command execution. Maybe String or compiled re.
+        :param newline_chars:  new line chars on device (a list).
+        :param runner: runner to run command.
+        """
+
+        super(GenericScpi, self).__init__(connection=connection, prompt=prompt, newline_chars=newline_chars,
+                                          runner=runner)
+        self.current_ret['RAW_OUTPUT'] = list()
+
+    def on_new_line(self, line, is_full_line):
+        if is_full_line:
+            self.current_ret['RAW_OUTPUT'].append(line)
+        return super(GenericScpi, self).on_new_line(line=line, is_full_line=is_full_line)
+
