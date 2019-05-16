df286aa5597d7a924225f030b6d67163c20b3e24
Marcin Usielski <marcin.usielski@nokia.com>
2019-05-16 15:32:38 +0200

    advise event

--- moler/events/unix/advise_to_change_your_password.py
+++ moler/events/unix/advise_to_change_your_password.py
@@ -5,22 +5,21 @@ __email__ = 'marcin.usielski@nokia.com'
 
 import datetime
 import re
-from dateutil import parser
 
 from moler.events.textualevent import TextualEvent
 from moler.exceptions import ParsingDone
 
 
-class LastLogin(TextualEvent):
+class AdviseToChangeYourPassword(TextualEvent):
     def __init__(self, connection, till_occurs_times=-1, runner=None):
         """
-        Event for 'Last login ... from ...'
+        Event for advise to change password.
 
         :param connection: moler connection to device, terminal when command is executed
         :param till_occurs_times: number of event occurrence
         :param runner: Runner to run event
         """
-        super(LastLogin, self).__init__(connection=connection, runner=runner, till_occurs_times=till_occurs_times)
+        super(AdviseToChangeYourPassword, self).__init__(connection=connection, runner=runner, till_occurs_times=till_occurs_times)
         self.current_ret = dict()
 
     def on_new_line(self, line, is_full_line):
@@ -33,27 +32,25 @@ class LastLogin(TextualEvent):
         """
         if is_full_line:
             try:
-                self._parse_last_login(line=line)
+                self._parse_advise(line=line)
             except ParsingDone:
                 pass
 
-    # Last login: Tue Jun 12 08:54:44 2018 from 127.0.0.1
-    _re_last_login = re.compile(r'Last login:\s+(?P<DATE>\S.*\S)\s+from\s+(?P<HOST>\S+)', re.I)
+    # Warning: you are advised to change your password (more than 90 days old)
+    _re_advise = re.compile(
+        r'Warning: you are advised to change your password \(more than\s+(?P<DAYS>\d+)\s+days old\)', re.I)
 
-    def _parse_last_login(self, line):
-        if self._regex_helper.search(LastLogin._re_last_login, line):
+    def _parse_advise(self, line):
+        if self._regex_helper.search(AdviseToChangeYourPassword._re_advise, line):
             self.current_ret["time"] = datetime.datetime.now()
-            self.current_ret["host"] = self._regex_helper.group("HOST")
-            date_str = self._regex_helper.group("DATE")
-            self.current_ret["date_raw"] = date_str
-            self.current_ret["date"] = parser.parse(date_str)
+            self.current_ret["days"] = int(self._regex_helper.group("DAYS"))
             self.event_occurred(event_data=self.current_ret)
             self.current_ret = dict()
             raise ParsingDone()
 
 
 EVENT_OUTPUT = """
-Last login: Tue Jun 12 08:54:44 2018 from 127.0.0.1
+Warning: you are advised to change your password (more than 90 days old)
 """
 
 EVENT_KWARGS = {
@@ -63,8 +60,6 @@ EVENT_KWARGS = {
 EVENT_RESULT = [
     {
         'time': datetime.datetime(2019, 1, 14, 13, 12, 48),
-        'host': '127.0.0.1',
-        'date_raw': 'Tue Jun 12 08:54:44 2018',
-        'date': datetime.datetime(2018, 6, 12, 8, 54, 44),
+        'days': 90
     }
 ]
