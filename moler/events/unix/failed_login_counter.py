00d3880ed971dffecf346b715054d73906323141
Marcin Usielski <marcin.usielski@nokia.com>
2019-05-17 11:23:51 +0200

    failed login counter

---
+++ moler/events/unix/failed_login_counter.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Marcin Usielski'
+__copyright__ = 'Copyright (C) 2019, Nokia'
+__email__ = 'marcin.usielski@nokia.com'
+
+import datetime
+import re
+
+from moler.events.lineevent import LineEvent
+
+
+class FailedLoginCounter(LineEvent):
+
+    # There were 2 failed login attempts since the last successful login
+    _re_attempts = re.compile(
+        r'There were (?P<ATTEMPTS_NR>\d+) failed login attempts since the last successful login', re.I)
+
+    def __init__(self, connection, till_occurs_times=-1, runner=None):
+        """
+        Event for check failed login attempts.
+
+        :param connection: moler connection to device, terminal when command is executed
+        :param till_occurs_times: number of event occurrence
+        :param runner: Runner to run event
+        """
+        super(FailedLoginCounter, self).__init__(connection=connection,
+                                                 runner=runner,
+                                                 till_occurs_times=till_occurs_times,
+                                                 detect_patterns=[FailedLoginCounter._re_attempts])
+        self.process_full_lines_only = True
+
+
+EVENT_OUTPUT = """
+There were 2 failed login attempts since the last successful login.
+"""
+
+EVENT_KWARGS = {
+    "till_occurs_times": 1
+}
+
+EVENT_RESULT = [
+    {
+        'time': datetime.datetime(2019, 1, 14, 13, 12, 48),
+        'groups': ('2',),
+        'line': 'There were 2 failed login attempts since the last successful login.',
+        'matched': 'There were 2 failed login attempts since the last successful login',
+        'named_groups': {'ATTEMPTS_NR': '2'},
+    }
+]
