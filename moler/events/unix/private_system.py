f95432651d387dc00f7cbdcf2a7bf9eab3ff0924
Marcin Usielski <marcin.usielski@nokia.com>
2019-05-27 11:15:09 +0200

    event fix

--- moler/events/unix/private_system.py
+++ moler/events/unix/private_system.py
@@ -48,40 +48,41 @@ EVENT_KWARGS = {
     "till_occurs_times": 1
 }
 EVENT_RESULT = [
-    {
-        'line': 'You are about to access a private system. This system is for the use of',
-        'matched': 'You are about to access a private system. This system is for the use of',
-        'groups': (),
-        'named_groups': {},
-        'time': datetime.datetime(2019, 5, 17, 12, 42, 38, 278418)
-    },
-    {
-        'line': 'authorized users only. All connections are logged to the extent and by means',
-        'matched': 'authorized users only. All connections are logged to the extent and by means',
-        'groups': (),
-        'named_groups': {},
-        'time': datetime.datetime(2019, 5, 17, 12, 42, 38, 278418)
-    },
-    {
-        'line': 'acceptable by the local legislation. Any unauthorized access or access',
-        'matched': 'acceptable by the local legislation. Any unauthorized access or access',
-        'groups': (),
-        'named_groups': {},
-        'time': datetime.datetime(2019, 5, 17, 12, 42, 38, 278418)
-    },
-    {
-        'line': 'attempts may be punished to the fullest extent possible under the applicable',
-        'matched': 'attempts may be punished to the fullest extent possible under the applicable',
-        'groups': (),
-        'named_groups': {},
-        'time': datetime.datetime(2019, 5, 17, 12, 42, 38, 278418)
-    },
-    {
-        'line': 'local legislation.',
-        'matched': 'local legislation.',
-        'groups': (),
-        'named_groups': {},
-        'time': datetime.datetime(2019, 5, 17, 12, 42, 38, 278418)
-    },
+    [
+        {
+            'line': 'You are about to access a private system. This system is for the use of',
+            'matched': 'You are about to access a private system. This system is for the use of',
+            'groups': (),
+            'named_groups': {},
+            'time': datetime.datetime(2019, 5, 17, 12, 42, 38, 278418)
+        },
+        {
+            'line': 'authorized users only. All connections are logged to the extent and by means',
+            'matched': 'authorized users only. All connections are logged to the extent and by means',
+            'groups': (),
+            'named_groups': {},
+            'time': datetime.datetime(2019, 5, 17, 12, 42, 38, 278418)
+        },
+        {
+            'line': 'acceptable by the local legislation. Any unauthorized access or access',
+            'matched': 'acceptable by the local legislation. Any unauthorized access or access',
+            'groups': (),
+            'named_groups': {},
+            'time': datetime.datetime(2019, 5, 17, 12, 42, 38, 278418)
+        },
+        {
+            'line': 'attempts may be punished to the fullest extent possible under the applicable',
+            'matched': 'attempts may be punished to the fullest extent possible under the applicable',
+            'groups': (),
+            'named_groups': {},
+            'time': datetime.datetime(2019, 5, 17, 12, 42, 38, 278418)
+        },
+        {
+            'line': 'local legislation.',
+            'matched': 'local legislation.',
+            'groups': (),
+            'named_groups': {},
+            'time': datetime.datetime(2019, 5, 17, 12, 42, 38, 278418)
+        },
+    ]
 ]
-
