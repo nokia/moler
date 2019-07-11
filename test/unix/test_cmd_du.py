c9b42661181b06040eef49ab3920e9d3839b7f5f
Marcin Szlapa <marcin.szlapa@nokia.com>
2019-07-11 06:05:52 -0400

    du

--- test/unix/test_cmd_du.py
+++ test/unix/test_cmd_du.py
@@ -9,6 +9,7 @@ __email__ = 'marcin.szlapa@nokia.com'
 
 from moler.cmd.unix.du import Du
 
+
 def test_du_returns_proper_command_string(buffer_connection):
     du_cmd = Du(buffer_connection, options="-sk *")
     assert "du -sk *" == du_cmd.command_string
