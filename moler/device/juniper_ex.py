75746b394a9ee3686f8172dec3487e63e455668e
Jakub Kupiec <jakub.kupiec@nokia.com>
2019-08-14 05:11:41 -0400

    add_example_conf

--- moler/device/juniper_ex.py
+++ moler/device/juniper_ex.py
@@ -16,3 +16,54 @@ class JuniperEX(JuniperGeneric):
     """Juniperex device class."""
 
     pass
+
+
+"""
+Example of device in yaml configuration file:
+    - with PROXY_PC:
+      JUNIPER_EX_PROXY_PC:
+        DEVICE_CLASS: moler.device.juniper_ex.JuniperEX
+        INITIAL_STATE: UNIX_LOCAL
+        CONNECTION_HOPS:
+          PROXY_PC:
+            CLI:
+              execute_command: ssh
+              command_params:
+                expected_prompt: "admin@switch>"
+                host: cli_host
+                login: cli_login
+                password: password
+                set_timeout: null
+            UNIX_LOCAL:
+              execute_command: exit
+              command_params:
+                expected_prompt: "moler_bash#"
+          UNIX_LOCAL:
+            PROXY_PC:
+              execute_command: ssh
+              command_params:
+                expected_prompt: "proxy_pc#"
+                host: proxy_pc_host
+                login: proxy_pc_login
+                password: password
+                set_timeout: null
+          CLI:
+            PROXY_PC:
+              execute_command: exit
+              command_params:
+                expected_prompt: "proxy_pc#"
+    -without PROXY_PC:
+      JUNIPER_EX:
+        DEVICE_CLASS: moler.device.juniper_ex.JuniperEX
+        INITIAL_STATE: UNIX_LOCAL
+        CONNECTION_HOPS:
+          UNIX_LOCAL:
+            CLI:
+              execute_command: ssh # default value
+              command_params:
+                host: cli_host
+                login: cli_login
+                password: password
+                set_timeout: null
+
+"""
