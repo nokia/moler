5a4cfce6827645299a77f9651aa178312e833a10
Marcin Usielski <marcin.usielski@nokia.com>
2019-09-09 10:47:33 +0200

    peppa

--- moler/device/textualdevice.py
+++ moler/device/textualdevice.py
@@ -99,8 +99,9 @@ class TextualDevice(object):
         self._collect_events_for_state_machine()
         self._run_prompts_observers()
         self._default_prompt = re.compile(r'^[^<]*[\$|%|#|>|~]\s*$')
-        msg = "Created device '{}' as instance of class '{}.{}'. with prompts:".format(self.name, self.__class__.__module__,
-                                                                         self.__class__.__name__)
+        msg = "Created device '{}' as instance of class '{}.{}'. with prompts:".format(self.name,
+                                                                                       self.__class__.__module__,
+                                                                                       self.__class__.__name__)
         self._log(level=logging.INFO, msg=msg)
         self._log(level=logging.INFO, msg=self._reverse_state_prompts_dict)
 
