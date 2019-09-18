6703569bc657805561fcc954085fe664849b0233
Michal Ernst <41105075+Ernold11@users.noreply.github.com>
2019-09-18 10:02:16 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Nokia'
 # The short X.Y version
 version = '0.13.0'
 # The full version, including alpha/beta/rc tags
-release = 'alpha'
+release = 'beta'
 
 
 # -- General configuration ---------------------------------------------------
