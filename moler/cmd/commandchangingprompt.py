7d4240cbac29c774f647432b240ccf59a34140e2
Marcin Usielski <marcin.usielski@nokia.com>
2019-10-02 15:41:20 +0200

    new generic

---
+++ moler/cmd/commandchangingprompt.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Generic Unix/Linux module
+"""
+
+__author__ = 'Marcin Usielski'
+__copyright__ = 'Copyright (C) 2019, Nokia'
+__email__ = 'marcin.usielski@nokia.com'
+
+import abc
+import six
+
+from moler.cmd.commandtextualgeneric import CommandTextualGeneric
+
+
+@six.add_metaclass(abc.ABCMeta)
+class CommandChangingPrompt(CommandTextualGeneric):
+    """Base class for textual commands."""
+
+    def __init__(self, connection, prompt=None, newline_chars=None, runner=None):
+        """
+        Base class for textual commands.
+
+        :param connection: connection to device.
+        :param prompt: expected prompt sending by device after command execution. Maybe String or compiled re.
+        :param newline_chars:  new line chars on device (a list).
+        :param runner: runner to run command.
+        """
+        super(CommandChangingPrompt, self).__init__(connection=connection, prompt=prompt, newline_chars=newline_chars,
+                                                    runner=runner)
