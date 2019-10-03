cdd207a3b6fc2b6cb086fcb8d1b8b77caf2292d0
Marcin Usielski <marcin.usielski@nokia.com>
2019-10-03 14:28:23 +0200

    exit

--- moler/cmd/unix/exit.py
+++ moler/cmd/unix/exit.py
@@ -3,45 +3,29 @@
 Exit command module.
 """
 
-__author__ = 'Michal Ernst'
-__copyright__ = 'Copyright (C) 2018, Nokia'
-__email__ = 'michal.ernst@nokia.com'
-
-from moler.exceptions import ParsingDone
-from moler.cmd.unix.genericunix import GenericUnixCommand
-from moler.cmd.commandtextualgeneric import CommandTextualGeneric
+__author__ = 'Michal Ernst, Marcin Usielski'
+__copyright__ = 'Copyright (C) 2018-2019, Nokia'
+__email__ = 'michal.ernst@nokia.com, marcin.usielski@nokia.com'
 
 
-class Exit(GenericUnixCommand):
-    def __init__(self, connection, prompt=None, expected_prompt='>', newline_chars=None, runner=None, target_newline="\n"):
+from moler.cmd.commandchangingprompt import CommandChangingPrompt
+
+
+class Exit(CommandChangingPrompt):
+    def __init__(self, connection, prompt=None, expected_prompt='>', newline_chars=None, runner=None,
+                 target_newline="\n"):
         """
         :param connection:
         :param prompt: Prompt of the starting shell
         :param expected_prompt: Prompt of the target shell reached after exit command
         :param newline_chars:
         """
-        super(Exit, self).__init__(connection=connection, prompt=prompt, newline_chars=newline_chars, runner=runner)
-        self.ret_required = False
-        self.target_newline = target_newline
-
-        # Parameters defined by calling the command
-        self._re_expected_prompt = CommandTextualGeneric._calculate_prompt(expected_prompt)  # Expected prompt on device
+        super(Exit, self).__init__(connection=connection, prompt=prompt, newline_chars=newline_chars, runner=runner,
+                                   expected_prompt=expected_prompt, target_newline=target_newline)
 
     def build_command_string(self):
         cmd = "exit"
         return cmd
-
-    def on_new_line(self, line, is_full_line):
-        try:
-            self._is_target_prompt(line)
-        except ParsingDone:
-            pass  # line has been fully parsed by one of above parse-methods
-
-    def _is_target_prompt(self, line):
-        if self._regex_helper.search_compiled(self._re_expected_prompt, line):
-            if not self.done():
-                self.set_result({})
-                raise ParsingDone
 
 
 COMMAND_OUTPUT = """
