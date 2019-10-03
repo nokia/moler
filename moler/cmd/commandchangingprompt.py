eb7475ff3d6f163c104b69f1c45c7a70a41bf4fb
Marcin Usielski <marcin.usielski@nokia.com>
2019-10-03 18:37:10 +0200

    docs

--- moler/cmd/commandchangingprompt.py
+++ moler/cmd/commandchangingprompt.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Generic command class for commands changing prompt
+Generic command class for commands change prompt
 """
 
 __author__ = 'Marcin Usielski'
@@ -23,9 +23,11 @@ class CommandChangingPrompt(CommandTextualGeneric):
         Base class for textual commands which change prompt.
 
         :param connection: connection to device.
+        :param expected_prompt: prompt on device changed by this command.
         :param prompt: expected prompt sending by device after command execution. Maybe String or compiled re.
         :param newline_chars:  new line chars on device (a list).
         :param runner: runner to run command.
+        :param target_newline: newline on device when command is finished and prompt is changed.
         """
         super(CommandChangingPrompt, self).__init__(connection=connection, prompt=prompt, newline_chars=newline_chars,
                                                     runner=runner)
@@ -35,12 +37,26 @@ class CommandChangingPrompt(CommandTextualGeneric):
         self.target_newline = target_newline
 
     def on_new_line(self, line, is_full_line):
+        """
+        Process output from device line by line.
+
+        :param line: Line from device.
+        :param is_full_line: True if line was eneded with newline char(s), False otherwise.
+        :return: None
+        """
         try:
             self._is_expected_prompt(line)
         except ParsingDone:
             pass  # line has been fully parsed by one of above parse-methods
 
     def _is_expected_prompt(self, line):
+        """
+        Checks if line contains expected prompt.
+
+        :param line:
+        :return: None
+        :raise: ParsingDone if line contains expected prompt
+        """
         if self._regex_helper.search_compiled(self._re_expected_prompt, line):
             if not self.done():
                 self.set_result(self.current_ret)
--- moler/cmd/unix/exit.py
+++ moler/cmd/unix/exit.py
@@ -15,15 +15,22 @@ class Exit(CommandChangingPrompt):
     def __init__(self, connection, prompt=None, expected_prompt='>', newline_chars=None, runner=None,
                  target_newline="\n"):
         """
-        :param connection:
-        :param prompt: Prompt of the starting shell
-        :param expected_prompt: Prompt of the target shell reached after exit command
-        :param newline_chars:
+        :param connection: connection to device.
+        :param expected_prompt: prompt on device changed by this command.
+        :param prompt: expected prompt sending by device after command execution. Maybe String or compiled re.
+        :param newline_chars:  new line chars on device (a list).
+        :param runner: runner to run command.
+        :param target_newline: newline on device when command is finished and prompt is changed.
         """
         super(Exit, self).__init__(connection=connection, prompt=prompt, newline_chars=newline_chars, runner=runner,
                                    expected_prompt=expected_prompt, target_newline=target_newline)
 
     def build_command_string(self):
+        """
+        Returns a string with command.
+
+        :return: String with the command.
+        """
         cmd = "exit"
         return cmd
 
