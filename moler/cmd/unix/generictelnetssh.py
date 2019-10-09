6f060fc990c8e5ce1ca5ce586de1276ca0df0886
Marcin Usielski <marcin.usielski@nokia.com>
2019-10-09 14:50:37 +0200

    delted commented code

--- moler/cmd/unix/generictelnetssh.py
+++ moler/cmd/unix/generictelnetssh.py
@@ -75,10 +75,6 @@ class GenericTelnetSsh(CommandChangingPrompt):
                                                prompt_after_login=prompt_after_login)
 
         # Parameters defined by calling the command
-        # self._re_expected_prompt = CommandTextualGeneric._calculate_prompt(expected_prompt)  # Expected prompt on device
-        # self._re_prompt_after_login = self._re_expected_prompt
-        # if prompt_after_login:
-        #     self._re_prompt_after_login = CommandTextualGeneric._calculate_prompt(prompt_after_login)
         self._re_failure_exceptions_indication = None
         if failure_exceptions_indication:
             self._re_failure_exceptions_indication = CommandTextualGeneric._calculate_prompt(
@@ -92,17 +88,11 @@ class GenericTelnetSsh(CommandChangingPrompt):
             self._passwords = copy_list(password, deep_copy=False)  # copy of list of passwords to modify
         self.host = host
         self.port = port
-        # self.set_timeout = set_timeout
-        # self.set_prompt = set_prompt
         self.term_mono = term_mono
         self.encrypt_password = encrypt_password
-        # self.target_newline = target_newline
-        # self.allowed_newline_after_prompt = allowed_newline_after_prompt
         self.repeat_password = repeat_password
 
         # Internal variables
-        # self._sent_timeout = False
-        # self._sent_prompt = False
         self._sent_login = False
         self._last_password = ""
 
@@ -136,34 +126,6 @@ class GenericTelnetSsh(CommandChangingPrompt):
             if not self._is_failure_exception(line):
                 self.set_exception(CommandFailure(self, "command failed in line '{}'".format(line)))
                 raise ParsingDone()
-
-    # def _detect_prompt_after_exception(self, line):
-    #     """
-    #     Detects start prompt.
-    #
-    #     :param line: Line from device.
-    #     :return: Nothing but raises ParsingDone if detects start prompt and any exception was set.
-    #     """
-    #     if self._stored_exception and self._regex_helper.search_compiled(self._re_prompt, line):
-    #         self._is_done = True
-    #         raise ParsingDone()
-
-    # def _settings_after_login(self, line, is_full_line):
-    #     """
-    #     Checks if settings after login are requested and sent.
-    #
-    #     :param line: Line from device.
-    #     :param is_full_line: True if line had new line chars, False otherwise.
-    #     :return: Nothing but raises ParsingDone if line has information to handle by this method.
-    #     """
-    #     sent = self._send_after_login_settings(line)
-    #     if sent:
-    #         raise ParsingDone()
-    #     if (not sent) and self._is_target_prompt(line) and (not is_full_line or self.allowed_newline_after_prompt):
-    #         if self._all_after_login_settings_sent() or self._no_after_login_settings_needed():
-    #             if not self.done():
-    #                 self.set_result({})
-    #                 raise ParsingDone()
 
     def _just_connected(self, line):
         """
@@ -212,73 +174,6 @@ class GenericTelnetSsh(CommandChangingPrompt):
             self._sent = True
             raise ParsingDone()
 
-    # def _send_after_login_settings(self, line):
-    #     """
-    #     Sends commands to set timeout and to change prompt.
-    #
-    #     :param line: Line from device.
-    #     :return: True if any command was sent, False if no command was sent.
-    #     """
-    #     if self._is_prompt_after_login(line):
-    #         if self._commands_to_set_connection_after_login(line):
-    #             return True
-    #         if self._timeout_set_needed() and not self._sent:
-    #             self._send_timeout_set()
-    #             return True  # just sent
-    #         elif self._prompt_set_needed() and not self._sent:
-    #             self._send_prompt_set()
-    #             return True  # just sent
-    #     return False  # nothing sent
-
-    # def _commands_to_set_connection_after_login(self, line):
-    #     return False
-
-    # def _no_after_login_settings_needed(self):
-    #     """
-    #     Checks if prompt and timeout commands are sent.
-    #
-    #     :return: True if commands for login nor timeout are no needed.
-    #     """
-    #     return (not self.set_prompt) and (not self.set_timeout)
-
-    # def _timeout_set_needed(self):
-    #     """
-    #     Checks if command to set timeout is still needed.
-    #
-    #     :return: True if command to set timeout is needed, otherwise (sent or not requested) False
-    #     """
-    #     return self.set_timeout and not self._sent_timeout
-
-    # def _send_timeout_set(self):
-    #     """
-    #     Sends command to set timeout
-    #
-    #     :return: Nothing
-    #     """
-    #     self.connection.sendline("")
-    #     self.connection.sendline(self.set_timeout)
-    #     self._sent_timeout = True
-    #     self._sent = True
-
-    # def _prompt_set_needed(self):
-    #     """
-    #     Checks if command to set prompt is still needed.
-    #
-    #     :return: True if command to set prompt is needed, otherwise (sent or not requested) False
-    #     """
-    #     return self.set_prompt and not self._sent_prompt
-
-    # def _send_prompt_set(self):
-    #     """
-    #     Sends command to set prompt.
-    #
-    #     :return: Nothing
-    #     """
-    #     self.connection.sendline("")
-    #     self.connection.sendline(self.set_prompt)
-    #     self._sent_prompt = True
-    #     self._sent = True
-
     def is_failure_indication(self, line):
         """
         Checks if line contains information that command fails.
@@ -316,43 +211,3 @@ class GenericTelnetSsh(CommandChangingPrompt):
         :return: Match object or None
         """
         return self._regex_helper.search_compiled(GenericTelnetSsh._re_password, line)
-
-    # def _is_target_prompt(self, line):
-    #     """
-    #     Checks if line contains prompt on target system.
-    #
-    #     :param line: Line from device
-    #     :return: Match object or None
-    #     """
-    #     return self._regex_helper.search_compiled(self._re_expected_prompt, line)
-
-    # def _is_prompt_after_login(self, line):
-    #     """
-    #     Checks if line contains prompt just after login.
-    #
-    #     :param line: Line from device
-    #     :return: Match object or None
-    #     """
-    #     return self._regex_helper.search_compiled(self._re_prompt_after_login, line)
-
-    # def _all_after_login_settings_sent(self):
-    #     """
-    #     Checks if all commands were sent by telnet command.
-    #
-    #     :return: True if all requested commands were sent, False if at least one left.
-    #     """
-    #     additional_commands_sent = self._sent_additional_settings_commands()  # Useful for Telnet commands
-    #     both_requested = self.set_prompt and self.set_timeout
-    #     both_sent = self._sent_prompt and self._sent_timeout
-    #     req_and_sent_prompt = self.set_prompt and self._sent_prompt
-    #     req_and_sent_timeout = self.set_timeout and self._sent_timeout
-    #     terminal_cmds_sent = ((both_requested and both_sent) or req_and_sent_timeout or req_and_sent_prompt)
-    #     return terminal_cmds_sent and additional_commands_sent
-
-    # def _sent_additional_settings_commands(self):
-    #     """
-    #     Checks if additional commands after connection established are sent (useful for telnet, not used for ssh).
-    #
-    #     :return: True if all additional commands are sent (if any). False if any command left in the queue.
-    #     """
-    #     return True
