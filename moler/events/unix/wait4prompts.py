c5016456dd7f4d5a147ab882e7661448b5daaba6
Marcin Usielski <marcin.usielski@nokia.com>
2019-10-11 13:57:21 +0200

    wait4prompts

--- moler/events/unix/wait4prompts.py
+++ moler/events/unix/wait4prompts.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-__author__ = 'Michal Ernst'
+__author__ = 'Michal Ernst, Marcin Usielski'
 __copyright__ = 'Copyright (C) 2019, Nokia'
-__email__ = 'michal.ernst@nokia.com'
+__email__ = 'michal.ernst@nokia.com, marcin.usielski@nokia.com'
 import datetime
 import re
 
@@ -68,6 +68,30 @@ EVENT_KWARGS = {
 }
 
 EVENT_RESULT = [
+    {
+        'line': "host:~ #",
+        "prompt_regex": "host:.*#",
+        "state": "UNIX_LOCAL",
+        'time': datetime.datetime(2019, 8, 22, 12, 42, 38, 278418)
+    }
+]
+
+EVENT_OUTPUT_compiled = """
+user@host01:~> TERM=xterm-mono telnet -4 host.domain.net 1500
+Login:
+Login:user
+Password:
+Last login: Thu Nov 23 10:38:16 2017 from 127.0.0.1
+Have a lot of fun...
+CLIENT5 [] has just connected!
+host:~ #"""
+
+EVENT_KWARGS_compiled = {
+    "prompts": {re.compile(r'host:.*#'): "UNIX_LOCAL"},
+    "till_occurs_times": 1
+}
+
+EVENT_RESULT_compiled = [
     {
         'line': "host:~ #",
         "prompt_regex": "host:.*#",
