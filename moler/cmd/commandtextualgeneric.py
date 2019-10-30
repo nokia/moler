cb635b46f23ede2c4bdbad89adb7cd71107216b1
Marcin Usielski <marcin.usielski@nokia.com>
2019-10-30 11:09:23 +0100

    new test

--- moler/cmd/commandtextualgeneric.py
+++ moler/cmd/commandtextualgeneric.py
@@ -356,12 +356,12 @@ class CommandTextualGeneric(Command):
                "break_on_timeout='{}', _last_not_full_line='{}', _re_prompt='{}', do_not_process_after_done='{}', "
                "newline_after_command_string='{}', wait_for_prompt_on_exception='{}', _stored_exception='{}', "
                "current_ret='{}', _newline_chars='{}', _concatenate_before_command_starts='{}', "
-               "_command_string_right_index='{}'.").format(
+               "_command_string_right_index='{}', _command_string_left_index='{}'.").format(
             self.__command_string, self._cmd_escaped, self._cmd_output_started, self.ret_required,
             self.break_on_timeout, self._last_not_full_line, self._re_prompt, self.do_not_process_after_done,
             self.newline_after_command_string, self.wait_for_prompt_on_exception, self._stored_exception,
             self.current_ret, self._newline_chars, self._concatenate_before_command_starts,
-            self._command_string_right_index)
+            self._command_string_right_index, self._command_string_left_index)
         self._log(logging.DEBUG, msg, levels_to_go_up=2)
 
     def has_any_result(self):
