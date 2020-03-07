c3552bde19ae996a22ffe95ecfee7d69b77900dc
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2020-03-07 19:26:46 +0100

    CommandTextualGeneric show regexp pattern and not compiled-object in logs

--- moler/cmd/commandtextualgeneric.py
+++ moler/cmd/commandtextualgeneric.py
@@ -378,8 +378,8 @@ class CommandTextualGeneric(Command):
                "newline_after_command_string='{}', wait_for_prompt_on_exception='{}', _stored_exception='{}', "
                "current_ret='{}', _newline_chars='{}', _concatenate_before_command_starts='{}', "
                "_command_string_right_index='{}', _command_string_left_index='{}'.").format(
-            self.__command_string, self._cmd_escaped, self._cmd_output_started, self.ret_required,
-            self.break_on_timeout, self._last_not_full_line, self._re_prompt, self.do_not_process_after_done,
+            self.__command_string, self._cmd_escaped.pattern, self._cmd_output_started, self.ret_required,
+            self.break_on_timeout, self._last_not_full_line, self._re_prompt.pattern, self.do_not_process_after_done,
             self.newline_after_command_string, self.wait_for_prompt_on_exception, self._stored_exception,
             self.current_ret, self._newline_chars, self._concatenate_before_command_starts,
             self._max_index_from_beginning, self._max_index_from_end)
