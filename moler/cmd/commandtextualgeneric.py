937a657de77662449a2ea3fd7836c0811facd161
Marcin Usielski <marcin.usielski@nokia.com>
2020-03-12 15:39:42 +0100

    fix

--- moler/cmd/commandtextualgeneric.py
+++ moler/cmd/commandtextualgeneric.py
@@ -184,8 +184,6 @@ class CommandTextualGeneric(Command):
         :param data: List of strings sent by device.
         :return: None.
         """
-        # Workaround for some terminals and python 2.7
-        data = u"".join(str(data.encode("utf-8", errors="ignore"))) if sys.version_info < (3, 0) else data
         lines = data.splitlines(True)
         for current_chunk in lines:
             line, is_full_line = self._update_from_cached_incomplete_line(current_chunk=current_chunk)
