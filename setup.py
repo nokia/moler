ac2416b2a43919fc7f8878f5e3cf2db162e443e9
rserafin <radoslaw.serafin@nokia.com>
2020-03-25 10:49:20 +0100

    New release v1.9.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with io.open(join(here, 'requirements.txt'), encoding='utf-8') as f:
 
 setup(
     name='moler',  # Required
-    version='1.8.0',  # Required
+    version='1.9.0',  # Required
     description='Moler is library to help in building automated tests',  # Required
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',  # Optional (see note above)
