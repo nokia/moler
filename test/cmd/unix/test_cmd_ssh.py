548630e8c3de5d390d490033b4fa8253409f1b96
Marcin Usielski <marcin.usielski@nokia.com>
2020-04-16 08:57:37 +0200

    fix test

--- test/cmd/unix/test_cmd_ssh.py
+++ test/cmd/unix/test_cmd_ssh.py
@@ -4,7 +4,7 @@ Testing of ssh command.
 """
 
 __author__ = 'Marcin Usielski'
-__copyright__ = 'Copyright (C) 2018-2019, Nokia'
+__copyright__ = 'Copyright (C) 2018-2020, Nokia'
 __email__ = 'marcin.usielski@nokia.com'
 
 from moler.cmd.unix.ssh import Ssh
