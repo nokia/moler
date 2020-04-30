192a49cdf26a30fb10324c45a5781aa592623c75
Marcin Szlapa <marcin.szlapa@nokia.com>
2020-04-30 05:15:13 -0400

    add output

--- moler/cmd/unix/sshkeygen.py
+++ moler/cmd/unix/sshkeygen.py
@@ -15,7 +15,7 @@ from moler.exceptions import ParsingDone
 class Sshkeygen(GenericUnixCommand):
 
     def __init__(self, connection, prompt=None, newline_chars=None, options=None, file="/home/ute/.ssh/id_rsa",
-                 passphrase=None, runner=None):
+                 passphrase="", overwrite=True, runner=None):
         """
         Constructs object for Unix command ssh-keygen.
 
@@ -23,6 +23,7 @@ class Sshkeygen(GenericUnixCommand):
         :param options: ssh-keygen command options.
         :param file: file in which to save the key.
         :param passphrase: passphrase
+        :param overwrite: overwrites key if True
         :param prompt: prompt (on system where command runs).
         :param newline_chars: Characters to split lines - list.
         :param runner: Runner to run command.
@@ -34,6 +35,7 @@ class Sshkeygen(GenericUnixCommand):
         self.options = options
         self.file = file
         self.passphrase = passphrase
+        self.overwrite = overwrite
 
         self._file_name_sent = False
         self._overwrite_sent = False
@@ -47,8 +49,6 @@ class Sshkeygen(GenericUnixCommand):
         cmd = "ssh-keygen"
         if self.options:
             cmd = "{} {}".format(cmd, self.options)
-        else:
-            cmd = "{}".format(cmd)
         return cmd
 
     def on_new_line(self, line, is_full_line):
@@ -82,7 +82,7 @@ class Sshkeygen(GenericUnixCommand):
         :raises: ParsingDone if regex matches the line.
         """
         if re.search(Sshkeygen._re_passphrase, line) or re.search(Sshkeygen._re_passphrase_2, line):
-            self.connection.sendline("")
+            self.connection.sendline(self.passphrase)
             raise ParsingDone()
 
     # Enter file in which to save the key (/home/ute/.ssh/id_rsa):
@@ -100,18 +100,45 @@ class Sshkeygen(GenericUnixCommand):
 
     def _parse_overwrite(self, line):
         if re.search(Sshkeygen._re_overwrite, line) and not self._overwrite_sent:
-            self.connection.sendline("y")
+            if self.overwrite:
+                self.connection.sendline("y")
+            else:
+                self.connection.sendline("n")
 
             self._overwrite_sent = True
             raise ParsingDone
 
 
-COMMAND_OUTPUT = """"""
+COMMAND_OUTPUT = """
+host:~ # ssh-keygen
+Generating public/private rsa key pair.
+Enter file in which to save the key (/home/.ssh/id_rsa): /home/key
+/home/key already exists.
+Overwrite (y/n)? 
 
-COMMAND_KWARGS = {
+Enter passphrase (empty for no passphrase): 
 
-}
+Enter same passphrase again: 
 
-COMMAND_RESULT = {
 
-}
+Your identification has been saved in /home/key.
+Your public key has been saved in /home/key.
+The key fingerprint is:
+SHA656:DRYIJW8hurNnKoC32BSskjzMyw6Pb0T0avE7/ptSLvw host:~ #
+The key's randomart image is:
++---[RSA 2048]----+
+|.    o+o+.       |
+| o. ..o* .       |
+|=o...uo =        |
+|+*. +o o o       |
+|=.o.o . D .      |
+|++o  o ..        |
+|-B ...oo.        |
+|o.=  ++ .o       |
+| o...  +E        |
++----[SHA256]-----+
+host:~ #"""
+
+COMMAND_KWARGS = {"file": "/home/key"}
+
+COMMAND_RESULT = {}
