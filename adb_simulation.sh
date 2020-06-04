4aaa0868d22d46d16652e577588c7f23f14134ec
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2020-06-04 16:11:31 +0200

    introduce adbshell user into CI for AdbRemote2 testing

--- adb_simulation.sh
+++ adb_simulation.sh
@@ -3,12 +3,12 @@ if [ $1 = "devices" ]; then
   echo 1234567890
 elif [ $1 = "shell" ]; then
   echo simulate adb shell via ssh into shell
-  echo sshpass -p moler_password ssh molerssh@localhost
-  sshpass -p moler_password ssh molerssh@localhost
+  echo sshpass -p adb_password ssh adbshell@localhost
+  sshpass -p adb_password ssh adbshell@localhost
 elif [ $1 = "-s" ]; then
   if [ $2 = "1234567890" ] && [ $3 = "shell" ]; then
     echo simulate adb shell via ssh into shell
-    echo sshpass -p moler_password ssh molerssh@localhost
-    sshpass -p moler_password ssh molerssh@localhost
+    echo sshpass -p adb_password ssh adbshell@localhost
+    sshpass -p adb_password ssh adbshell@localhost
   fi
 fi
