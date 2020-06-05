1fbbddb662d4eef23d824da8931f0376e74452b6
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2020-06-05 15:05:33 +0200

    adb_simulation.sh - more verbose

--- adb_simulation.sh
+++ adb_simulation.sh
@@ -1,14 +1,22 @@
 echo $0
+whoami
+which sshpass
 if [ "$1" = "devices" ]; then
   echo 1234567890
 elif [ "$1" = "shell" ]; then
   echo simulate adb shell via ssh into shell
   echo sshpass -p adb_password ssh adbshell@localhost
   sshpass -p adb_password ssh adbshell@localhost
+  exit_code=$?
+  echo "SSH STATUS: $exit_code"
+  whoami
 elif [ "$1" = "-s" ]; then
   if [ "$2" = "1234567890" ] && [ "$3" = "shell" ]; then
     echo simulate adb shell via ssh into shell
     echo sshpass -p adb_password ssh adbshell@localhost
     sshpass -p adb_password ssh adbshell@localhost
+    exit_code=$?
+    echo "SSH STATUS: $exit_code"
+    whoami
   fi
 fi
