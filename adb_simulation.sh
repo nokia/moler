ffc61beb5843a0169f1675584c801f47bb63d807
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2020-06-05 11:59:41 +0200

    quotes in adb_simulation.sh - protect against "unary operator expected" bash error

--- adb_simulation.sh
+++ adb_simulation.sh
@@ -1,12 +1,12 @@
 echo $0
-if [ $1 = "devices" ]; then
+if [ "$1" = "devices" ]; then
   echo 1234567890
-elif [ $1 = "shell" ]; then
+elif [ "$1" = "shell" ]; then
   echo simulate adb shell via ssh into shell
   echo sshpass -p adb_password ssh adbshell@localhost
   sshpass -p adb_password ssh adbshell@localhost
-elif [ $1 = "-s" ]; then
-  if [ $2 = "1234567890" ] && [ $3 = "shell" ]; then
+elif [ "$1" = "-s" ]; then
+  if [ "$2" = "1234567890" ] && [ "$3" = "shell" ]; then
     echo simulate adb shell via ssh into shell
     echo sshpass -p adb_password ssh adbshell@localhost
     sshpass -p adb_password ssh adbshell@localhost
