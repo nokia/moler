26c86300af189e3c144d4e36b808442a2ffd02b0
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2020-06-05 15:50:52 +0200

    CI: run all tests after fix

--- adb_simulation.sh
+++ adb_simulation.sh
@@ -1,11 +1,10 @@
 echo $0
 whoami
-which sshpass
 if [ "$1" = "devices" ]; then
   echo 1234567890
 elif [ "$1" = "shell" ]; then
   echo simulate adb shell via ssh into shell
-  echo sshpass -p adb_password ssh adbshell@localhost
+  echo sshpass -p adb_password ssh -oStrictHostKeyChecking=no adbshell@localhost
   sshpass -p adb_password ssh -oStrictHostKeyChecking=no adbshell@localhost
   exit_code=$?
   echo "SSH STATUS: $exit_code"
@@ -13,7 +12,7 @@ elif [ "$1" = "shell" ]; then
 elif [ "$1" = "-s" ]; then
   if [ "$2" = "1234567890" ] && [ "$3" = "shell" ]; then
     echo simulate adb shell via ssh into shell
-    echo sshpass -p adb_password ssh adbshell@localhost
+    echo sshpass -p adb_password ssh -oStrictHostKeyChecking=no adbshell@localhost
     sshpass -p adb_password ssh -oStrictHostKeyChecking=no adbshell@localhost
     exit_code=$?
     echo "SSH STATUS: $exit_code"
