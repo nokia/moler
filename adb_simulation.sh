0449e16b3400c2b34657b2175f6c0b67be0a452e
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2020-06-05 15:29:00 +0200

    adb_simulation.sh - fix against SSH STATUS: 6
    The exit code 6 means that "Host public key is unknown"

--- adb_simulation.sh
+++ adb_simulation.sh
@@ -6,7 +6,7 @@ if [ "$1" = "devices" ]; then
 elif [ "$1" = "shell" ]; then
   echo simulate adb shell via ssh into shell
   echo sshpass -p adb_password ssh adbshell@localhost
-  sshpass -p adb_password ssh adbshell@localhost
+  sshpass -p adb_password ssh -oStrictHostKeyChecking=no adbshell@localhost
   exit_code=$?
   echo "SSH STATUS: $exit_code"
   whoami
@@ -14,7 +14,7 @@ elif [ "$1" = "-s" ]; then
   if [ "$2" = "1234567890" ] && [ "$3" = "shell" ]; then
     echo simulate adb shell via ssh into shell
     echo sshpass -p adb_password ssh adbshell@localhost
-    sshpass -p adb_password ssh adbshell@localhost
+    sshpass -p adb_password ssh -oStrictHostKeyChecking=no adbshell@localhost
     exit_code=$?
     echo "SSH STATUS: $exit_code"
     whoami
