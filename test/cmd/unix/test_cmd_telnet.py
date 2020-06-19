ebf411afde25042a9a31bc4994c2873fbec9ea81
Marcin Usielski <marcin.usielski@nokia.com>
2020-06-19 17:06:16 +0200

    fix for testg

--- test/cmd/unix/test_cmd_telnet.py
+++ test/cmd/unix/test_cmd_telnet.py
@@ -178,6 +178,13 @@ def command_output_and_expected_result():
     for line in lines:
         data = data + line
     result = dict()
+    result['LINES'] = [
+        'Login:user',
+        'Password: ',
+        'Last login: Thu Nov 23 10:38:16 2017 from 127.0.0.1',
+        'Have a lot of fun...',
+        'host:~ # export TMOUT="2678400"'
+    ]
     return data, result
 
 
