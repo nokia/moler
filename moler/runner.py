cc8527acc1066b60209a813ec8b38c4004fb18b2
Marcin Usielski <35992110+marcin-usielski@users.noreply.github.com>
2020-12-08 17:09:58 +0100

    Log exception from runner (#367)
    * Log exception from runner

--- moler/runner.py
+++ moler/runner.py
@@ -443,7 +443,8 @@ class ThreadPoolExecutorRunner(ConnectionObserverRunner):
                 # observers should not raise exceptions during data parsing
                 # but if they do so - we fix it
                 with observer_lock:
-                    self.logger.warning("Unhandled exception from '{} 'caught by runner.".format(connection_observer))
+                    self.logger.warning("Unhandled exception from '{} 'caught by runner. '{}' : '{}'.".format(
+                        connection_observer, exc, repr(exc)))
                     ex_msg = "Unexpected exception from {} caught by runner when processing data >>{}<< at '{}':" \
                              " >>>{}<<< -> repr: >>>{}<<<".format(connection_observer, data, timestamp, exc, repr(exc))
                     if connection_observer.is_command():
