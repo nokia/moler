035a77dced49d0239d3a099d1a9aa34a1b635628
Adam Klekowski <klekowskiadam@gmail.com>
2021-03-10 08:36:18 +0100

    remove reduntant parameter in test

--- test/cmd/at/test_set_mode.py
+++ test/cmd/at/test_set_mode.py
@@ -12,6 +12,6 @@ from moler.cmd.at.set_mode import SetMode
 from moler.exceptions import WrongUsage
 
 
-def test_for_incorrect_mode(buffer_connection, command_output_and_expected_result_for_line_in_use):
+def test_for_incorrect_mode():
     with pytest.raises(WrongUsage):
-        _ = SetMode(connection=buffer_connection.moler_connection, selected_mode="Incorrect")
+        SetMode(selected_mode="Incorrect")
