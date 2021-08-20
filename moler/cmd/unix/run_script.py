deecad55e1ea1f0412b442fb2d2bccc9eb08f3b0
Marcin Usielski <marcin.usielski@nokia.com>
2021-08-20 15:51:20 +0200

    regex for success and for failure for command RunScript

--- moler/cmd/unix/run_script.py
+++ moler/cmd/unix/run_script.py
@@ -4,31 +4,34 @@ Run script command
 """
 
 __author__ = 'Marcin Usielski'
-__copyright__ = 'Copyright (C) 2018-2020, Nokia'
+__copyright__ = 'Copyright (C) 2018-2021, Nokia'
 __email__ = 'marcin.usielski@nokia.com'
 
 import re
 
 from moler.cmd.unix.genericunix import GenericUnixCommand
-from moler.exceptions import CommandFailure
+from moler.exceptions import CommandFailure, ParsingDone
+from moler.helpers import copy_list
 
 
 class RunScript(GenericUnixCommand):
 
     def __init__(self, connection, script_command, error_regex=re.compile("error", re.IGNORECASE),
-                 prompt=None, newline_chars=None, runner=None):
+                 prompt=None, newline_chars=None, runner=None, success_regex=None):
         """
         :param connection: Moler connection to device, terminal when command is executed.
         :param script_command: path to script to run.
-        :param error_regex: Regex to detect error in script output.l
+        :param error_regex: Regex to detect error in script output. Regex or list of regex.
         :param prompt: prompt (on system where command runs).
         :param newline_chars: Characters to split lines - list.
         :param runner: Runner to run command.
+        :param success_regex: Regex must be found in output to success command. Regex or list of regex.
         """
         super(RunScript, self).__init__(connection=connection, prompt=prompt, newline_chars=newline_chars,
                                         runner=runner)
         self.script_command = script_command
-        self.error_regex = error_regex
+        self.success_regex = self._copy_list(success_regex)
+        self.error_regex = self._copy_list(error_regex)
         self.ret_required = False
 
     def build_command_string(self):
@@ -45,9 +48,42 @@ class RunScript(GenericUnixCommand):
         :param is_full_line: True if line had new line chars, False otherwise
         :return: None
         """
-        if self.error_regex and self._regex_helper.search_compiled(self.error_regex, line):
-            self.set_exception(CommandFailure(self, "Found error regex in line '{}'".format(line)))
+        try:
+            self._find_error(line)
+            self._find_success(line)
+        except ParsingDone:
+            pass
         return super(RunScript, self).on_new_line(line, is_full_line)
+
+    def _find_success(self, line):
+        for success_regex in self.success_regex:
+            if self._regex_helper.search_compiled(success_regex, line):
+                self.success_regex.remove(success_regex)
+                raise ParsingDone()
+
+    def _find_error(self, line):
+        for error_regex in self.error_regex:
+            if self._regex_helper.search_compiled(error_regex, line):
+                self.set_exception(CommandFailure(self, "Found error regex '{}' in line '{}'".format(
+                    error_regex, line)))
+                raise ParsingDone()
+
+    def _copy_list(self, src):
+        if src is None:
+            return list()
+        if isinstance(src, list) or isinstance(src, tuple):
+            return copy_list(src, deep_copy=False)
+        self.success_regex = list(src)
+
+    @property
+    def _is_done(self):
+        return super(RunScript, self)._is_done
+
+    @_is_done.setter
+    def _is_done(self, value):
+        if value is True and len(self.success_regex) > 0:
+            self.set_exception(CommandFailure(self, "Not found all regex for success. Left: '{}'.".format(
+                self.success_regex)))
 
 
 COMMAND_OUTPUT = """
