09c9b1ca8447bad7b40f89b6452bbed2077e06dc
Marcin Usielski <35992110+marcin-usielski@users.noreply.github.com>
2021-08-24 07:20:06 +0200

    Parsing fail and success for RunScript (#418)
    * regex for success and for failure for command RunScript
    
    * RunScript with success_regex
    
    * fix
    
    * list comp

--- test/cmd/unix/test_cmd_run_script.py
+++ test/cmd/unix/test_cmd_run_script.py
@@ -4,7 +4,7 @@ RunScript command test module.
 """
 
 __author__ = 'Marcin Usielski'
-__copyright__ = 'Copyright (C) 2018, Nokia'
+__copyright__ = 'Copyright (C) 2018-2021, Nokia'
 __email__ = 'marcin.usielski@nokia.com'
 
 import pytest
@@ -25,6 +25,24 @@ def test_run_script_raise_exception(buffer_connection, command_output_and_expect
         cmd()
 
 
+def test_run_script_raise_exception_wrong_output(buffer_connection, command_output_lines):
+    command_output, expected_result = command_output_lines
+    buffer_connection.remote_inject_response([command_output])
+    cmd = RunScript(connection=buffer_connection.moler_connection, script_command="./myScript.sh",
+                    success_regex="Line 3", error_regex=None)
+    with pytest.raises(CommandFailure):
+        cmd()
+
+
+def test_run_script_proper_output(buffer_connection, command_output_lines):
+    command_output, expected_result = command_output_lines
+    buffer_connection.remote_inject_response([command_output])
+    cmd = RunScript(connection=buffer_connection.moler_connection, script_command="./myScript.sh",
+                    success_regex=["Line 1", "Line 2"])
+    result = cmd()
+    assert result == expected_result
+
+
 def test_run_script_not_raise_exception(buffer_connection, command_output_and_expected_result):
     command_output, expected_result = command_output_and_expected_result
     buffer_connection.remote_inject_response([command_output])
@@ -35,8 +53,19 @@ def test_run_script_not_raise_exception(buffer_connection, command_output_and_ex
 
 @pytest.fixture
 def command_output_and_expected_result():
-    data = """ute@debdev:~$ ./myScript.sh
+    data = """./myScript.sh
 ERROR: wrong data
-ute@debdev:~$"""
+moler_bash#"""
+    result = dict()
+    return data, result
+
+
+@pytest.fixture
+def command_output_lines():
+    data = """./myScript.sh
+Line 1
+Inter line
+Line 2
+moler_bash#"""
     result = dict()
     return data, result
