bf14d2dc6a70f97744a04b7209919d3c93b23b9f
Marcin Usielski <35992110+marcin-usielski@users.noreply.github.com>
2023-02-21 08:30:08 +0100

    Version 2.8.0 (#460)
    * Version 2.8.0
    
    * Update README.md
    
    v2.8.0
    
    * Update README.md
    
    add support 3.9 and 3.10
    
    ---------
    
    Co-authored-by: tkrol88 <tomasz.krol@nokia.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2019-2023, Nokia'
 author = 'Nokia'
 
 # The short X.Y version
-version = '2.7.0'
+version = '2.8.0'
 # The full version, including alpha/beta/rc tags
 release = 'stable'
 
