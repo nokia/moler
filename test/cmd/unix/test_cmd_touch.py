e2f3f87caebe2ed7a586d8a3e20eb5ac5feb06a9
Marcin Usielski <marcin.usielski@nokia.com>
2024-01-12 16:16:35 +0100

    new test [AI]

--- test/cmd/unix/test_cmd_touch.py
+++ test/cmd/unix/test_cmd_touch.py
@@ -23,6 +23,15 @@ def test_touch_cannot_create(buffer_connection):
 
 def test_touch_read_only(buffer_connection):
     touch_cmd = Touch(connection=buffer_connection.moler_connection, path="file.asc")
+    assert "touch file.asc" == touch_cmd.command_string
+    command_output = "touch file.asc\ntouch: cannot touch 'file.asc': Read-only file system\nmoler_bash#"
+    buffer_connection.remote_inject_response([command_output])
+    with pytest.raises(CommandFailure):
+        touch_cmd()
+
+
+def test_touch_read_only_remove(buffer_connection):
+    touch_cmd = Touch(connection=buffer_connection.moler_connection, path="file.asc")
     touch_cmd._re_fail = None
     touch_cmd.add_failure_indication("Read-only file system")
     assert "touch file.asc" == touch_cmd.command_string
