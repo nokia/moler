37f44f71e933e71e6f4bb9f9a55d177a93327ac4
Marcin Usielski <marcin.usielski@nokia.com>
2024-01-24 11:43:19 +0100

    life status

--- moler/util/connection_observer_life_status.py
+++ moler/util/connection_observer_life_status.py
@@ -14,7 +14,7 @@ class ConnectionObserverLifeStatus(object):
         """
         self.inactivity_timeout: float = 0.0  # If positive value and no data are sent by connection in this time then method
         #                                on_inactivity will be called.
-        self.last_feed_time:  Optional(float) = None  # Time of last called data_received or on_inactivity.
+        self.last_feed_time:  Optional[float] = None  # Time of last called data_received or on_inactivity.
         self.start_time: float = 0.0  # means epoch: 1970-01-01 00:00:00
         self.in_terminating: bool = False  # Set True if ConnectionObserver object is just after __timeout but it can do
         #                              something during terminating_timeout. False if the ConnectionObserver object runs
@@ -25,6 +25,6 @@ class ConnectionObserverLifeStatus(object):
         self.terminating_timeout: float = 0.0  # value for terminating connection_observer when it timeouts. Set positive value
         #                                        for command if they can do anything if timeout. Set 0 for observer or command
         #                                        if it cannot do anything if timeout.
-        self.timeout: Optional(float) = 20.0  # default
+        self.timeout: Optional[float] = 20.0  # default
         self.is_done: bool = False
         self.is_cancelled: bool = False
