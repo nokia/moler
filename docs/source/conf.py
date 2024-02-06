68021e1e84a65e453a97ed7a18a8304302bf4398
Marcin Usielski <marcin.usielski@nokia.com>
2024-02-06 15:51:59 +0100

    Version 3.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2019-2024, Nokia'
 author = 'Nokia'
 
 # The short X.Y version
-version = '3.0.0'
+version = '3.1.0'
 # The full version, including alpha/beta/rc tags
 release = 'stable'
 
