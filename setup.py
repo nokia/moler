9a3d21583056caefa7e25a95a7626278ca2f7e30
tkrol88 <tomasz.krol@nokia.com>
2024-02-13 06:20:13 +0000

    add 'python_requires' +  code reformat (#503)

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
 import io
 from os.path import abspath, dirname, join
-
 from setuptools import setup, find_packages
 
 here = abspath(dirname(__file__))
-
 with io.open(join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 with io.open(join(here, 'requirements.txt'), encoding='utf-8') as f:
-    requires = f.read().splitlines()
+    requirements = f.read().splitlines()
 
 setup(
-    name='moler',  # Required
-    version='3.1.0',  # Required
+    name='moler',
+    version='3.1.0',
     description='Moler is a library for working with terminals, mainly for automated tests',  # Required
-    long_description=long_description,  # Optional
-    long_description_content_type='text/markdown',  # Optional (see note above)
-    url='https://github.com/nokia/moler',  # Optional
-    author='Nokia',  # Optional
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/nokia/moler',
+    author='Nokia',
     license='BSD 3-Clause',
-    classifiers=[  # Optional
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
 
         'Environment :: Console',
@@ -47,31 +45,11 @@ setup(
         'Topic :: Software Development :: Testing',
         'Topic :: System :: Networking'
     ],
-
-    keywords='testing development',  # Optional
-
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
-    packages=find_packages(exclude=['docs', 'examples', 'images', 'test']),  # Required
-
-    package_data={'moler': ['config/bash_config']},
-
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=requires,  # Optional
-
-    project_urls={  # Optional
+    keywords='testing development',
+    packages=find_packages(exclude=['docs', 'examples', 'images', 'test', 'test.*']),
+    install_requires=requirements,
+    python_requires='>=3.7',
+    project_urls={
         'Bug Reports': 'https://github.com/nokia/moler/issues',
         'Source': 'https://github.com/nokia/moler',
     },
