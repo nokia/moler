18486e0d315bdc5ed4d3fc2020423f9e5bbab79f
Marcin Usielski <marcin.usielski@nokia.com>
2024-02-14 11:30:01 +0100

    [AI] docstrings

--- moler/util/connection_observer_life_status.py
+++ moler/util/connection_observer_life_status.py
@@ -18,21 +18,21 @@ class ConnectionObserverLifeStatus:
         Create instance of ConnectionObserverLifeStatus class.
         """
         self.inactivity_timeout: float = 0.0  # If positive value and no data are sent by connection in this time then method
-        #                                on_inactivity will be called.
+        #                                       on_inactivity will be called.
         self.last_feed_time: Optional[float] = None  # Time of last called data_received or on_inactivity.
-        self.start_time: float = 0.0  # means epoch: 1970-01-01 00:00:00
+        self.start_time: float = 0.0  # use time.monotonic() to get current time
         self.in_terminating: bool = False  # Set True if ConnectionObserver object is just after __timeout but it can do
-        #                              something during terminating_timeout. False if the ConnectionObserver object runs
-        #                              during normal timeout. For Runners only!
+        #                                    something during terminating_timeout. False if the ConnectionObserver
+        #                                    object runs during normal timeout. For Runners only!
         self.was_on_timeout_called: bool = False  # Set True if method on_timeout was called. False otherwise. For Runners
         #                                           only!
-        self._is_running: bool = False
+        self._is_running: bool = False  # Set True if ConnectionObserver object is running. False otherwise.
         self.terminating_timeout: float = 0.0  # value for terminating connection_observer when it timeouts. Set positive value
         #                                        for command if they can do anything if timeout. Set 0 for observer or command
         #                                        if it cannot do anything if timeout.
         self.timeout: Optional[float] = 20.0  # default
-        self.is_done: bool = False
-        self.is_cancelled: bool = False
+        self.is_done: bool = False  # Set True if ConnectionObserver object is done. False otherwise.
+        self.is_cancelled: bool = False  # Set True if ConnectionObserver object is cancelled. False otherwise.
 
     def __str__(self) -> str:
         """
@@ -47,7 +47,8 @@ class ConnectionObserverLifeStatus:
                f"timeout={self.timeout}, is_done={self.is_done}, is_cancelled={self.is_cancelled})"
 
     def __repr__(self) -> str:
-        """ Returns string representation of ConnectionObserverLifeStatus object.
+        """
+        Return string representation of ConnectionObserverLifeStatus object.
         :return: String representation of ConnectionObserverLifeStatus object.
         """
 
