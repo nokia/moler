15a7a4f7ed600521bef5736f34da72c8d1d5a806
U-NSN-INTRA\miernst <miernst@N-20UEPF2SNFE6.nsn-intra.net>
2024-02-15 15:55:15 +0100

    Change one problematic string

--- moler/util/cmds_events_doc.py
+++ moler/util/cmds_events_doc.py
@@ -301,7 +301,12 @@ def check_if_documentation_exists(path2cmds):
     if errors_found:
         print("\n".join(errors_found))
         msg = f"Following {observer_type.lower()} have incorrect documentation:"
-        err_msg = "{}\n    {}".format(msg, "\n    ".join(wrong_commands.keys()))  # pylint: disable=consider-using-f-string
+        err_msg = f"{msg}\n    "
+
+        for wrong_command in wrong_commands.keys():
+            err_msg += f"\n    {wrong_command}"
+
+        # err_msg = "{}\n    {}".format(msg, "\n    ".join(wrong_commands.keys()))  # pylint: disable=consider-using-f-string
         print(err_msg)
         return False
     if number_of_command_found == 0:
