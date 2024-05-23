e64ed51da87ee4ab315b97123d77aa2c7323de22
Marcin Usielski <marcin.usielski@nokia.com>
2024-05-23 14:52:20 +0200

    c

--- moler/device/abstract_device.py
+++ moler/device/abstract_device.py
@@ -5,7 +5,7 @@ Moler's device has 2 main responsibilities:
 - be the state machine that controls which commands may run in given state
 """
 __author__ = "Marcin Usielski"
-__copyright__ = "Copyright (C) 2019-2020, Nokia"
+__copyright__ = "Copyright (C) 2019-2024, Nokia"
 __email__ = "marcin.usielski@nokia.com"
 
 import abc
