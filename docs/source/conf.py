af135e99212ac5f3a705cd87b837767a5621b2f4
Marcin Usielski <35992110+marcin-usielski@users.noreply.github.com>
2026-02-04 09:42:23 +0100

    Version 4.7.0 (#589)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2019-2026, Nokia'
 author = 'Nokia'
 
 # The short X.Y version
-version = '4.6.0'
+version = '4.7.0'
 # The full version, including alpha/beta/rc tags
 release = 'stable'
 
